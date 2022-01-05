2b3422d731c72bb0d043beaf87ab459ed94e44ca
techyCoder81 <42820193+techyCoder81@users.noreply.github.com>
2022-01-04 23:07:59 -0500

    fixing paths
    fixing python build script paths

--- scripts/make_dist.py
+++ scripts/make_dist.py
@@ -7,16 +7,17 @@ exec(open("build.py").read())
 
 # build zips
 
-if not os.path.exists("build"):
+if not os.path.exists("../build"):
+  print("no build dir!")
   exit("build dir was missing!")
 
 # if distribution folder exists, delete it
-if "distributions" in os.listdir('.'):
-  shutil.rmtree('distributions')
-os.mkdir('distributions')
+if "distributions" in os.listdir('..'):
+  shutil.rmtree('../distributions')
+os.mkdir('../distributions')
 
 # zip each folder in the staging dir
-packages = os.listdir("build")
+packages = os.listdir("../build")
 for package_name in packages:
-  shutil.make_archive(package_name, 'zip', 'build/' + package_name)
-  shutil.move(package_name + ".zip", "distributions/" + package_name + ".zip")
+  shutil.make_archive(package_name, 'zip', '../build/' + package_name)
+  shutil.move(package_name + ".zip", "../distributions/" + package_name + ".zip")
