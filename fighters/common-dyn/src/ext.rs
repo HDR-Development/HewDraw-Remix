40cb57ee02ae8386fc96d2c9bda373dd7ab6ac33
blu-dev <the.blu.dev@gmail.com>
2022-01-09 21:45:39 -0700

    Don't recompile every fighter after modifying common
    This uses `common-dyn`, which is a new crate that uses predefined exports from `common` which prevents a recompilation of every crate. Common is still built into `fighters` so the changes should reflect.

---
+++ fighters/common-dyn/src/ext.rs
@@ -0,0 +1,1 @@
+include!("../../common/src/ext.rs");
---
+++ fighters/common-dyn/src/lib.rs
@@ -0,0 +1,9 @@
+/// This crate is intended to be used during development to help speed up compile times
+/// The `common` crate itself gets built and linked into the `fighters` crate, however 
+/// all of the individual characters will depend on this crate, as changing the `common` crate can cause
+/// lengthy compile times if we have to rebuild the entire plugin numerous times to test some changes
+
+mod ext;
+pub use ext::*;
+
+pub mod djc;
