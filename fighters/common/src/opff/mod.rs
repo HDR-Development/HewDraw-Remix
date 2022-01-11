b56fb6356dc0f691f4fe28c0b0a348c94256e1ff
blu-dev <the.blu.dev@gmail.com>
2022-01-10 17:56:28 -0700

    undo changes to mod.rs

--- fighters/common/src/opff/mod.rs
+++ fighters/common/src/opff/mod.rs
@@ -6,20 +6,20 @@ use smash::lib::{lua_const::*, L2CValue, L2CAgent};
 use smash::lua2cpp::L2CFighterCommon;
 use smash::lua2cpp::L2CFighterBase;							 
 
-// pub mod ledges;
-// pub mod shields;
-// pub mod physics;
-// pub mod tech;
-// pub mod tech_cleanup;
-// pub mod cancels;
-// pub mod var_resets;
-// pub mod gentleman;
-// pub mod momentum_transfer_line;
-pub mod shotos;
+pub mod ledges;
+pub mod shields;
+pub mod physics;
+pub mod tech;
+pub mod tech_cleanup;
+pub mod cancels;
+pub mod var_resets;
+pub mod gentleman;
+pub mod momentum_transfer_line;
+//pub mod shotos;
 //pub mod magic;
-// pub mod gimmick;
-// pub mod floats;
-// pub mod other;
+pub mod gimmick;
+pub mod floats;
+pub mod other;
 
 /*
 pub fn install() {
@@ -51,8 +51,7 @@ impl FrameInfo {
     pub unsafe fn update_and_get(fighter: &mut L2CFighterCommon) -> Option<Self> {
         let lua_state = fighter.lua_state_agent;
         let boma = sv_system::battle_object_module_accessor(lua_state);
-        // let id = hdr::get_player_number(boma);
-        let id = 0;
+        let id = hdr::get_player_number(boma);
         if !(0..8).contains(&id) {
             return None;
         }
