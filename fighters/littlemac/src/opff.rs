bf79d70060b6a010f10368e088b2c607724c49fb
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2022-02-02 16:35:06 -0500

    replace JumpButton cat checks with AirEscape

--- fighters/littlemac/src/opff.rs
+++ fighters/littlemac/src/opff.rs
@@ -100,7 +100,7 @@ unsafe fn nspecial_cancels(boma: &mut BattleObjectModuleAccessor, status_kind: i
     //PM-like neutral-b canceling
     if status_kind == *FIGHTER_LITTLEMAC_STATUS_KIND_SPECIAL_N_START {
         if situation_kind == *SITUATION_KIND_AIR {
-            if boma.is_cat_flag(Cat1::JumpButton) {
+            if boma.is_cat_flag(Cat1::AirEscape) {
                 WorkModule::unable_transition_term_group(boma, *FIGHTER_STATUS_TRANSITION_GROUP_CHK_AIR_ESCAPE);
                 StatusModule::change_status_request_from_script(boma, *FIGHTER_STATUS_KIND_FALL, false);
                 ControlModule::clear_command_one(boma, *FIGHTER_PAD_COMMAND_CATEGORY1, *FIGHTER_PAD_CMD_CAT1_AIR_ESCAPE);
