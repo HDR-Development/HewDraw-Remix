4fa9b9512860398c6eedb8374781a2a1f26c2589
techyCoder81 <mtklein98@gmail.com>
2022-02-16 12:00:26 -0500

    improve changelog generation
    improve changelog generation to now use the commit message from all merge commits, which means it will pull from the description on pull requests.

--- scripts/make_changelog.py
+++ scripts/make_changelog.py
@@ -7,29 +7,48 @@ branch_name = pkgutil.run_command("git rev-parse --abbrev-ref HEAD > tmp").strip
 print("current branch is " + branch_name)
 
 # build the git command
-git_command = "git log --oneline " + branch_name + "...origin/stable"
+git_command = "git log --oneline " + branch_name + "...origin/engine"
 print("git command is:\n" + git_command)
 log_output = pkgutil.run_command(git_command)
 print(log_output)
 
 # get the cleaned lines of output
 all_lines = log_output.split("\n")
-cleaned_lines = []
+cleaned_lines = set()
 for line in all_lines:
-    if line != "" and "Merge pull request" not in line:
-        cleaned_lines.append(line.strip())
+    if line != "" and "Merge " in line and not "Merge branch 'dev'" in line:
+        cleaned_lines.add(line.strip())
 
-# build the output file
-file_output = ""
-print("cleaned lines:\n")
+#cleaned_lines = reversed(cleaned_lines)
+
+# show cleaned lines
+print("merge lines:\n")
 for line in cleaned_lines:
     print(line)
-    if len(line.split(")")) > 1:
-        commit_text = line.split(")", 1)[1]
-    else:
-        commit_text = line.split(" ", 1)[1]
-    file_output += line.split(" ")[0] + " : " + commit_text + "\n"
 
-os.system("echo '" + file_output + "' > change_summary.txt")
+final_lines = dict()
+
+
+for line in cleaned_lines:
+    commit_hash = line.split(" ")[0]
+    commit_text = pkgutil.run_command("git log -1 --pretty=format:%b " + commit_hash).strip()
+
+    line_add = commit_hash + " : " + commit_text
+    print("adding line: " + line_add)
+    if commit_text.strip() == "":
+        print("empty commit: " + line)
+    else:
+        final_lines[commit_hash] = commit_text
+
+file_output = ""
+for hash,text in final_lines.items():
+    file_output += hash + ' -> "' + text + '"\n\n'
+
+print("\nfile output is:\n" + file_output)
+
+with open('change_summary.txt', "w") as myfile:
+    myfile.write(file_output)
+
+
 
 os.remove('tmp')
