8dca015b0f1b552cb8d9503ce186637ca5c85fa0
techyCoder81 <mtklein98@gmail.com>
2022-02-15 20:51:12 -0500

    generate change summary file
    adding script to generate a changes summary file

---
+++ scripts/make_changelog.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import shutil, os, sys, pkgutil
+
+# determine branch name
+branch_name = pkgutil.run_command("git rev-parse --abbrev-ref HEAD > tmp").strip()
+
+print("current branch is " + branch_name)
+
+# build the git command
+git_command = "git log --pretty=%B " + branch_name + "...stable"
+print("git command is:\n" + git_command)
+log_output = pkgutil.run_command(git_command)
+print(log_output)
+
+# get the cleaned lines of output
+all_lines = log_output.split("\n")
+cleaned_lines = []
+for line in all_lines:
+    if line is not "" and "Merge pull request" not in line:
+        cleaned_lines.append(line.strip())
+
+# build the output file
+file_output = ""
+print("cleaned lines:\n")
+for line in cleaned_lines:
+    print(line)
+    file_output += line + "\n"
+
+os.system("echo '" + file_output + "' > change_summary.txt")
+
+os.remove('tmp')
