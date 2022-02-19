f25d8b22e837cfce41b02a82e39ae11826f79960
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2022-02-19 13:09:34 -0500

    comment out printlns :)))

--- fighters/common/src/function_hooks/change_status.rs
+++ fighters/common/src/function_hooks/change_status.rs
@@ -5,9 +5,9 @@ use globals::*;
 #[skyline::hook(replace=StatusModule::change_status_request_from_script)]
 unsafe fn change_status_request_from_script_hook(boma: &mut BattleObjectModuleAccessor, status_kind: i32, arg3: bool) -> u64 {
     let mut next_status = status_kind;
-    println!("change status from: {}", StatusModule::status_kind(boma));
+    //println!("change status from: {}", StatusModule::status_kind(boma));
     
-    println!("next status: {}", next_status);
+    //println!("next status: {}", next_status);
     original!()(boma, next_status, arg3)
 }
 
