085687f6fed8f5fee798c4f874aa5e49b8d98aeb
blu-dev <the.blu.dev@gmail.com>
2022-03-03 14:20:32 -0700

    fix inputing salty runback leaving pause/training menu up

--- fighters/common/src/function_hooks/controls.rs
+++ fighters/common/src/function_hooks/controls.rs
@@ -4,7 +4,7 @@ use utils::ext::*;
 #[repr(C)]
 struct SomeControllerStruct {
     padding: [u8; 0x10],
-    controller: &'static Controller
+    controller: &'static mut Controller
 }
 
 #[skyline::hook(offset = offsets::map_controls())]
@@ -12,11 +12,11 @@ unsafe fn map_controls_hook(
     mappings: *mut ControllerMapping,
     player_idx: i32,
     out: *mut MappedInputs,
-    controller_struct: &SomeControllerStruct,
+    controller_struct: &mut SomeControllerStruct,
     arg: bool
 ) {
-    let controller = controller_struct.controller;
     let ret = original!()(mappings, player_idx, out, controller_struct, arg);
+    let controller = &mut controller_struct.controller;
 
     // Check if the button combos are being pressed and then force Stock Share + AttackRaw/SpecialRaw depending on input
 
@@ -25,10 +25,20 @@ unsafe fn map_controls_hook(
     && controller.current_buttons.a()
     && (controller.current_buttons.minus() || controller.current_buttons.plus())
     {
+        controller.current_buttons.set_plus(false);
+        controller.current_buttons.set_minus(false);
+        controller.just_down.set_plus(false);
+        controller.just_down.set_minus(false);
+
         if controller.current_buttons.y() {
             (*out).buttons = Buttons::StockShare | Buttons::AttackRaw;
         } else if controller.current_buttons.x() {
             (*out).buttons = Buttons::StockShare | Buttons::SpecialRaw;
+        } else {
+            controller.current_buttons.set_plus(true);
+            controller.current_buttons.set_minus(true);
+            controller.just_down.set_plus(true);
+            controller.just_down.set_minus(true);
         }
     }
 }
