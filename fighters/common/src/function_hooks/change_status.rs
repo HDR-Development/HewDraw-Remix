ed68ae839b5e0c137444e5e250e55b7209dc9ad8
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2022-03-08 18:05:54 -0500

    clean up

--- fighters/common/src/function_hooks/change_status.rs
+++ fighters/common/src/function_hooks/change_status.rs
@@ -6,18 +6,14 @@ use globals::*;
 unsafe fn change_status_request_from_script_hook(boma: &mut BattleObjectModuleAccessor, status_kind: i32, arg3: bool) -> u64 {
     let mut next_status = status_kind;
 
-    if boma.is_fighter() {
-        if boma.kind() == *FIGHTER_KIND_TRAIL {
-            if StatusModule::status_kind(boma) == *FIGHTER_TRAIL_STATUS_KIND_SPECIAL_S_SEARCH {
-                if next_status == *FIGHTER_TRAIL_STATUS_KIND_SPECIAL_S_TURN {
-                    if (!VarModule::is_flag(boma.object(), vars::trail::IS_SIDE_SPECIAL_INPUT)
-                    && !(ControlModule::check_button_on(boma, *CONTROL_PAD_BUTTON_SPECIAL) || ControlModule::check_button_on(boma, *CONTROL_PAD_BUTTON_SPECIAL_RAW)))
-                    || VarModule::is_flag(boma.object(), vars::trail::STOP_SIDE_SPECIAL) { 
-                        next_status = *FIGHTER_TRAIL_STATUS_KIND_SPECIAL_S_END;
-                    }
-                }
-            }
-        }
+    if boma.is_fighter()
+    && boma.kind() == *FIGHTER_KIND_TRAIL
+    && StatusModule::status_kind(boma) == *FIGHTER_TRAIL_STATUS_KIND_SPECIAL_S_SEARCH
+    && next_status == *FIGHTER_TRAIL_STATUS_KIND_SPECIAL_S_TURN
+    && ((!VarModule::is_flag(boma.object(), vars::trail::IS_SIDE_SPECIAL_INPUT)
+    && !(ControlModule::check_button_on(boma, *CONTROL_PAD_BUTTON_SPECIAL) || ControlModule::check_button_on(boma, *CONTROL_PAD_BUTTON_SPECIAL_RAW)))
+        || VarModule::is_flag(boma.object(), vars::trail::STOP_SIDE_SPECIAL)) { 
+        next_status = *FIGHTER_TRAIL_STATUS_KIND_SPECIAL_S_END;
     }
     //println!("next status: {}", next_status);
     original!()(boma, next_status, arg3)
