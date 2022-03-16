4a2f54bae40607f1536ba47f0c77e6769cd9b7a0
black-calculus <52509873+black-calculus@users.noreply.github.com>
2022-03-16 15:20:06 -0700

    Effect beautification
    Add effects to normal down special's reflection, and more telegraphed effects to the wall summon

--- fighters/palutena/src/acmd/specials.rs
+++ fighters/palutena/src/acmd/specials.rs
@@ -114,6 +114,45 @@ unsafe fn palutena_special_lw_reflect_game(fighter: &mut L2CAgentBase) {
 
 }
 
+#[acmd_script( agent = "palutena", script = "effect_speciallwreflect" , category = ACMD_EFFECT , low_priority)]
+unsafe fn palutena_special_lw_reflect_effect(fighter: &mut L2CAgentBase) {
+    let lua_state = fighter.lua_state_agent;
+    let boma = fighter.boma();
+    frame(lua_state, 1.0);
+    if is_excute(fighter) {
+        if VarModule::is_flag(fighter.battle_object, vars::palutena::SPECIAL_LW_AEGIS_REFLECTOR) {
+            LANDING_EFFECT(fighter, Hash40::new("sys_action_smoke_h"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 0.8, 0, 0, 0, 0, 0, 0, false);
+            EFFECT_FOLLOW(fighter, Hash40::new("palutena_wand_light_trace"), Hash40::new("stick"), 0, 8.65, 0, 0, 0, 0, 1, true);
+            EffectModule::enable_sync_init_pos_last(boma);
+            EFFECT_FOLLOW(fighter, Hash40::new("palutena_wand_light2"), Hash40::new("stick"), 0, 8.65, 0, 0, 0, 0, 1, true);
+        }
+    }
+    frame(lua_state, 8.0);
+    if is_excute(fighter) {
+        if VarModule::is_flag(fighter.battle_object, vars::palutena::SPECIAL_LW_AEGIS_REFLECTOR) {
+            EFFECT(fighter, Hash40::new("palutena_throw_twinkle"), Hash40::new("top"), 0.0, 16.0, -8.0, 0, 0, 0, 0.95, 0, 0, 0, 0, 0, 0, true);
+        }
+        else{
+            EFFECT(fighter, Hash40::new("palutena_mirror"), Hash40::new("top"), 12.0, 12.0, 0, 0, 22.5, 0, 0.8, 0, 0, 0, 0, 0, 0, false);
+            EFFECT_DETACH_KIND(fighter, Hash40::new("palutena_backlight"), -1);
+        }
+    }
+    frame(lua_state, 10.0);
+    if is_excute(fighter) {
+        if VarModule::is_flag(fighter.battle_object, vars::palutena::SPECIAL_LW_AEGIS_REFLECTOR) {
+            EFFECT_FOLLOW_ALPHA(fighter, Hash40::new("palutena_backlight"), Hash40::new("top"), -1, 21, 1, 0, 90, 0, 1, true, 0.7);
+        }
+    }
+    frame(lua_state, 35.0);
+    if is_excute(fighter) {
+        if VarModule::is_flag(fighter.battle_object, vars::palutena::SPECIAL_LW_AEGIS_REFLECTOR) {
+            EFFECT_OFF_KIND(fighter, Hash40::new("palutena_wand_light_trace"), false, false);
+            EFFECT_OFF_KIND(fighter, Hash40::new("palutena_wand_light2"), false, false);
+        }
+    }
+
+}
+
 #[acmd_script( agent = "palutena", script = "game_specialairlwreflect" , category = ACMD_GAME , low_priority)]
 unsafe fn palutena_special_air_lw_reflect_game(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
@@ -150,13 +189,53 @@ unsafe fn palutena_special_air_lw_reflect_game(fighter: &mut L2CAgentBase) {
 
 }
 
+#[acmd_script( agent = "palutena", script = "effect_specialairlwreflect" , category = ACMD_EFFECT , low_priority)]
+unsafe fn palutena_special_air_lw_reflect_effect(fighter: &mut L2CAgentBase) {
+    let lua_state = fighter.lua_state_agent;
+    let boma = fighter.boma();
+    frame(lua_state, 1.0);
+    if is_excute(fighter) {
+        if VarModule::is_flag(fighter.battle_object, vars::palutena::SPECIAL_LW_AEGIS_REFLECTOR) {
+            EFFECT_FOLLOW(fighter, Hash40::new("palutena_wand_light_trace"), Hash40::new("stick"), 0, 8.65, 0, 0, 0, 0, 1, true);
+            EffectModule::enable_sync_init_pos_last(boma);
+            EFFECT_FOLLOW(fighter, Hash40::new("palutena_wand_light2"), Hash40::new("stick"), 0, 8.65, 0, 0, 0, 0, 1, true);
+        }
+    }
+    frame(lua_state, 8.0);
+    if is_excute(fighter) {
+        if VarModule::is_flag(fighter.battle_object, vars::palutena::SPECIAL_LW_AEGIS_REFLECTOR) {
+            EFFECT(fighter, Hash40::new("palutena_throw_twinkle"), Hash40::new("top"), 0.0, 16.0, -8.0, 0, 0, 0, 0.95, 0, 0, 0, 0, 0, 0, true);
+        }
+        else{
+            EFFECT(fighter, Hash40::new("palutena_mirror"), Hash40::new("top"), 12.0, 12.0, 0, 0, 22.5, 0, 0.8, 0, 0, 0, 0, 0, 0, false);
+            EFFECT_DETACH_KIND(fighter, Hash40::new("palutena_backlight"), -1);
+        }
+    }
+    frame(lua_state, 10.0);
+    if is_excute(fighter) {
+        if VarModule::is_flag(fighter.battle_object, vars::palutena::SPECIAL_LW_AEGIS_REFLECTOR) {
+            EFFECT_FOLLOW_ALPHA(fighter, Hash40::new("palutena_backlight"), Hash40::new("top"), -1, 21, 1, 0, 90, 0, 1, true, 0.7);
+        }
+    }
+    frame(lua_state, 35.0);
+    if is_excute(fighter) {
+        if VarModule::is_flag(fighter.battle_object, vars::palutena::SPECIAL_LW_AEGIS_REFLECTOR) {
+            EFFECT_OFF_KIND(fighter, Hash40::new("palutena_wand_light_trace"), false, false);
+            EFFECT_OFF_KIND(fighter, Hash40::new("palutena_wand_light2"), false, false);
+        }
+    }
+
+}
+
 
 pub fn install() {
     install_acmd_scripts!(
         palutena_special_lw_game,
         palutena_special_air_lw_game,
         palutena_special_lw_reflect_game,
+        palutena_special_lw_reflect_effect,
         palutena_special_air_lw_reflect_game,
+        palutena_special_air_lw_reflect_effect,
     );
 }
 
