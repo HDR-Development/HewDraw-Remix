e12c6bab4e031f0f5d2d6eb8958ffaf1a4a8a3a6
TeamPearson <99865721+TeamPearson@users.noreply.github.com>
2022-03-17 14:32:22 -0500

    Update specials.rs

--- fighters/koopa/src/acmd/specials.rs
+++ fighters/koopa/src/acmd/specials.rs
@@ -85,6 +85,7 @@ unsafe fn koopa_special_air_hi_game(fighter: &mut L2CAgentBase) {
         wait(lua_state, 4.0);
     }
     if is_excute(fighter) {
+        AttackModule::clear_all(boma);
         ATTACK(fighter, 0, 0, Hash40::new("top"), 2.0, 60, 180, 0, 50, 5.5, 0.0, 9.0, 4.0, Some(0.0), Some(9.0), Some(-4.0), 1.25, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_BODY);
     }
     wait(lua_state, 4.0);
