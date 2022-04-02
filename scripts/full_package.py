a632ea2a7de5cb8069f15874c10658c171d0203e
techyCoder81 <mtklein98@gmail.com>
2022-04-02 19:38:01 -0400

    Update full_package.py

--- scripts/full_package.py
+++ scripts/full_package.py
@@ -53,11 +53,11 @@ download_and_extract("skyline-dev", "skyline", "beta", "skyline.zip", "/atmosphe
 
 print("getting libnro_hook.nro")
 urllib.request.urlretrieve("https://github.com/ultimate-research/nro-hook-plugin/releases/download/beta/libnro_hook.nro", "libnro_hook.nro")
-shutil.move("libnro_hook.nro", "package/atmosphere/contents/01006A800016E000/romfs/skyline/plugins/")
+shutil.move("libnro_hook.nro", "switch-package/atmosphere/contents/01006A800016E000/romfs/skyline/plugins/")
 
 print("getting libsmashline_hook.nro")
 urllib.request.urlretrieve("https://github.com/blu-dev/smashline_hook/releases/download/1.2.0/libsmashline_hook.nro", "libsmashline_hook.nro")
-shutil.move("libsmashline_hook.nro", "package/atmosphere/contents/01006A800016E000/romfs/skyline/plugins/")
+shutil.move("libsmashline_hook.nro", "switch-package/atmosphere/contents/01006A800016E000/romfs/skyline/plugins/")
 
 #print("getting libsmashline_hook_development.nro")
 #urllib.request.urlretrieve("https://github.com/blu-dev/smashline_hook/releases/download/1.1.1/libsmashline_hook_development.nro", "libsmashline_hook_development.nro")
