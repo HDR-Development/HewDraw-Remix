5544a557363e9370e2e4df65b1168ca1bc1881f6
blu-dev <the.blu.dev@gmail.com>
2022-04-08 17:26:02 -0700

    Add remaining up special scripts

--- fighters/elight/src/status.rs
+++ fighters/elight/src/status.rs
@@ -1,9 +1,13 @@
 use super::*;
 
-mod special_hi;
+mod special_hi_attack;
 mod special_hi_jump;
+mod special_hi_finish;
+mod special_hi;
 
 pub fn install() {
-    special_hi::install();
+    special_hi_attack::install();
     special_hi_jump::install();
+    special_hi_finish::install();
+    special_hi::install();
 }
