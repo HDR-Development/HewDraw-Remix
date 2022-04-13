48a467763f66e803ef9e08e43675c06d3100534f
WuBoytH <alexwuief@gmail.com>
2022-04-12 22:57:27 -0400

    Antenna Pikmin Indicator
    Resolves #476

--- fighters/pikmin/src/opff.rs
+++ fighters/pikmin/src/opff.rs
@@ -51,10 +51,62 @@ pub unsafe fn solimar_scaling(boma: &mut BattleObjectModuleAccessor, status_kind
     }
 }
 
+#[repr(C)]
+struct TroopManager {
+  _x0: u64,
+  max_pikmin_count: usize, // always 3
+  current_pikmin_count: usize,
+  pikmin_objects: *mut *mut BattleObject,
+  pikmin: [*mut BattleObject; 3],
+  // remainder that we don't care about
+  // funny blujay made this happen
+}
+
+unsafe fn pikmin_antenna_indicator(fighter: &mut L2CFighterCommon) {
+    if fighter.global_table[STATUS_KIND].get_i32() == *FIGHTER_STATUS_KIND_ENTRY
+    || sv_information::is_ready_go() {
+        let troops = WorkModule::get_int64(fighter.module_accessor, 0x100000C0);
+        // 0x100000C0 = FIGHTER_PIKMIN_INSTANCE_WORK_INT_TROOPS_MANAGER_ADDRESS
+        let troopmanager = troops as *const TroopManager;
+        let count = (*troopmanager).current_pikmin_count;
+        let pikmin;
+        let pikmin_id;
+        if count > 0 {
+            pikmin = (*troopmanager).pikmin[0];
+            pikmin_id = (*pikmin).battle_object_id;
+        }
+        else {
+            pikmin = std::ptr::null_mut();
+            pikmin_id = *BATTLE_OBJECT_ID_INVALID as u32;
+        }
+        let antenna_eff = WorkModule::get_int(fighter.module_accessor, 0x100000C4) as u32;
+        // 0x100000C4 = FIGHTER_PIKMIN_INSTANCE_WORK_INT_PIKMIN_ANTENNA_EFFECT_HANDLE
+        if pikmin_id != *BATTLE_OBJECT_ID_INVALID as u32
+        && sv_battle_object::is_active(pikmin_id) {
+            let variation = WorkModule::get_int((*pikmin).module_accessor, *WEAPON_PIKMIN_PIKMIN_INSTANCE_WORK_ID_INT_VARIATION);
+            let color = match variation {
+                0 => Vector3f{x: 1.0, y: 0.2, z: 0.2}, // Red
+                1 => Vector3f{x: 1.0, y: 1.0, z: 0.2}, // Yellow
+                2 => Vector3f{x: 0.2, y: 0.2, z: 1.0}, // Blue
+                3 => Vector3f{x: 0.8, y: 0.8, z: 0.8}, // White
+                _ => Vector3f{x: 0.4, y: 0.2, z: 0.8}  // Purple
+            };
+            EffectModule::set_alpha(fighter.module_accessor, antenna_eff, 1.0);
+            EffectModule::set_rgb(fighter.module_accessor, antenna_eff, color.x, color.y, color.z);
+        }
+        else {
+            // No Pikmin, make it transparent and grey.
+            EffectModule::set_rgb(fighter.module_accessor, antenna_eff, 0.8, 0.8, 0.8);
+            EffectModule::set_alpha(fighter.module_accessor, antenna_eff, 0.05);
+        }
+    }
+}
+
 pub unsafe fn moveset(fighter: &mut L2CFighterCommon, boma: &mut BattleObjectModuleAccessor, id: usize, cat: [i32 ; 4], status_kind: i32, situation_kind: i32, motion_kind: u64, stick_x: f32, stick_y: f32, facing: f32, frame: f32) {
     winged_pikmin_cancel(boma, status_kind, cat[0]);
     pikmin_order_b_reverse(fighter);
     solimar_scaling(boma, status_kind, frame);
+    pikmin_antenna_indicator(fighter);
 }
 
 #[utils::macros::opff(FIGHTER_KIND_PIKMIN )]
