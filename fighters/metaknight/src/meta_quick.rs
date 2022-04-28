8cc29cfa8d42a11bc8cea762a2fe95c5309ac89d
techyCoder81 <mtklein98@gmail.com>
2022-04-28 18:59:23 -0400

    meta quick no sound effect unless you arent entering taunt

--- fighters/metaknight/src/meta_quick.rs
+++ fighters/metaknight/src/meta_quick.rs
@@ -7,20 +7,20 @@ pub unsafe fn run(fighter: &mut smash::lua2cpp::L2CFighterCommon) {
     // update MeterModule
     MeterModule::update(fighter.object(), false);
     MeterModule::watch_damage(fighter.object(), true);
-    MeterModule::set_damage_gain_mul(fighter.object(), 5.0);
+    MeterModule::set_damage_gain_mul(fighter.object(), 6.0);
     
-    println!("Meter Module: {}", MeterModule::meter(fighter.object()));
-    println!("Gimmick Timer: {}", VarModule::get_int(fighter.object(), vars::common::GIMMICK_TIMER));
+    //println!("Meter Module: {}", MeterModule::meter(fighter.object()));
+    //println!("Gimmick Timer: {}", VarModule::get_int(fighter.object(), vars::common::GIMMICK_TIMER));
     
     // if we have full meter, enable meta quick
     if MeterModule::level(fighter.object()) >= 10 {
         if !fighter.is_status_one_of(&[*FIGHTER_STATUS_KIND_GUARD, *FIGHTER_STATUS_KIND_GUARD_ON])
             && fighter.is_cat_flag(Cat2::AppealAll) {
 
-            MeterModule::drain(fighter.object(), 10);
+            MeterModule::drain(fighter.object(), 1);
             
-            // 10 seconds of quick
-            start_meta_quick(fighter, 600);
+            // 8 seconds of quick per 50 damage
+            start_meta_quick(fighter, 8 * 60);
         } else {
             show_quick_ready_flash(fighter);
         }
@@ -28,44 +28,70 @@ pub unsafe fn run(fighter: &mut smash::lua2cpp::L2CFighterCommon) {
         ColorBlendModule::cancel_main_color(fighter.boma(), 0);
     }
 
+    // during meta quick, if you deal damage, it should extend meta quick
+    if is_meta_quick(fighter) && MeterModule::level(fighter.object()) >= 1 {
+        MeterModule::drain(fighter.object(), 1);
+        // an additional 0.5 seconds of quick per 5 damage dealt
+        VarModule::add_int(fighter.object(), vars::common::GIMMICK_TIMER, 30);
+    }
+    
+    // state machine which handles the speed/accel aspects of meta quick
+    // at the beginning of the status, 
+    if fighter.global_table[CURRENT_FRAME].get_i32() <= 2 && !VarModule::is_flag(fighter.object(), vars::metaknight::COMPLETED_SET_SPEEDS) {
+        
+        //println!("we are setting speeds!");
+
+        if is_meta_quick(fighter) {
+            
+            // set the X motion speed multiplier (where movement is baked into an anim)
+            lua_bind::FighterKineticEnergyMotion::set_speed_mul(fighter.get_motion_energy(), 1.2);
+
+            // set the X motion accel multiplier for control energy (used in the air, during walk, fall, etc)
+            lua_bind::FighterKineticEnergyController::mul_x_accel_mul( fighter.get_controller_energy(), 1.3);
+
+            // set the X motion accel multiplier for control energy (used in the air, during walk, fall, etc)
+            lua_bind::FighterKineticEnergyController::mul_x_accel_add( fighter.get_controller_energy(), 1.3);
+
+            // set the X speed max multiplier for control energy (used in the air, during walk, fall, etc)
+            lua_bind::FighterKineticEnergyController::mul_x_speed_max(fighter.get_controller_energy(), 1.3);
+
+        } else {
+            // metaknight has slightly lowered stats when not in meta quick
+
+            // set the X motion speed multiplier (where movement is baked into an anim)
+            lua_bind::FighterKineticEnergyMotion::set_speed_mul(fighter.get_motion_energy(), 0.5);
+
+            // set the X motion accel multiplier for control energy (used in the air, during walk, fall, etc)
+            lua_bind::FighterKineticEnergyController::mul_x_accel_mul( fighter.get_controller_energy(), 0.5);
+
+            // set the X motion accel multiplier for control energy (used in the air, during walk, fall, etc)
+            lua_bind::FighterKineticEnergyController::mul_x_accel_add( fighter.get_controller_energy(), 0.5);
+
+            // set the X speed max multiplier for control energy (used in the air, during walk, fall, etc)
+            lua_bind::FighterKineticEnergyController::mul_x_speed_max(fighter.get_controller_energy(), 0.5);
+        }
+        // we no longer need to set these values
+        VarModule::on_flag(fighter.object(), vars::metaknight::COMPLETED_SET_SPEEDS);
+    }
+    if fighter.global_table[CURRENT_FRAME].get_i32() > 3
+      || StatusModule::is_changing(fighter.boma()){
+        VarModule::off_flag(fighter.object(), vars::metaknight::COMPLETED_SET_SPEEDS)
+    }
+    
     if is_meta_quick(fighter) {
         update_meta_quick_timer(fighter);
         show_quick_active_effect(fighter);
 
-        // state machine which handles the speed/accel aspects of meta quick
-        // at the beginning of the status, 
-        if fighter.global_table[CURRENT_FRAME].get_i32() <= 2 && !VarModule::is_flag(fighter.object(), vars::metaknight::COMPLETED_SET_SPEEDS) {
-            
-            println!("we are setting speeds!");
-            // set the X motion speed multiplier (where movement is baked into an anim)
-            lua_bind::FighterKineticEnergyMotion::set_speed_mul(fighter.get_motion_energy(), 1.3);
-
-            // set the X motion accel multiplier for control energy (used in the air, during walk, fall, etc)
-            lua_bind::FighterKineticEnergyController::mul_x_accel_mul( fighter.get_controller_energy(), 1.5);
-
-            // set the X motion accel multiplier for control energy (used in the air, during walk, fall, etc)
-            lua_bind::FighterKineticEnergyController::mul_x_accel_add( fighter.get_controller_energy(), 1.5);
-
-            // set the X speed max multiplier for control energy (used in the air, during walk, fall, etc)
-            lua_bind::FighterKineticEnergyController::mul_x_speed_max(fighter.get_controller_energy(), 1.5);
-
-            // we no longer need to set these values
-            VarModule::on_flag(fighter.object(), vars::metaknight::COMPLETED_SET_SPEEDS);
-        }
-        if fighter.global_table[CURRENT_FRAME].get_i32() > 3 {
-            VarModule::off_flag(fighter.object(), vars::metaknight::COMPLETED_SET_SPEEDS)
-        }
-        
         // set the increased jump speed max multiplier for momentum transfer
         VarModule::set_float(fighter.object(), vars::common::JUMP_SPEED_MAX_MUL, 1.5);
-
     } else {
         kill_quick_effect(fighter);
-
+    
         // set the regular jump speed max multiplier for momentum transfer
         VarModule::set_float(fighter.object(), vars::common::JUMP_SPEED_MAX_MUL, 1.0);
     }
-    println!("Jump speed max mul: {}", VarModule::get_float(fighter.object(), vars::common::JUMP_SPEED_MAX_MUL));
+
+    //println!("Jump speed max mul: {}", VarModule::get_float(fighter.object(), vars::common::JUMP_SPEED_MAX_MUL));
 }
 
 /// decrement meta quick timer
@@ -90,8 +116,11 @@ pub unsafe fn is_meta_quick(fighter: &mut smash::lua2cpp::L2CFighterCommon) -> b
 /// length: how many frames meta quick should be active
 unsafe fn start_meta_quick(fighter: &mut smash::lua2cpp::L2CFighterCommon, length: i32) {
     VarModule::set_int(fighter.object(), vars::common::GIMMICK_TIMER, length);
-    SoundModule::stop_all_sound(fighter.boma());
-    PLAY_SE(fighter, Hash40::new("vc_metaknight_appeal01"));
+
+    // only play sfx if you aren't about to get a taunt instead
+    if !(fighter.is_situation(*SITUATION_KIND_GROUND) && CancelModule::is_enable_cancel(fighter.boma())) {
+        PLAY_SE(fighter, Hash40::new("vc_metaknight_appeal01"));
+    }
 }
 
 
@@ -137,7 +166,7 @@ unsafe fn show_quick_active_effect(fighter: &mut smash::lua2cpp::L2CFighterCommo
 /// handle flashing to indicate that meta quick is available (similar to waft vfx)
 unsafe fn show_quick_ready_flash(fighter: &mut smash::lua2cpp::L2CFighterCommon) {
     let flash_timer = VarModule::get_int(fighter.object(), vars::metaknight::META_QUICK_READY_FLASH_TIMER);
-    println!("Quick Ready Flash Timer: {}", VarModule::get_int(fighter.object(), vars::metaknight::META_QUICK_READY_FLASH_TIMER));
+    //println!("Quick Ready Flash Timer: {}", VarModule::get_int(fighter.object(), vars::metaknight::META_QUICK_READY_FLASH_TIMER));
 
     VarModule::inc_int(fighter.object(), vars::metaknight::META_QUICK_READY_FLASH_TIMER);
     match flash_timer {
@@ -152,22 +181,6 @@ unsafe fn show_quick_ready_flash(fighter: &mut smash::lua2cpp::L2CFighterCommon)
         11 => {
             ColorBlendModule::cancel_main_color(fighter.boma(), 0);
         }
-        /*
-        10..=19 => {
-            let cmb_vec1 = Vector4f{x: 0.5, y: 0.5, z: 0.5, w: 0.9};
-            let cmb_vec2 = Vector4f{x: 0.5, y: 0.5, z: 0.5, w: 0.9};
-            ColorBlendModule::set_main_color(fighter.boma(), &cmb_vec1, &cmb_vec2, 1.0, 1.0, 10, true);
-        },
-        20..=29 => {
-            let cmb_vec1 = Vector4f{x: 0.8, y: 0.8, z: 0.8, w: 0.9};
-            let cmb_vec2 = Vector4f{x: 0.8, y: 0.8, z: 0.8, w: 0.9};
-            ColorBlendModule::set_main_color(fighter.boma(), &cmb_vec1, &cmb_vec2, 1.0, 1.0, 10, true);
-        },
-        30..=39 => {
-            let cmb_vec1 = Vector4f{x: 0.5, y: 0.5, z: 0.5, w: 0.9};
-            let cmb_vec2 = Vector4f{x: 0.5, y: 0.5, z: 0.5, w: 0.9};
-            ColorBlendModule::set_main_color(fighter.boma(), &cmb_vec1, &cmb_vec2, 1.0, 1.0, 10, true);
-        },*/
         12..=49 => {},
         _ => {
             VarModule::set_int(fighter.object(), vars::metaknight::META_QUICK_READY_FLASH_TIMER, 0);
