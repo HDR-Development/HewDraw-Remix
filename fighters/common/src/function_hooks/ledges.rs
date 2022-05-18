7b5afd32b0a022ca772cd4e23221a149a51f624b
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2022-05-18 12:43:54 -0400

    PM PK Rocket behavior
    no more janky wallrides! no more bonking!

--- fighters/common/src/function_hooks/ledges.rs
+++ fighters/common/src/function_hooks/ledges.rs
@@ -219,12 +219,15 @@ unsafe fn check_cliff_entry_specializer(boma: &mut BattleObjectModuleAccessor) -
 
     if fighter_kind == *FIGHTER_KIND_NESS {
         if status_kind == *FIGHTER_NESS_STATUS_KIND_SPECIAL_HI_ATTACK {
+            /*
             if frame > 5.0 && frame < 15.0 {
                 return 1;
             }
             else{
                 return -1;
             }
+            */
+            return 1;
         }
     }
 
