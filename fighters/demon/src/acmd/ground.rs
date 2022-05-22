216fd3124c9281ba72d0fc6705e2aea5253a4f7a
black-calculus <52509873+black-calculus@users.noreply.github.com>
2022-05-21 21:56:16 -0700

    Decrease base hitstun on jab 1

--- fighters/demon/src/acmd/ground.rs
+++ fighters/demon/src/acmd/ground.rs
@@ -18,11 +18,11 @@ unsafe fn game_attack11(fighter: &mut L2CAgentBase) {
         ATTACK(fighter, 3, 0, Hash40::new("top"), 3.0, 361, 10, 0, 30, 3.2, 0.0, 11.0, 8.75, Some(0.0), Some(13.5), Some(8.75), 0.5, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new_raw(0x193bdcb0cc), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_DEMON_PUNCH01, *ATTACK_REGION_PUNCH);
         ATTACK(fighter, 4, 0, Hash40::new("top"), 3.0, 361, 10, 0, 30, 3.2, 0.0, 11.0, 3.25, Some(0.0), Some(13.5), Some(3.25), 0.5, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new_raw(0x193bdcb0cc), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_DEMON_PUNCH01, *ATTACK_REGION_PUNCH);
         ATTACK(fighter, 5, 0, Hash40::new("top"), 3.0, 33, 15, 0, 35, 3.6, 0.0, 13.2, 3.25, Some(0.0), Some(13.2), Some(9.0), 0.5, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_A, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new_raw(0x193bdcb0cc), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_DEMON_PUNCH01, *ATTACK_REGION_PUNCH);
-        AttackModule::set_add_reaction_frame_revised(boma, 0, 8.0, false);
-        AttackModule::set_add_reaction_frame_revised(boma, 1, 8.0, false);
-        AttackModule::set_add_reaction_frame_revised(boma, 2, 8.0, false);
-        AttackModule::set_add_reaction_frame_revised(boma, 3, 8.0, false);
-        AttackModule::set_add_reaction_frame_revised(boma, 4, 8.0, false);
+        AttackModule::set_add_reaction_frame_revised(boma, 0, 4.0, false);
+        AttackModule::set_add_reaction_frame_revised(boma, 1, 4.0, false);
+        AttackModule::set_add_reaction_frame_revised(boma, 2, 4.0, false);
+        AttackModule::set_add_reaction_frame_revised(boma, 3, 4.0, false);
+        AttackModule::set_add_reaction_frame_revised(boma, 4, 4.0, false);
         ATK_SET_SHIELD_SETOFF_MUL(fighter, 0, 1.3);
         ATK_SET_SHIELD_SETOFF_MUL(fighter, 1, 1.3);
         ATK_SET_SHIELD_SETOFF_MUL(fighter, 2, 1.3);
