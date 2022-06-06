266cd74322f161a8014fca5a925b54fae71f5734
JoaoAlmeida20 <redstone113@gmail.com>
2022-06-06 01:28:02 +0100

    fix compile error due to static arrays of deref'd LuaConsts

--- fighters/common/src/opff/var_resets.rs
+++ fighters/common/src/opff/var_resets.rs
@@ -9,29 +9,28 @@ use smash::app::lua_bind::*;
 use smash::lib::lua_const::*;
 use smash::hash40;
 
-const DAMAGE_STATUSES : &'static [i32] = &[*FIGHTER_STATUS_KIND_DEAD,
-                                            *FIGHTER_STATUS_KIND_REBIRTH,
-                                            *FIGHTER_STATUS_KIND_WIN,
-                                            *FIGHTER_STATUS_KIND_LOSE,
-                                            *FIGHTER_STATUS_KIND_ENTRY];
+unsafe fn var_resets(boma: &mut BattleObjectModuleAccessor) {
+    let damage_statuses = &[*FIGHTER_STATUS_KIND_DEAD,
+                                        *FIGHTER_STATUS_KIND_REBIRTH,
+                                        *FIGHTER_STATUS_KIND_WIN,
+                                        *FIGHTER_STATUS_KIND_LOSE,
+                                        *FIGHTER_STATUS_KIND_ENTRY];
 
-const DEATH_STATUSES : &'static [i32] = &[*FIGHTER_STATUS_KIND_DAMAGE,
-                                            *FIGHTER_STATUS_KIND_DAMAGE_AIR,
-                                            *FIGHTER_STATUS_KIND_DAMAGE_FLY,
-                                            *FIGHTER_STATUS_KIND_DAMAGE_FLY_ROLL,
-                                            *FIGHTER_STATUS_KIND_DAMAGE_FLY_METEOR,
-                                            *FIGHTER_STATUS_KIND_DAMAGE_FLY_REFLECT_LR,
-                                            *FIGHTER_STATUS_KIND_DAMAGE_FLY_REFLECT_U,
-                                            *FIGHTER_STATUS_KIND_DAMAGE_FLY_REFLECT_D,
-                                            *FIGHTER_STATUS_KIND_DAMAGE_FALL];
+    let death_statuses = &[*FIGHTER_STATUS_KIND_DAMAGE,
+                                        *FIGHTER_STATUS_KIND_DAMAGE_AIR,
+                                        *FIGHTER_STATUS_KIND_DAMAGE_FLY,
+                                        *FIGHTER_STATUS_KIND_DAMAGE_FLY_ROLL,
+                                        *FIGHTER_STATUS_KIND_DAMAGE_FLY_METEOR,
+                                        *FIGHTER_STATUS_KIND_DAMAGE_FLY_REFLECT_LR,
+                                        *FIGHTER_STATUS_KIND_DAMAGE_FLY_REFLECT_U,
+                                        *FIGHTER_STATUS_KIND_DAMAGE_FLY_REFLECT_D,
+                                        *FIGHTER_STATUS_KIND_DAMAGE_FALL];
 
-unsafe fn special_cancel_flag_reset(boma: &mut BattleObjectModuleAccessor) {
-    
     // Up Special Cancel
     if VarModule::is_flag(boma.object(), vars::common::UP_SPECIAL_CANCEL) {
         if !boma.is_situation(*SITUATION_KIND_AIR)
-        || boma.is_status_one_of(DAMAGE_STATUSES)
-        || boma.is_status_one_of(DEATH_STATUSES) {
+        || boma.is_status_one_of(damage_statuses)
+        || boma.is_status_one_of(death_statuses) {
             VarModule::off_flag(boma.object(), vars::common::UP_SPECIAL_CANCEL);
         }
     }
@@ -39,8 +38,8 @@ unsafe fn special_cancel_flag_reset(boma: &mut BattleObjectModuleAccessor) {
     // Side Special Cancel
     if VarModule::is_flag(boma.object(), vars::common::SIDE_SPECIAL_CANCEL) {
         if !boma.is_situation(*SITUATION_KIND_AIR)
-        || boma.is_status_one_of(DAMAGE_STATUSES)
-        || boma.is_status_one_of(DEATH_STATUSES) {
+        || boma.is_status_one_of(damage_statuses)
+        || boma.is_status_one_of(death_statuses) {
             VarModule::off_flag(boma.object(), vars::common::SIDE_SPECIAL_CANCEL);
         }
     }
@@ -48,15 +47,15 @@ unsafe fn special_cancel_flag_reset(boma: &mut BattleObjectModuleAccessor) {
     // Side Special Cancel (doesn't reset on hit)
     if VarModule::is_flag(boma.object(), vars::common::SIDE_SPECIAL_CANCEL_NO_HIT)
     && (!boma.is_situation(*SITUATION_KIND_AIR)
-    || boma.is_status_one_of(DEATH_STATUSES)) {
+    || boma.is_status_one_of(death_statuses)) {
         VarModule::off_flag(boma.object(), vars::common::SIDE_SPECIAL_CANCEL_NO_HIT);
     }
 
     // Aerial Special Used
     if VarModule::is_flag(boma.object(), vars::common::AIR_SPECIAL_USED) {
         if !boma.is_situation(*SITUATION_KIND_AIR)
-        || boma.is_status_one_of(DAMAGE_STATUSES)
-        || boma.is_status_one_of(DEATH_STATUSES) {
+        || boma.is_status_one_of(damage_statuses)
+        || boma.is_status_one_of(death_statuses) {
             VarModule::off_flag(boma.object(), vars::common::AIR_SPECIAL_USED);
         }
     }
@@ -64,7 +63,7 @@ unsafe fn special_cancel_flag_reset(boma: &mut BattleObjectModuleAccessor) {
     // Up Special Wall Jump
     if VarModule::is_flag(boma.object(), vars::common::SPECIAL_WALL_JUMP) {
         if !boma.is_situation(*SITUATION_KIND_AIR)
-        || boma.is_status_one_of(DEATH_STATUSES) {
+        || boma.is_status_one_of(death_statuses) {
             VarModule::off_flag(boma.object(), vars::common::SPECIAL_WALL_JUMP);
         }
     }
@@ -72,8 +71,8 @@ unsafe fn special_cancel_flag_reset(boma: &mut BattleObjectModuleAccessor) {
     // Up Special Interrupt
     if VarModule::is_flag(boma.object(), vars::common::UP_SPECIAL_INTERRUPT) {
         if !boma.is_situation(*SITUATION_KIND_AIR)
-        || boma.is_status_one_of(DAMAGE_STATUSES)
-        || boma.is_status_one_of(DEATH_STATUSES) {
+        || boma.is_status_one_of(damage_statuses)
+        || boma.is_status_one_of(death_statuses) {
             VarModule::off_flag(boma.object(), vars::common::UP_SPECIAL_INTERRUPT);
         }
     }
@@ -81,30 +80,26 @@ unsafe fn special_cancel_flag_reset(boma: &mut BattleObjectModuleAccessor) {
     // Up Special Intterupt Airtime
     if VarModule::is_flag(boma.object(), vars::common::UP_SPECIAL_INTERRUPT_AIRTIME) {
         if !boma.is_situation(*SITUATION_KIND_AIR)
-        || boma.is_status_one_of(DAMAGE_STATUSES)
-        || boma.is_status_one_of(DEATH_STATUSES) {
+        || boma.is_status_one_of(damage_statuses)
+        || boma.is_status_one_of(death_statuses) {
             VarModule::off_flag(boma.object(), vars::common::UP_SPECIAL_INTERRUPT_AIRTIME);
         }
     }
-}
 
-unsafe fn special_motion_reset(boma: &mut BattleObjectModuleAccessor) {
+    // Special Motion Reset
     if !boma.is_situation(*SITUATION_KIND_AIR)
-    || boma.is_status_one_of(DEATH_STATUSES) {
+    || boma.is_status_one_of(death_statuses) {
         VarModule::off_flag(boma.object(), vars::common::SPECIAL_STALL);
         VarModule::off_flag(boma.object(), vars::common::SPECIAL_STALL_USED);
     }
-}
 
-unsafe fn aerial_glide_toss_reset(boma: &mut BattleObjectModuleAccessor) {
+    // Aerial Glide Toss Reset
     if !boma.is_situation(*SITUATION_KIND_AIR)
-    || boma.is_status_one_of(DEATH_STATUSES) {
+    || boma.is_status_one_of(death_statuses) {
         VarModule::set_int(boma.object(), vars::common::AGT_USED_COUNTER, 0);
     }
 }
 
 pub unsafe fn run(boma: &mut BattleObjectModuleAccessor, cat: [i32 ; 4], status_kind: i32, situation_kind: i32, fighter_kind: i32, stick_x: f32, stick_y: f32, facing: f32) {
-    special_cancel_flag_reset(boma);
-    special_motion_reset(boma);
-    aerial_glide_toss_reset(boma);
+    var_resets(boma);
 }
