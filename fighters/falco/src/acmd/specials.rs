19db7e4c3c82dd1f544c0d888b04d9fba57c730d
Mad Villain <101300494+MadVillainLordQ@users.noreply.github.com>
2022-06-12 13:16:32 -0400

    Shine Shield Frame Advantage/Hitbox Corrections

--- fighters/falco/src/acmd/specials.rs
+++ fighters/falco/src/acmd/specials.rs
@@ -157,14 +157,9 @@ unsafe fn falco_special_air_lw_game(fighter: &mut L2CAgentBase) {
     frame(lua_state, 3.0);
     if is_excute(fighter) {
         ATTACK(fighter, 0, 0, Hash40::new("top"), 8.0, 84, 48, 0, 103, 7.5, 0.0, 8.0, -1.0, None, None, None, 0.6, 1.15, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_ENERGY);
-        ATK_SET_SHIELD_SETOFF_MUL(fighter, 0, 0.7);
+        ATK_SET_SHIELD_SETOFF_MUL(fighter, 0, 0.64);
     }
     frame(lua_state, 4.0);
-    if is_excute(fighter) {
-        ATTACK(fighter, 0, 0, Hash40::new("top"), 8.0, 84, 48, 0, 103, 7.5, 0.0, 8.0, -4.0, None, None, None, 0.6, 1.15, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_ENERGY);
-        ATK_SET_SHIELD_SETOFF_MUL(fighter, 0, 0.7);
-    }
-    frame(lua_state, 5.0);
     if is_excute(fighter) {
         AttackModule::clear_all(boma);
     }
@@ -184,14 +179,9 @@ unsafe fn falco_special_lw_game(fighter: &mut L2CAgentBase) {
     frame(lua_state, 3.0);
     if is_excute(fighter) {
         ATTACK(fighter, 0, 0, Hash40::new("top"), 8.0, 84, 48, 0, 103, 7.5, 0.0, 8.0, -1.0, None, None, None, 0.6, 1.15, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_ENERGY);
-        ATK_SET_SHIELD_SETOFF_MUL(fighter, 0, 0.7);
+        ATK_SET_SHIELD_SETOFF_MUL(fighter, 0, 0.64);
     }
     frame(lua_state, 4.0);
-    if is_excute(fighter) {
-        ATTACK(fighter, 0, 0, Hash40::new("top"), 8.0, 84, 48, 0, 103, 7.5, 0.0, 8.0, -4.0, None, None, None, 0.6, 1.15, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_ENERGY);
-        ATK_SET_SHIELD_SETOFF_MUL(fighter, 0, 0.7);
-    }
-    frame(lua_state, 5.0);
     if is_excute(fighter) {
         AttackModule::clear_all(boma);
     }
