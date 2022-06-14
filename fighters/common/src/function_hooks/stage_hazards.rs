327c2402e445addb00e81979146ce03b5b676118
techyCoder81 <mtklein98@gmail.com>
2022-06-14 14:30:18 -0400

    Update stage_hazards.rs

--- fighters/common/src/function_hooks/stage_hazards.rs
+++ fighters/common/src/function_hooks/stage_hazards.rs
@@ -40,6 +40,7 @@ static HAZARDLESS_STAGE_IDS: &[u32] = &[
     0x68, // wario ware,
     0x6e, // halberd
     0x77, // summit
+    0xcb, // find mii (StreetPass)
     0xb9, // reset bomb forest
     0xec, // skyloft,
     0x107, // wrecking crew
