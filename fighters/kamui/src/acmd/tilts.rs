234ae03d11d1d1bf80e16adcc2ba302abbecc43f
TeamPearson <99865721+TeamPearson@users.noreply.github.com>
2022-06-23 10:51:38 -0500

    Update tilts.rs

--- fighters/kamui/src/acmd/tilts.rs
+++ fighters/kamui/src/acmd/tilts.rs
@@ -45,7 +45,7 @@ unsafe fn kamui_attack_hi3_game(fighter: &mut L2CAgentBase) {
         ATTACK(fighter, 3, 0, Hash40::new("top"), 9.0, 88, 53, 0, 65, 5.0, 0.0, 12.0, -3.0, Some(0.0), Some(12.0), Some(-6.0), 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
         AttackModule::set_attack_height_all(boma, app::AttackHeight(*ATTACK_HEIGHT_HIGH), false);
     }
-    wait(lua_state, 4.0);
+    wait(lua_state, 8.0);
     if is_excute(fighter) {
         AttackModule::clear_all(boma);
     }
