855ce129aeb56b3fd020edee8db6685bb193c830
unknown <jn383@cornell.edu>
2022-07-02 14:38:47 -0400

    discharge kb/recoil change and electroweb bugfix

--- fighters/pichu/src/acmd/aerials.rs
+++ fighters/pichu/src/acmd/aerials.rs
@@ -165,7 +165,7 @@ unsafe fn pichu_attack_air_lw_game(fighter: &mut L2CAgentBase) {
         FT_ADD_DAMAGE(fighter, 2.0*(VarModule::get_float(boma.object(), vars::pichu::instance::CHARGE_RECOIL_MUL)));
         HIT_NODE(fighter, Hash40::new("mimir1"), *HIT_STATUS_XLU);
         HIT_NODE(fighter, Hash40::new("mimil1"), *HIT_STATUS_XLU);
-        ATTACK(fighter, 0, 0, Hash40::new("hip"), (12.0), 361, 110, 0, 40, 4.5, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_HEAD);
+        ATTACK(fighter, 0, 0, Hash40::new("hip"), (12.0*(VarModule::get_float(boma.object(), vars::pichu::instance::CHARGE_DAMAGE_MUL))), 361, 110, 0, 40, 4.5, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_HEAD);
         ATTACK(fighter, 1, 0, Hash40::new("neck"), (13.0*(VarModule::get_float(boma.object(), vars::pichu::instance::CHARGE_DAMAGE_MUL))), 270, 86, 0, 16, 5.5, 4.5, -1.3, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_HEAD);
         
     }
