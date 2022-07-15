a6eb5085869fe4627be74d52e7511cc0f157a412
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2022-07-14 20:25:07 -0400

    remove 2-framing

--- fighters/common/src/general_statuses/cliff.rs
+++ fighters/common/src/general_statuses/cliff.rs
@@ -10,6 +10,7 @@ pub fn install() {
 fn nro_hook(info: &skyline::nro::NroInfo) {
     if info.name == "common" {
         skyline::install_hooks!(
+            status_CliffCatchMove,
             status_end_CliffCatchMove,
             status_end_CliffCatch,
             status_end_CliffWait,
@@ -23,10 +24,19 @@ fn nro_hook(info: &skyline::nro::NroInfo) {
     }
 }
 
+#[skyline::hook(replace = smash::lua2cpp::L2CFighterCommon_status_CliffCatchMove)]
+unsafe fn status_CliffCatchMove(fighter: &mut L2CFighterCommon) -> L2CValue {
+    if WorkModule::is_flag(fighter.module_accessor, *FIGHTER_INSTANCE_WORK_ID_FLAG_CLIFF_XLU) {
+        HitModule::set_whole(fighter.module_accessor, HitStatus(*HIT_STATUS_XLU), 0);
+    }
+    call_original!(fighter)
+}
+
 #[skyline::hook(replace = smash::lua2cpp::L2CFighterCommon_status_end_CliffCatchMove)]
 unsafe fn status_end_CliffCatchMove(fighter: &mut L2CFighterCommon) -> L2CValue {
     if StatusModule::status_kind_next(fighter.module_accessor) != *FIGHTER_STATUS_KIND_CLIFF_CATCH {
         VarModule::set_vec3(fighter.object(), vars::common::instance::LEDGE_POS, Vector3f {x: 0.0, y: 0.0, z: 0.0});
+        HitModule::set_xlu_frame_global(fighter.module_accessor, 0, 0);
     }
     call_original!(fighter)
 }
