a7a3a2be25c993a48ed80876df4e6b180a99fc1d
Mad Villain <101300494+MadVillainLordQ@users.noreply.github.com>
2022-07-20 17:34:30 -0400

    Simon Hitbox Adjustments

--- fighters/simon/src/opff.rs
+++ fighters/simon/src/opff.rs
@@ -17,44 +17,8 @@ unsafe fn holy_water_ac_b_rev(fighter: &mut L2CFighterCommon, boma: &mut BattleO
     }
 }
 
-// Simon Cross Fast Fall/Land Cancel
-unsafe fn cross_ff_land_cancel(boma: &mut BattleObjectModuleAccessor, id: usize, status_kind: i32, situation_kind: i32, cat2: i32, stick_y: f32) {
-    if [*FIGHTER_STATUS_KIND_SPECIAL_S, *FIGHTER_SIMON_STATUS_KIND_SPECIAL_S2].contains(&status_kind) {
-        if situation_kind == *SITUATION_KIND_AIR {
-            VarModule::on_flag(boma.object(), vars::simon::status::AIR_CROSS);
-            if boma.is_cat_flag(Cat2::FallJump)
-                && stick_y < -0.66
-                && KineticModule::get_sum_speed_y(boma, *FIGHTER_KINETIC_ENERGY_ID_GRAVITY) <= 0.0 {
-                WorkModule::set_flag(boma, true, *FIGHTER_STATUS_WORK_ID_FLAG_RESERVE_DIVE);
-            }
-        }
-    }
-}
-
-// Turn off air_cross flag if not in Cross in the air
-unsafe fn air_cross_air_off(boma: &mut BattleObjectModuleAccessor, id: usize, status_kind: i32, situation_kind: i32) {
-    if situation_kind == *SITUATION_KIND_AIR {
-        if ![*FIGHTER_STATUS_KIND_SPECIAL_S, *FIGHTER_SIMON_STATUS_KIND_SPECIAL_S2].contains(&status_kind) {
-            if VarModule::is_flag(boma.object(), vars::simon::status::AIR_CROSS) {
-                VarModule::off_flag(boma.object(), vars::simon::status::AIR_CROSS);
-            }
-        }
-    }
-}
-
-// Land cancel Cross if used in the air and fallen to the ground
-unsafe fn land_cancel_cross(boma: &mut BattleObjectModuleAccessor, id: usize, situation_kind: i32) {
-    if situation_kind == *SITUATION_KIND_GROUND && VarModule::is_flag(boma.object(), vars::simon::status::AIR_CROSS) {
-        VarModule::off_flag(boma.object(), vars::simon::status::AIR_CROSS);
-        StatusModule::change_status_request_from_script(boma, *FIGHTER_STATUS_KIND_LANDING, false);
-    }
-}
-
 pub unsafe fn moveset(fighter: &mut L2CFighterCommon, boma: &mut BattleObjectModuleAccessor, id: usize, cat: [i32 ; 4], status_kind: i32, situation_kind: i32, motion_kind: u64, stick_x: f32, stick_y: f32, facing: f32, frame: f32) {
     holy_water_ac_b_rev(fighter, boma, id, status_kind, situation_kind, cat[0], frame);
-    cross_ff_land_cancel(boma, id, status_kind, situation_kind, cat[1], stick_y);
-    air_cross_air_off(boma, id, status_kind, situation_kind);
-    land_cancel_cross(boma, id, situation_kind);
 }
 
 #[utils::macros::opff(FIGHTER_KIND_SIMON )]
