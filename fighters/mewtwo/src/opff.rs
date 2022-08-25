71d9c7e259acccdb1a5122ceb68a8f1602865336
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2022-08-24 21:15:38 -0400

    comments

--- fighters/mewtwo/src/opff.rs
+++ fighters/mewtwo/src/opff.rs
@@ -12,17 +12,19 @@ unsafe fn actionable_teleport_air(fighter: &mut L2CFighterCommon, boma: &mut Bat
             VarModule::on_flag(boma.object(), vars::mewtwo::instance::GROUNDED_TELEPORT);
         }
     }
-
+    // Allows M2 to turnaround based on stick position when reappearing
     if status_kind == *FIGHTER_MEWTWO_STATUS_KIND_SPECIAL_HI_2 && MotionModule::is_end(boma) {
         if boma.get_num_used_jumps() < boma.get_jump_count_max() {
             PostureModule::set_stick_lr(boma, 0.0);
             PostureModule::update_rot_y_lr(boma);
         }
     }
+    // Actionability when double jump isn't burned
     if status_kind == *FIGHTER_MEWTWO_STATUS_KIND_SPECIAL_HI_3 && situation_kind == *SITUATION_KIND_AIR && frame > 8.0 {
         if boma.get_num_used_jumps() < boma.get_jump_count_max() {
             VarModule::on_flag(boma.object(), vars::common::instance::UP_SPECIAL_CANCEL);
             CancelModule::enable_cancel(boma);
+            // Consume double jump, except when Teleport is initiated on ground
             if !VarModule::is_flag(boma.object(), vars::mewtwo::instance::GROUNDED_TELEPORT) {
                 WorkModule::inc_int(boma, *FIGHTER_INSTANCE_WORK_ID_INT_JUMP_COUNT);
             }
@@ -32,7 +34,7 @@ unsafe fn actionable_teleport_air(fighter: &mut L2CFighterCommon, boma: &mut Bat
 
 unsafe fn dj_upB_jump_refresh(fighter: &mut L2CFighterCommon) {
     if fighter.is_status(*FIGHTER_STATUS_KIND_JUMP_AERIAL) {
-        // if first 3 frames of dj
+        // If first 3 frames of dj
         if fighter.motion_frame() <= 2.0 {
             VarModule::on_flag(fighter.battle_object, vars::mewtwo::instance::UP_SPECIAL_JUMP_REFRESH);
         }
