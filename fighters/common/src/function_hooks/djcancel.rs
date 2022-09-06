6453a8473186cc351a4350e1f1f9821c3dee42ed
WuBoytH <alexwuief@gmail.com>
2022-09-06 09:05:08 -0400

    check for fighter or weapon

--- fighters/common/src/function_hooks/djcancel.rs
+++ fighters/common/src/function_hooks/djcancel.rs
@@ -8,15 +8,16 @@ use globals::*;
 //=================================================================
 #[skyline::hook(replace=KineticModule::change_kinetic)]
 unsafe fn change_kinetic_hook(boma: &mut BattleObjectModuleAccessor, kinetic_type: i32) -> i32 {
-    let mut kinetic_type_new = kinetic_type;
+    if boma.is_fighter() {
+        let mut kinetic_type_new = kinetic_type;
 
-    /*   --------------  SPACIE LASER MOMENTUM  -----------------  */
-    match crate::function_hooks::momentum_transfer::change_kinetic_momentum_related(boma, kinetic_type_new) {
-        Some(x) => kinetic_type_new = x,
-        None => ()
+        /*   --------------  SPACIE LASER MOMENTUM  -----------------  */
+        match crate::function_hooks::momentum_transfer::change_kinetic_momentum_related(boma, kinetic_type_new) {
+            Some(x) => kinetic_type_new = x,
+            None => ()
+        }
+        /*   --------------------------------------------------------  */    
     }
-    /*   --------------------------------------------------------  */
-
     original!()(boma, kinetic_type_new)
 }
 
