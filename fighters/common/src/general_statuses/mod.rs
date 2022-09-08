fd47764d2e9c86b46924bcd77f25867e20bc7cfe
Askew <sqk9gw@gmail.com>
2022-09-08 09:48:54 -0400

    Comment

--- fighters/common/src/general_statuses/mod.rs
+++ fighters/common/src/general_statuses/mod.rs
@@ -72,7 +72,7 @@ pub unsafe fn status_pre_DamageAir(fighter: &mut L2CFighterCommon) -> L2CValue {
     
     if VarModule::is_flag(fighter.battle_object, vars::common::instance::IS_KNOCKDOWN_THROW)
     || (degrees >= meteor_vector_min && degrees <= meteor_vector_max && DamageModule::reaction(fighter.module_accessor, 0) >= 65.0) {
-        println!("forced tumble");
+        //println!("forced tumble");
         StatusModule::set_status_kind_interrupt(fighter.module_accessor, *FIGHTER_STATUS_KIND_DAMAGE_FLY_METEOR);
         return 1.into();
     }
