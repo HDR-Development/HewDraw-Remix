d692e77edf40c4f0bb707ca22d898268f10f3dac
Riah <gabrielgreenbean@gmail.com>
2022-09-19 20:22:12 -0700

    Numberydoos

--- fighters/shulk/src/acmd/ground.rs
+++ fighters/shulk/src/acmd/ground.rs
@@ -20,9 +20,9 @@ unsafe fn shulk_attack_11_game(fighter: &mut L2CAgentBase) {
         ATTACK(fighter, 3, 0, Hash40::new("top"), 2.0, 361, 10, 0, 25, 2.0, 0.0, 9.0, 11.0, None, None, None, 1.1, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA_d, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_PUNCH, *ATTACK_REGION_PUNCH);
         //jablock hitbox
         ATTACK(fighter, 4, 0, Hash40::new("top"), 2.0, 361, 10, 0, 25, 2.0, 0.0, 9.0, 2.5, Some(0.0), Some(10.0), Some(0.0), 1.5, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_PUNCH, *ATTACK_REGION_PUNCH);
-        AttackModule::set_add_reaction_frame(boma, 0, 2, false);
-        AttackModule::set_add_reaction_frame(boma, 1, 2, false);
-        AttackModule::set_add_reaction_frame(boma, 2, 2, false);
+        AttackModule::set_add_reaction_frame(boma, 0, 3.0, false);
+        AttackModule::set_add_reaction_frame(boma, 1, 3.0, false);
+        AttackModule::set_add_reaction_frame(boma, 2, 3.0, false);
     }
     wait(lua_state, 1.0);
     if is_excute(fighter) {
