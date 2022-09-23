6fd7f85aa491e29d6513e25d504fa477446fa1a1
Elias Michaias <emskeirik@gmail.com>
2022-09-22 22:39:40 -0400

    Addendum to Code Cleanup

--- fighters/duckhunt/src/acmd/ground.rs
+++ fighters/duckhunt/src/acmd/ground.rs
@@ -4,9 +4,13 @@ use super::*;
 unsafe fn duckhunt_attackdash_game(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
     let boma = fighter.boma();
+    if is_excute(fighter) {
+        FT_MOTION_RATE(fighter, 0.70);
+    }
     frame(lua_state, 10.0);
     if is_excute(fighter) {
         ATTACK(fighter, 0, 0, Hash40::new("top"), 10.0, 60, 40, 0, 100, 5.5, 0.0, 7.0, 9.0, Some(0.0), Some(7.0), Some(2.0), 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_sting"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_HEAD);
+        FT_MOTION_RATE(fighter, 1.0);
     }
     frame(lua_state, 13.0);
     if is_excute(fighter) {
