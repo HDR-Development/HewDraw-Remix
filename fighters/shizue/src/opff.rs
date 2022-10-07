d9bac5b6a6d9327aff06ee599b2b84309a46ec4a
Elias Michaias <emskeirik@gmail.com>
2022-10-07 11:16:38 -0400

    Uh

--- fighters/shizue/src/opff.rs
+++ fighters/shizue/src/opff.rs
@@ -65,23 +65,24 @@ unsafe fn reel_in(boma: &mut BattleObjectModuleAccessor, status_kind: i32, situa
         }
     }
 }
-
-//May be useful later to remove grab on rod pull back
-//#[smashline::weapon_frame_callback]
-//pub fn fishingrod_callback(weapon: &mut smash::lua2cpp::L2CFighterBase) {
-//    unsafe {
-//       if weapon.kind() != WEAPON_KIND_SHIZUE_FISHINGROD {
-//            return
-//        }
-//        let boma = weapon.boma();
-//
-//        if boma.is_status(*WEAPON_SHIZUE_FISHINGROD_STATUS_KIND_END) {
-//            WeaponSpecializer_ShizueFishingrod::enable_search(boma, false);
-//        }
-//
-//        let status = StatusModule::status_kind(boma);
-//    }
-//}
+ 
+//Disable grab on fishingrod when pullingback
+#[weapon_frame_callback]
+fn fishingrod_callback(weapon : &mut L2CFighterBase) {
+    unsafe {
+        let object_id = (*weapon.battle_object).battle_object_id;
+        let owner_id = WorkModule::get_int(weapon.module_accessor, *WEAPON_INSTANCE_WORK_ID_INT_LINK_OWNER);
+        let activate_id = WorkModule::get_int(weapon.module_accessor, *WEAPON_INSTANCE_WORK_ID_INT_ACTIVATE_FOUNDER_ID);
+        let serial_id = WorkModule::get_int(weapon.module_accessor, 0x10000002);
+        let kind = utility::get_kind(&mut (*weapon.module_accessor));
+        if kind == *WEAPON_KIND_SHIZUE_FISHINGROD {
+            let status = StatusModule::status_kind(weapon.module_accessor);
+            if status == *WEAPON_SHIZUE_FISHINGROD_STATUS_KIND_REEL {
+                WeaponSpecializer_ShizueFishingrod::enable_search(weapon.module_accessor, false);
+            }
+        }
+    }
+}
 
 // Lloid Trap Fire Jump Cancel
 unsafe fn lloid_trap_fire_jc(boma: &mut BattleObjectModuleAccessor, status_kind: i32, situation_kind: i32, cat1: i32, stick_x: f32, facing: f32, frame: f32) {
@@ -170,7 +171,6 @@ pub fn shizue_frame_wrapper(fighter: &mut smash::lua2cpp::L2CFighterCommon) {
         empty_flash(fighter);
         balloon_dash(fighter);
         balloon_special_cancel(fighter);
-        //line_drop(fighter);
     }
 }
 
