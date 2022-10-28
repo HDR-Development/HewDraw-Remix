03a6ae4d44165e48431cbeb98396cc32f6444a3a
ncsuddut <ncsuddut@ncsu.edu>
2022-10-28 00:13:29 -0400

    special cancel DTilt/DSmash into Sapling

--- fighters/murabito/src/opff.rs
+++ fighters/murabito/src/opff.rs
@@ -3,6 +3,17 @@ utils::import_noreturn!(common::opff::fighter_common_opff);
 use super::*;
 use globals::*;
 
+unsafe fn sapling_special_cancel(fighter: &mut L2CFighterCommon) {
+    let boma = fighter.boma();
+    if fighter.is_status_one_of(&[*FIGHTER_STATUS_KIND_ATTACK_LW3, *FIGHTER_STATUS_KIND_ATTACK_LW4])
+    && (AttackModule::is_infliction_status(boma, *COLLISION_KIND_MASK_HIT) || AttackModule::is_infliction_status(boma, *COLLISION_KIND_MASK_SHIELD))
+    && !fighter.is_in_hitlag()
+    && fighter.is_cat_flag(Cat1::SpecialLw)
+    && !ArticleModule::is_exist(fighter.module_accessor, *FIGHTER_MURABITO_GENERATE_ARTICLE_TREE)
+    && !ArticleModule::is_exist(fighter.module_accessor, *FIGHTER_MURABITO_GENERATE_ARTICLE_SPROUT) {
+        StatusModule::change_status_request_from_script(boma, *FIGHTER_STATUS_KIND_SPECIAL_LW, true);
+    }
+}
  
 pub unsafe fn moveset(boma: &mut BattleObjectModuleAccessor, id: usize, cat: [i32 ; 4], status_kind: i32, situation_kind: i32, motion_kind: u64, stick_x: f32, stick_y: f32, facing: f32, frame: f32) {
 }
@@ -11,7 +22,8 @@ pub unsafe fn moveset(boma: &mut BattleObjectModuleAccessor, id: usize, cat: [i3
 pub fn murabito_frame_wrapper(fighter: &mut smash::lua2cpp::L2CFighterCommon) {
     unsafe {
         common::opff::fighter_common_opff(fighter);
-		murabito_frame(fighter)
+		murabito_frame(fighter);
+        sapling_special_cancel(fighter);
     }
 }
 
