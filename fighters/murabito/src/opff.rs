d9e69c389f5986dd66d4a39b99be60ab729c2bd2
ncsuddut <ncsuddut@ncsu.edu>
2022-11-08 15:11:14 -0500

    fix buggy can canceling

--- fighters/murabito/src/opff.rs
+++ fighters/murabito/src/opff.rs
@@ -14,8 +14,35 @@ unsafe fn sapling_special_cancel(fighter: &mut L2CFighterCommon) {
         StatusModule::change_status_request_from_script(boma, *FIGHTER_STATUS_KIND_SPECIAL_LW, true);
     }
 }
+
+unsafe fn dspecial_cancels(boma: &mut BattleObjectModuleAccessor, status_kind: i32, situation_kind: i32, cat1: i32) {
+    //PM-like down-b canceling
+    if (status_kind == *FIGHTER_MURABITO_STATUS_KIND_SPECIAL_LW_WATER_AIR
+        || status_kind == *FIGHTER_MURABITO_STATUS_KIND_SPECIAL_LW_WATER_DASH_B
+        || status_kind == *FIGHTER_MURABITO_STATUS_KIND_SPECIAL_LW_WATER_DASH_F
+        || status_kind == *FIGHTER_MURABITO_STATUS_KIND_SPECIAL_LW_WATER_JUMP
+        || status_kind == *FIGHTER_MURABITO_STATUS_KIND_SPECIAL_LW_WATER_JUMP_SQUAT
+        || status_kind == *FIGHTER_MURABITO_STATUS_KIND_SPECIAL_LW_WATER_LANDING
+        || status_kind == *FIGHTER_MURABITO_STATUS_KIND_SPECIAL_LW_WATER_WAIT
+        || status_kind == *FIGHTER_MURABITO_STATUS_KIND_SPECIAL_LW_WATER_WALK_B
+        || status_kind == *FIGHTER_MURABITO_STATUS_KIND_SPECIAL_LW_WATER_WALK_BRAKE_B
+        || status_kind == *FIGHTER_MURABITO_STATUS_KIND_SPECIAL_LW_WATER_WALK_BRAKE_F
+        || status_kind == *FIGHTER_MURABITO_STATUS_KIND_SPECIAL_LW_WATER_WALK_F) {
+            if ControlModule::check_button_trigger(boma, *CONTROL_PAD_BUTTON_GUARD) {
+                if situation_kind == *SITUATION_KIND_AIR {
+                    WorkModule::unable_transition_term_group(boma, *FIGHTER_STATUS_TRANSITION_GROUP_CHK_AIR_ESCAPE);
+                    ControlModule::clear_command_one(boma, *FIGHTER_PAD_COMMAND_CATEGORY1, *FIGHTER_PAD_CMD_CAT1_AIR_ESCAPE);
+                    StatusModule::change_status_request_from_script(boma, *FIGHTER_STATUS_KIND_FALL, false);
+                }
+                if situation_kind == *SITUATION_KIND_GROUND {
+                    StatusModule::change_status_request_from_script(boma, *FIGHTER_STATUS_KIND_WAIT, false);
+                }
+            }
+    }
+}
  
 pub unsafe fn moveset(boma: &mut BattleObjectModuleAccessor, id: usize, cat: [i32 ; 4], status_kind: i32, situation_kind: i32, motion_kind: u64, stick_x: f32, stick_y: f32, facing: f32, frame: f32) {
+    dspecial_cancels(boma, status_kind, situation_kind, cat[0]);
 }
 
 #[utils::macros::opff(FIGHTER_KIND_MURABITO )]
