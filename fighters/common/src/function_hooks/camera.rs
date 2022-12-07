d5699e360f4881e23a21fcfa0094f21c3b59b09a
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2022-12-06 22:16:25 -0500

    no opff

--- fighters/common/src/function_hooks/camera.rs
+++ fighters/common/src/function_hooks/camera.rs
@@ -1,6 +1,13 @@
 use super::*;
 use utils::ext::*;
 
+static mut IS_STOP: bool = false;
+
+#[skyline::hook(offset = 0x3a7f90, inline)]
+unsafe fn battle_object__process_begin_sub(ctx: &skyline::hooks::InlineCtx) {
+    IS_STOP = *ctx.registers[19].w.as_ref() & 1 != 0;
+}
+
 #[skyline::hook(offset = 0x2606270)]
 unsafe fn cameramanager__update_frame(camera_manager: *mut *mut u64) {
     call_original!(camera_manager);
@@ -19,6 +26,7 @@ unsafe fn cameramanager__update(camera_manager: *mut *mut u64) {
 
 pub fn install() {
     skyline::install_hooks!(
+        battle_object__process_begin_sub,
         cameramanager__update_frame,
         cameramanager__update
     );
