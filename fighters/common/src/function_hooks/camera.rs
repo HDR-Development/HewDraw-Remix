eb8bfb40c293b402fe0d62a098681fbe53ec8969
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2022-12-07 13:50:31 -0500

    blumjay saves the day

--- fighters/common/src/function_hooks/camera.rs
+++ fighters/common/src/function_hooks/camera.rs
@@ -2,28 +2,15 @@ use super::*;
 use utils::ext::*;
 
 
-static mut IS_STOP: bool = false;
-
-// Keeps track of hitlag
-#[skyline::hook(offset = 0x3a7f74, inline)]
-unsafe fn battle_object__process_begin_sub(ctx: &skyline::hooks::InlineCtx) {
-    let boma = *ctx.registers[23].x.as_ref() as *mut BattleObjectModuleAccessor;
-    let stop_module = *(boma as *mut BattleObjectModuleAccessor as *mut u64).add(0x90 / 8) as *const u64;
-    IS_STOP = *(stop_module as *const bool).add(0x3c);
-}
-
-// Doubles camera speed while not in hitlag
-#[skyline::hook(offset = 0x4f0a80)]
-unsafe fn cameramanager__update(camera_manager: *mut *mut u64) {
-    call_original!(camera_manager);
-    if !IS_STOP {
-        call_original!(camera_manager);
-    }
+// Doubles camera speed
+#[skyline::hook(offset = 0x4fdbf0)]
+unsafe fn normal_camera(ptr: u64, float: f32) {
+    call_original!(ptr, float);
+    call_original!(ptr, float);
 }
 
 pub fn install() {
     skyline::install_hooks!(
-        battle_object__process_begin_sub,
-        cameramanager__update
+        normal_camera
     );
 }
