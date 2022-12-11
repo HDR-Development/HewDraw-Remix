f315d9ff164a1292d667664be872733a09e5cbae
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2022-12-11 18:32:51 -0500

    reduce strength of getup attacks

--- fighters/common/src/function_hooks/attack.rs
+++ fighters/common/src/function_hooks/attack.rs
@@ -5,11 +5,26 @@ use std::arch::asm;
 
 #[skyline::hook(offset = 0x3dc160)]
 unsafe fn attack_module_set_attack(module: u64, id: i32, group: i32, data: &mut smash2::app::AttackData) {
+    let boma = *(module as *mut *mut BattleObjectModuleAccessor).add(1);
+
     // if a hitbox does not intentionally trip 100% of time, remove random trip chance
     if data.slip < 1.0 {
         // -1.0 trip chance prevents any tripping whatsoever
         data.slip = -1.0;
     }
+
+    // Reduce strength of getup attacks
+    if (*boma).is_status(*FIGHTER_STATUS_KIND_DOWN_STAND_ATTACK) {
+        data.power = 6.0;  // damage
+        data.vector = 361;  // angle
+        data.sub_shield = 0;  // shield damage modifier
+    }
+    if (*boma).is_status(*FIGHTER_STATUS_KIND_SLIP_STAND_ATTACK) {
+        data.power = 5.0;
+        data.vector = 361;
+        data.sub_shield = 0;
+    }
+    
     call_original!(module, id, group, data)
 }
 
