fa67a2780142e6f6c84c810889214ab251ab2f1b
techyCoder81 <42820193+techyCoder81@users.noreply.github.com>
2023-01-03 21:04:42 -0500

    fix to uspecial iframes and launcher activity

--- fighters/donkey/src/acmd/specials.rs
+++ fighters/donkey/src/acmd/specials.rs
@@ -230,10 +230,10 @@ unsafe fn special_hi(fighter: &mut L2CAgentBase) {
         ATTACK(fighter, 3, 0, Hash40::new("top"), 12.0, 361, 90, 0, 60, 8.0, 0.0, 10.5, -5.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_PUNCH, *ATTACK_REGION_BODY);
     }
     
-    frame(lua_state, 9.0);
+    frame(lua_state, 10.0);
     if is_excute(fighter) {
         AttackModule::clear_all(boma);
-        HitModule::set_status_all(boma, app::HitStatus(*HIT_STATUS_NORMAL), 0);
+        HitModule::set_whole(boma, app::HitStatus(*HIT_STATUS_NORMAL), 0);
         FT_MOTION_RATE(fighter, 1.0);
     }
     frame(lua_state, 19.0);
