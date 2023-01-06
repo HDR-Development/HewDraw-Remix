c74f424c0260bbcadf20fa8b8ff2495de6f60227
techyCoder81 <42820193+techyCoder81@users.noreply.github.com>
2023-01-06 12:31:17 -0500

    improved transitions and throw status

--- fighters/nana/src/status.rs
+++ fighters/nana/src/status.rs
@@ -5,7 +5,8 @@ utils::import!(popo::{ics_dash});
  
 pub fn install() {
     install_status_scripts!(
-        dash
+        dash,
+        throw
     );
 }
 
@@ -14,4 +15,19 @@ pub fn install() {
 #[status_script(agent = "nana", status = FIGHTER_STATUS_KIND_DASH, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_MAIN)]
 pub unsafe fn dash(fighter: &mut L2CFighterCommon) -> L2CValue {
     popo::ics_dash(fighter)
+}
+
+#[status_script(agent = "nana", status = FIGHTER_STATUS_KIND_THROW, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_MAIN)]
+pub unsafe fn throw(fighter: &mut L2CFighterCommon) -> L2CValue {
+    // force nana into a random throw
+    let selected = app::sv_math::rand(hash40("fighter"), 100);
+    let throw_name = match selected {
+        0..=24 => "throw_b",
+        25..=49 => "throw_f",
+        50..=74 => "throw_lw",
+        _ => "throw_hi"
+    };
+
+    MotionModule::change_motion(fighter.boma(), Hash40::new(throw_name), 0.0, 1.0, false, 0.0, false, false);
+    fighter.sub_shift_status_main(L2CValue::Ptr(L2CFighterCommon_status_Throw_Main as *const () as _))
 }
