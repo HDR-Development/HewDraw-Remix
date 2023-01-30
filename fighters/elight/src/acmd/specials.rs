367ff4136d84a3fc4ed77ce240e1eec0b85e0ad3
HisashiASM <98564957+HisashiASM@users.noreply.github.com>
2023-01-30 02:35:31 -0600

    A little more now
    Balls

--- fighters/elight/src/acmd/specials.rs
+++ fighters/elight/src/acmd/specials.rs
@@ -358,7 +358,7 @@ unsafe fn game_specialsstart (fighter: &mut L2CAgentBase) {
 	let boma = fighter.boma();
     frame(lua_state, 1.0);
     if is_excute(fighter) {
-        MotionModule::set_rate(boma, 2.0);
+        MotionModule::set_rate(boma, 4.0);
     }
 }
 
@@ -460,7 +460,7 @@ unsafe fn game_specials4(fighter: &mut L2CAgentBase) {
 	let boma = fighter.boma();
     frame(lua_state, 4.0);
     if is_excute(fighter) {
-        ATTACK(fighter, 0, 0, Hash40::new("rot"), 16.0, 90, 115, 0, 45, 3.5, 0.0, 0.0, 0.0, Some(0.0), Some(0.0), Some(0.0), 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, true, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
+        ATTACK(fighter, 0, 0, Hash40::new("rot"), 16.0, 90, 115, 0, 45, 3.5, 0.0, 0.0, 0.0, Some(0.0), Some(0.0), Some(0.0), 1.5, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, true, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
         ATTACK(fighter, 1, 0, Hash40::new("rot"), 11.0, 60, 100, 65, 45, 12.0, 0.0, 0.0, 0.0, Some(0.0), Some(0.0), Some(0.0), 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, true, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
     }
     frame(lua_state, 6.0);
