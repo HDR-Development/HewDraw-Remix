c191d96669720c6c7643e7ba7e056384ff505da8
Askew <sqk9gw@gmail.com>
2023-02-12 22:26:45 -0500

    Effect refinement

--- fighters/purin/src/acmd/aerials.rs
+++ fighters/purin/src/acmd/aerials.rs
@@ -146,7 +146,7 @@ unsafe fn purin_effect_attackairhi(fighter: &mut L2CAgentBase) {
     frame(lua_state, 9.0);
     if is_excute(fighter) {
         EFFECT_FOLLOW_FLIP_ALPHA(fighter, Hash40::new("sys_attack_arc"), Hash40::new("sys_attack_arc"), Hash40::new("top"), 0, 7, 1, 0, -85, -90, 0.85, true, *EF_FLIP_YZ, 0.3);
-        LAST_EFFECT_SET_RATE(fighter, 0.65);
+        LAST_EFFECT_SET_RATE(fighter, 0.75);
     }
 }
 
