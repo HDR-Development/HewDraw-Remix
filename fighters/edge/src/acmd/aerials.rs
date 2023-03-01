91b97f82664d51ce7dcb08c4c3aebd81acd4a198
yoonikoo <timothykoo497@outlook.com>
2023-02-28 20:50:20 -0800

    Update aerials.rs
    Mistake was made mb

--- fighters/edge/src/acmd/aerials.rs
+++ fighters/edge/src/acmd/aerials.rs
@@ -320,10 +320,6 @@ unsafe fn sephiroth_attack_air_hi_game(fighter: &mut L2CAgentBase) {
     if is_excute(fighter) {
         WorkModule::off_flag(boma, *FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING);
     }   
-    frame(lua_state, 52.0);
-    if is_excute(fighter) {
-        ATTACK(fighter, 2, 0, Hash40::new("arml"), 6.0, 64, 96, 0, 55, 2.5, -1.0, 0.0, 1.0, None, None, None, 0.9, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
-    }
 }
 
 #[acmd_script( agent = "edge", script = "effect_attackairhi" , category = ACMD_EFFECT , low_priority)]
