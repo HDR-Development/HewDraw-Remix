5e3df737cef16d0aa0529a8279d045e785306e2a
stingers135 <115596132+stingers135@users.noreply.github.com>
2023-03-13 07:47:46 -0400

    Remove wallriding
    Wallriding removed

--- fighters/sheik/src/opff.rs
+++ fighters/sheik/src/opff.rs
@@ -28,7 +28,7 @@ extern "Rust" {
     fn gimmick_flash(boma: &mut BattleObjectModuleAccessor);
 }
 
-pub unsafe fn sheik_teleport_cancel(boma: &mut BattleObjectModuleAccessor, status_kind: i32, id: usize) {
+//pub unsafe fn sheik_teleport_cancel(boma: &mut BattleObjectModuleAccessor, status_kind: i32, id: usize) {
     /*
     if status_kind == *FIGHTER_SHEIK_STATUS_KIND_SPECIAL_HI_MOVE {
         if compare_mask(ControlModule::get_pad_flag(boma), *FIGHTER_PAD_FLAG_SPECIAL_TRIGGER) {
@@ -38,38 +38,38 @@ pub unsafe fn sheik_teleport_cancel(boma: &mut BattleObjectModuleAccessor, statu
     */
 
     // Wall Ride momentum fixes
-    let mut wall_ride = Vector3f{x: 1.0, y: 1.0, z: 1.0};
-    let touch_right = GroundModule::is_wall_touch_line(boma, *GROUND_TOUCH_FLAG_RIGHT_SIDE as u32);
-    let touch_left = GroundModule::is_wall_touch_line(boma, *GROUND_TOUCH_FLAG_LEFT_SIDE as u32);
-    let warp_speed = WorkModule::get_param_float(boma, hash40("param_special_hi"), hash40("warp_speed_add")) + WorkModule::get_param_float(boma, hash40("param_special_hi"), hash40("warp_speed_mul"));
-
-    if status_kind == *FIGHTER_SHEIK_STATUS_KIND_SPECIAL_HI_MOVE {
-        if touch_right || touch_left || VarModule::is_flag(boma.object(), vars::common::instance::IS_TELEPORT_WALL_RIDE) {
-            VarModule::on_flag(boma.object(), vars::common::instance::IS_TELEPORT_WALL_RIDE);
-            if (touch_right && KineticModule::get_sum_speed_x(boma, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN) < 0.0) || (touch_left && KineticModule::get_sum_speed_x(boma, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN) > 0.0) {
-                let rise_speed = KineticModule::get_sum_speed_y(boma, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN);
-                if rise_speed > 0.0 {
-                    wall_ride = Vector3f{x: 0.0, y: (warp_speed / rise_speed), z: 1.0};
-                }
-                else {
-                    wall_ride = Vector3f{x: 0.0, y: 1.0, z: 1.0};
-                }
-                KineticModule::mul_speed(boma, &wall_ride, *FIGHTER_KINETIC_ENERGY_ID_GRAVITY);
-            }
-        }
-    }
-    else if status_kind == *FIGHTER_SHEIK_STATUS_KIND_SPECIAL_HI_END {
-        if touch_right || touch_left {
-            if (touch_right && KineticModule::get_sum_speed_x(boma, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN) < 0.0) || (touch_left && KineticModule::get_sum_speed_x(boma, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN) > 0.0) {
-                wall_ride = Vector3f{x: 0.0, y: 1.0, z: 1.0};
-                KineticModule::mul_speed(boma, &wall_ride, *FIGHTER_KINETIC_ENERGY_ID_GRAVITY);
-            }
-        }
-    }
-    else {
-        VarModule::off_flag(boma.object(), vars::common::instance::IS_TELEPORT_WALL_RIDE);
-    }
-}
+//    let mut wall_ride = Vector3f{x: 1.0, y: 1.0, z: 1.0};
+//    let touch_right = GroundModule::is_wall_touch_line(boma, *GROUND_TOUCH_FLAG_RIGHT_SIDE as u32);
+//    let touch_left = GroundModule::is_wall_touch_line(boma, *GROUND_TOUCH_FLAG_LEFT_SIDE as u32);
+//    let warp_speed = WorkModule::get_param_float(boma, hash40("param_special_hi"), hash40("warp_speed_add")) + WorkModule::get_param_float(boma, hash40("param_special_hi"), hash40("warp_speed_mul"));
+//
+//   if status_kind == *FIGHTER_SHEIK_STATUS_KIND_SPECIAL_HI_MOVE {
+//        if touch_right || touch_left || VarModule::is_flag(boma.object(), vars::common::instance::IS_TELEPORT_WALL_RIDE) {
+//            VarModule::on_flag(boma.object(), vars::common::instance::IS_TELEPORT_WALL_RIDE);
+//            if (touch_right && KineticModule::get_sum_speed_x(boma, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN) < 0.0) || (touch_left && KineticModule::get_sum_speed_x(boma, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN) > 0.0) {
+//                let rise_speed = KineticModule::get_sum_speed_y(boma, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN);
+//                if rise_speed > 0.0 {
+//                    wall_ride = Vector3f{x: 0.0, y: (warp_speed / rise_speed), z: 1.0};
+//                }
+//                else {
+//                    wall_ride = Vector3f{x: 0.0, y: 1.0, z: 1.0};
+//                }
+//                KineticModule::mul_speed(boma, &wall_ride, *FIGHTER_KINETIC_ENERGY_ID_GRAVITY);
+//            }
+//        }
+//    }
+//    else if status_kind == *FIGHTER_SHEIK_STATUS_KIND_SPECIAL_HI_END {
+//        if touch_right || touch_left {
+//            if (touch_right && KineticModule::get_sum_speed_x(boma, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN) < 0.0) || (touch_left && KineticModule::get_sum_speed_x(boma, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN) > 0.0) {
+//                wall_ride = Vector3f{x: 0.0, y: 1.0, z: 1.0};
+//                KineticModule::mul_speed(boma, &wall_ride, *FIGHTER_KINETIC_ENERGY_ID_GRAVITY);
+//            }
+//        }
+//    }
+//    else {
+//        VarModule::off_flag(boma.object(), vars::common::instance::IS_TELEPORT_WALL_RIDE);
+//    }
+//}
 
 // pub unsafe fn hitfall_aerials(fighter: &mut L2CFighterCommon, frame: f32) {
 //     if fighter.is_status(*FIGHTER_STATUS_KIND_ATTACK_AIR) {
@@ -88,7 +88,7 @@ pub unsafe fn sheik_teleport_cancel(boma: &mut BattleObjectModuleAccessor, statu
 pub unsafe fn moveset(fighter: &mut L2CFighterCommon, boma: &mut BattleObjectModuleAccessor, id: usize, cat: [i32 ; 4], status_kind: i32, situation_kind: i32, motion_kind: u64, stick_x: f32, stick_y: f32, facing: f32, frame: f32) {
     bouncing_fish_return_cancel(fighter, boma, status_kind, situation_kind, cat[0], frame);
     nspecial_cancels(fighter, boma, status_kind, situation_kind);
-    sheik_teleport_cancel(boma, status_kind, id);
+    //sheik_teleport_cancel(boma, status_kind, id);
     //hitfall_aerials(fighter, frame);
 }
 
