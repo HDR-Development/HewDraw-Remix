9b8a3267cd625f37749f61741939300ab152e7cc
techyCoder81 <42820193+techyCoder81@users.noreply.github.com>
2023-03-17 19:44:54 -0400

    testing

---
+++ scripts/extract_lvd_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TOP="$(pwd)"
+
+ls -lart
+unzip lvd_dumper.zip
+cd scripts
+ls switch-package/sdcard/ultimate/mods/hdr-stages/stage/
+mkdir lvd
+cp switch-package/sdcard/ultimate/mods/hdr-stages/stage/ lvd -rf
+
+find lvd -not -name '*.lvd' -type f | xargs rm
+cd $TOP/publish
+./Ultimate_LVD_data $TOP/scripts/lvd/stage
+ls output
+ls output/stages
+cd output
+zip -f lvd.zip stages
+cp lvd.zip $TOP/scripts/artifacts
