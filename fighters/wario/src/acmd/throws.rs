b0910d3dfecabf987856294e989da388bad12b30
C# <afr0man005@gmail.com>
2023-03-19 02:30:54 -0700

    Update throws.rs

--- fighters/wario/src/acmd/throws.rs
+++ fighters/wario/src/acmd/throws.rs
@@ -1,28 +1,35 @@
 
 use super::*;
 
+
 #[acmd_script( agent = "wario", script = "game_catchattack" , category = ACMD_GAME , low_priority)]
-unsafe fn catch_attack(fighter: &mut L2CAgentBase) {
+unsafe fn game_catch_attack(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
     let boma = fighter.boma();
     
     frame(lua_state, 2.0);
     if is_excute(fighter) {
-        ATTACK(fighter, 0, 1, Hash40::new("top"), 0.8, 361, 100, 30, 0, 5.0, 0.0, 10.0, 7.0, None, None, None, 2.3, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_PUNCH, *ATTACK_REGION_PUNCH);
+        ATTACK(fighter, 0, 0, Hash40::new("top"),  1.3, 361, 100, 30, 0, 5.0, 0.0, 10.0, 7.0, None, None, None, 0.95, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, true, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_coin"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_COIN, *ATTACK_REGION_PUNCH);
         AttackModule::set_catch_only_all(boma, true, false);
+        AttackModule::set_damage_shake_scale(boma, 0.0);
+        AttackModule::set_attack_camera_quake_forced(boma, 0, *CAMERA_QUAKE_KIND_NONE, false);
     }
     wait(lua_state, 1.0);
     if is_excute(fighter) {
         AttackModule::clear_all(boma);
+        MotionModule::set_frame(boma.get_grabbed_opponent_boma(), 0.1, false);
     }
-    wait(lua_state, 1.0);
+    frame(fighter.lua_state_agent,12.0);
     if is_excute(fighter) {
-        ATTACK(fighter, 0, 0, Hash40::new("top"), 0.8, 361, 100, 30, 0, 5.0, 0.0, 10.0, 7.0, None, None, None, 2.3, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, true, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_coin"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_COIN, *ATTACK_REGION_PUNCH);
-        AttackModule::set_catch_only_all(boma, true, false);
+        let defender= boma.get_grabbed_opponent_boma();
+        MotionModule::set_frame(defender, 14.0, false);
+        MotionModule::set_rate(defender, 1.0);
     }
-    wait(lua_state, 1.0);
-    if is_excute(fighter) {
-        AttackModule::clear_all(boma);
+}
+#[acmd_script( agent = "wario", script = "effect_catchattack", category = ACMD_EFFECT )]
+unsafe fn effect_catch_attack(fighter: &mut L2CAgentBase) {
+    if macros::is_excute(fighter) {
+        EFFECT_FOLLOW_NO_STOP(fighter, Hash40::new("sys_attack_speedline"), Hash40::new("top"), 0.0, 7.25, 11.0, 260, 0, 0, 1.1, true);
     }
 }
 
@@ -335,6 +342,7 @@ unsafe fn expression_throwf(fighter: &mut L2CAgentBase) {
 pub fn install() {
     install_acmd_scripts!(
         catch_attack,
+        effect_catch_attack,
         game_throwlw,
         
         game_throwhi,
