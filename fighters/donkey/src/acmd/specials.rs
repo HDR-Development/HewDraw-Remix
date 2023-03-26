9841865fcc2d9b57dd14db8e41db4e3f57fe2f83
techyCoder81 <42820193+techyCoder81@users.noreply.github.com>
2023-03-25 22:43:25 -0400

    Update specials.rs

--- fighters/donkey/src/acmd/specials.rs
+++ fighters/donkey/src/acmd/specials.rs
@@ -219,9 +219,9 @@ unsafe fn special_hi(fighter: &mut L2CAgentBase) {
     let boma = fighter.boma();
     frame(lua_state, 1.0);
     if is_excute(fighter) {
-        FT_MOTION_RATE(fighter, 0.500);
+        FT_MOTION_RATE(fighter, 2.0);
     }
-    frame(lua_state, 4.0);
+    frame(lua_state, 3.0);
     if is_excute(fighter) {
         FT_MOTION_RATE(fighter, 0.5);
         HitModule::set_whole(fighter.module_accessor, HitStatus(*HIT_STATUS_XLU), 0);
@@ -230,7 +230,7 @@ unsafe fn special_hi(fighter: &mut L2CAgentBase) {
         ATTACK(fighter, 3, 0, Hash40::new("top"), 12.0, 361, 90, 0, 60, 6.0, 0.0, 10.5, -5.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_PUNCH, *ATTACK_REGION_BODY);
     }
     
-    frame(lua_state, 10.0);
+    frame(lua_state, 9.0);
     if is_excute(fighter) {
         AttackModule::clear_all(boma);
         HitModule::set_whole(boma, app::HitStatus(*HIT_STATUS_NORMAL), 0);
