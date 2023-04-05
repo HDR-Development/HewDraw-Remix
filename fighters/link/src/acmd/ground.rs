c608558af8f42e1cc25155a993de00aab18d793a
yoonikoo <timothykoo497@outlook.com>
2023-04-05 00:20:44 -0700

    Jab 1 , DA, BAIR fixes. Arrow and Boomerang Adjustments

--- fighters/link/src/acmd/ground.rs
+++ fighters/link/src/acmd/ground.rs
@@ -21,6 +21,8 @@ unsafe fn attack_11(fighter: &mut L2CAgentBase) {
         AttackModule::clear_all(boma);
         WorkModule::on_flag(boma, *FIGHTER_STATUS_ATTACK_FLAG_ENABLE_COMBO);
     }
+    frame(lua_state, 11.0);
+    FT_MOTION_RATE(fighter, 0.65);
 }
 
 #[acmd_script( agent = "link", script = "game_attack12" , category = ACMD_GAME , low_priority)]
@@ -28,6 +30,7 @@ unsafe fn attack_12 (fighter: &mut L2CAgentBase) {
 	let lua_state = fighter.lua_state_agent;
 	let boma = fighter.boma();
 	frame(lua_state, 5.0);
+    FT_MOTION_RATE(fighter, 1.0);
 	if is_excute(fighter) {
 		ATTACK(fighter, 0, 0, Hash40::new("sword1"), 3.0, 361, 25, 0, 25, 3.0, 1.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
 		ATTACK(fighter, 1, 0, Hash40::new("sword1"), 3.0, 361, 25, 0, 25, 3.0, 6.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
@@ -66,6 +69,7 @@ unsafe fn attack_dash(fighter: &mut L2CAgentBase) {
         ATTACK(fighter, 1, 0, Hash40::new("armr"), 13.0, 55, 78, 0, 85, 2.5, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_SWORD);
         ATTACK(fighter, 2, 0, Hash40::new("sword2"), 13.0, 55, 82, 0, 75, 3.0, 3.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
 		ATTACK(fighter, 3, 0, Hash40::new("sword2"), 14.0, 55, 82, 0, 70, 3.0, 8.7, 0.0, 0.0, None, None, None, 1.2, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
+        ATK_SET_SHIELD_SETOFF_MUL_arg5(fighter, 0, 1, 2, 3, 1.4);
         AttackModule::set_attack_height_all(boma, app::AttackHeight(*ATTACK_HEIGHT_HIGH), false);
     }
     wait(lua_state, 5.0);
@@ -73,7 +77,7 @@ unsafe fn attack_dash(fighter: &mut L2CAgentBase) {
         AttackModule::clear_all(boma);
     }
 	frame(lua_state, 40.0);
-    FT_MOTION_RATE(fighter, 0.750);
+    FT_MOTION_RATE(fighter, 0.75);
     
 	frame(lua_state, 50.0);
     FT_MOTION_RATE(fighter, 1.0);
