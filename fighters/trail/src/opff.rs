2c8128eedfefa7e07b51f4173263d89bb8893d42
Aaron <aaronalexander647@gmail.com>
2023-04-06 12:19:15 -0700

    Sora Blizzaga Rework

--- fighters/trail/src/opff.rs
+++ fighters/trail/src/opff.rs
@@ -116,10 +116,10 @@ unsafe fn magic_cancels(boma: &mut BattleObjectModuleAccessor) {
     }
     // Blizzard jump cancel
     if (boma.is_status(*FIGHTER_TRAIL_STATUS_KIND_SPECIAL_N2)
-        && AttackModule::is_infliction_status(boma, *COLLISION_KIND_MASK_HIT) 
+        && AttackModule::is_infliction_status(boma, *COLLISION_KIND_MASK_HIT)
+        && !boma.is_in_hitlag()
         && VarModule::get_int(boma.object(), vars::common::instance::LAST_ATTACK_HITBOX_ID) == 0) {
                 boma.check_jump_cancel(false);
-                AttackModule::clear_all(boma);
                 WorkModule::off_flag(boma,  *FIGHTER_TRAIL_INSTANCE_WORK_ID_FLAG_MAGIC_SELECT_FORBID);
                 WorkModule::on_flag(boma,  *FIGHTER_TRAIL_STATUS_SPECIAL_N2_FLAG_CHANGE_MAGIC);
     }
