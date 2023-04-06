715ed83fcef4fe4d1da1289bd2a9538c395507de
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2023-04-06 00:45:35 -0400

    initial pass

---
+++ fighters/wario/src/status/special_s.rs
@@ -0,0 +1,23 @@
+use super::*;
+use globals::*;
+
+// WEAPON_WARIO_WARIOBIKE_STATUS_KIND_SPECIAL_S_ESCAPE_START
+
+#[status_script(agent = "wario_wariobike", status = WEAPON_WARIO_WARIOBIKE_STATUS_KIND_SPECIAL_S_ESCAPE_START, condition = LUA_SCRIPT_STATUS_FUNC_INIT_STATUS)]
+pub unsafe fn special_s_escape_start_init(weapon: &mut L2CWeaponCommon) -> L2CValue {
+    let owner_id = WorkModule::get_int(weapon.module_accessor, *WEAPON_INSTANCE_WORK_ID_INT_LINK_OWNER) as u32;
+    let wario = utils::util::get_battle_object_from_id(owner_id);
+    let wario_boma = &mut *(*wario).module_accessor;
+    if weapon.global_table[SITUATION_KIND] == SITUATION_KIND_AIR
+    && wario_boma.get_num_used_jumps() < wario_boma.get_jump_count_max() {
+        WorkModule::inc_int(wario_boma, *FIGHTER_INSTANCE_WORK_ID_INT_JUMP_COUNT);
+    }
+    0.into()
+}
+
+
+pub fn install() {
+    install_status_scripts!(
+        special_s_escape_start_init
+    );
+}
