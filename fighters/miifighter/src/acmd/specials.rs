747047164b593dce2b2ae0804fadbb22a225fbe8
chrispo <69305550+chrispo-git@users.noreply.github.com>
2023-05-01 09:29:06 +0100

    Update specials.rs

--- fighters/miifighter/src/acmd/specials.rs
+++ fighters/miifighter/src/acmd/specials.rs
@@ -875,10 +875,11 @@ unsafe fn miifighter_special_air_lw2_auto_attack_game(fighter: &mut L2CAgentBase
 unsafe fn miifighter_special_lw3_catch_game(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
     let boma = fighter.boma();
+    frame(lua_state, 1.0);
     if is_excute(fighter) {
         ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH, 0, 3.0, 361, 100, 0, 70, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
     }
-    FT_MOTION_RATE(fighter, 2.5);
+    FT_MOTION_RATE(fighter, 8.0/(4.0-1.0));
     frame(lua_state, 4.0);
     FT_MOTION_RATE(fighter, 1.0);
     if is_excute(fighter) {
