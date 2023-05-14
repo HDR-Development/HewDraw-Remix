e0da5d1751b42217c52cd2594755a8da59693a16
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2023-05-14 16:01:11 -0400

    MK - remove drill rush land cancel on whiff

--- fighters/metaknight/src/status/special_s.rs
+++ fighters/metaknight/src/status/special_s.rs
@@ -14,7 +14,8 @@ pub fn install() {
 pub unsafe fn special_s_end_end(fighter: &mut L2CFighterCommon) -> L2CValue {
     let ret = original!(fighter);
     // Land cancel on sideB rebound
-    if fighter.is_motion_one_of(&[Hash40::new("special_air_s_finish"), Hash40::new("special_air_s_finish_c2"), Hash40::new("special_air_s_finish_c3"), Hash40::new("special_air_s_finish_c4")]) {
+    if fighter.is_motion_one_of(&[Hash40::new("special_air_s_finish"), Hash40::new("special_air_s_finish_c2"), Hash40::new("special_air_s_finish_c3"), Hash40::new("special_air_s_finish_c4")])
+    && VarModule::is_flag(fighter.battle_object, vars::metaknight::instance::DOWN_SPECIAL_HIT) {
         let landing_frame = WorkModule::get_param_float(fighter.module_accessor, hash40("landing_frame"), 0);
         WorkModule::set_float(fighter.module_accessor, landing_frame, *FIGHTER_INSTANCE_WORK_ID_FLOAT_LANDING_FRAME);
         WorkModule::off_flag(fighter.module_accessor, *FIGHTER_INSTANCE_WORK_ID_FLAG_DISABLE_LANDING_CANCEL);
