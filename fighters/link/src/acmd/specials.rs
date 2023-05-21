170f678ac483923d603a98662e4a6339a84078b1
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2023-05-21 17:05:38 -0400

    fix grounded upB whiff

--- fighters/link/src/acmd/specials.rs
+++ fighters/link/src/acmd/specials.rs
@@ -6,8 +6,8 @@ use super::*;
 unsafe fn special_hi(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
     let boma = fighter.boma();
-    FT_MOTION_RATE(fighter, 7.0/(4.0-0.0));
-    frame(lua_state, 4.0);
+    FT_MOTION_RATE(fighter, 7.0/(3.75-0.0));
+    frame(lua_state, 3.75);
     FT_MOTION_RATE(fighter, 1.0);
     if is_excute(fighter) {
         ATTACK(fighter, 0, 0, Hash40::new("sword2"), 14.0, 40, 80, 0, 50, 3.0, -2.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
