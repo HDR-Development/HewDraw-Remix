21550e7d98a4f6bd1d97cd28d234ea6740c0b9b6
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2023-05-24 16:17:37 -0400

    frame 7 can spawn

--- fighters/duckhunt/src/acmd/specials.rs
+++ fighters/duckhunt/src/acmd/specials.rs
@@ -30,8 +30,14 @@ unsafe fn duckhunt_special_lw_game(fighter: &mut L2CAgentBase) {
 unsafe fn duckhunt_special_n_game(fighter: &mut L2CAgentBase) {
     let lua_state: u64 = fighter.lua_state_agent;
     let boma = fighter.boma();
-    frame(lua_state, 1.0);
-    FT_MOTION_RATE(fighter, 12.0/(16.0-1.0));
+    FT_MOTION_RATE(fighter, 12.0/(16.0-0.0));
+    frame(lua_state, 8.0);
+    if is_excute(fighter) {
+        if !ArticleModule::is_exist(fighter.module_accessor, *FIGHTER_DUCKHUNT_GENERATE_ARTICLE_CAN) {
+            WorkModule::off_flag(fighter.module_accessor, *FIGHTER_DUCKHUNT_INSTANCE_WORK_ID_FLAG_RELEASE_CAN);
+            ArticleModule::generate_article(fighter.module_accessor, *FIGHTER_DUCKHUNT_GENERATE_ARTICLE_CAN, false, -1);
+        }
+    }
     frame(lua_state, 16.0);
     if is_excute(fighter) {
         WorkModule::on_flag(boma, *FIGHTER_DUCKHUNT_INSTANCE_WORK_ID_FLAG_RELEASE_CAN);
---
+++ fighters/duckhunt/src/status/mod.rs
@@ -0,0 +1,8 @@
+use super::*;
+
+mod special_n;
+
+
+pub fn install() {
+    special_n::install();
+}
