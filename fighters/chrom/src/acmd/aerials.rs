45566485781fd412948fe874551c5281ce8601db
oasisharp <ohussain11@hotmail.com>
2023-06-03 01:28:03 +0100

    Update aerials.rs

--- fighters/chrom/src/acmd/aerials.rs
+++ fighters/chrom/src/acmd/aerials.rs
@@ -80,6 +80,23 @@ unsafe fn chrom_attack_air_f_game(fighter: &mut L2CAgentBase) {
     
 }
 
+#[acmd_script( agent = "chrom", script = "expression_attackairf", category = ACMD_EXPRESSION, low_priority )]
+unsafe fn chrom_attack_air_f_expression(fighter: &mut L2CAgentBase) {
+    let lua_state = fighter.lua_state_agent;
+    let boma = fighter.boma();
+    if is_excute(fighter) {
+        AttackModule::set_attack_reference_joint_id(boma, Hash40::new("sword1"), AttackDirectionAxis(*ATTACK_DIRECTION_Z), AttackDirectionAxis(*ATTACK_DIRECTION_X), AttackDirectionAxis(*ATTACK_DIRECTION_Y));
+    }
+    frame(lua_state, 6.0);
+    if is_excute(fighter) {
+        ControlModule::set_rumble(boma, Hash40::new("rbkind_nohitm"), 0, false, *BATTLE_OBJECT_ID_INVALID as u32);
+    }
+    frame(lua_state, 8.0);
+    if is_excute(fighter) {
+        RUMBLE_HIT(fighter, Hash40::new("rbkind_slashm"), 0);
+    }
+}
+
 #[acmd_script( agent = "chrom", script = "game_attackairb" , category = ACMD_GAME , low_priority)]
 unsafe fn chrom_attack_air_b_game(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
@@ -189,6 +206,7 @@ pub fn install() {
     install_acmd_scripts!(
         chrom_attack_air_n_game,
         chrom_attack_air_f_game,
+        chrom_attack_air_f_expression,
         chrom_attack_air_b_game,
         chrom_attack_air_hi_game,
         chrom_attack_air_lw_game,
