0a72ea557e0c0994bf6a9bd039853b85b3039f67
oasisharp <ohussain11@hotmail.com>
2023-06-04 01:29:14 +0100

    finalised

--- fighters/captain/src/acmd/aerials.rs
+++ fighters/captain/src/acmd/aerials.rs
@@ -239,6 +239,21 @@ unsafe fn captain_attack_air_hi_game(fighter: &mut L2CAgentBase) {
     
 }
 
+#[acmd_script( agent = "captain", script = "expression_attackairhi", category = ACMD_EXPRESSION, low_priority )]
+unsafe fn captain_attack_air_hi_expression(fighter: &mut L2CAgentBase) {
+    let lua_state = fighter.lua_state_agent;
+    let boma = fighter.boma();
+    frame(lua_state, 4.0);
+    if is_excute(fighter) {
+        ControlModule::set_rumble(boma, Hash40::new("rbkind_nohitl"), 0, false, *BATTLE_OBJECT_ID_INVALID as u32);
+    }
+    frame(lua_state, 6.0);
+    if is_excute(fighter) {
+        RUMBLE_HIT(fighter, Hash40::new("rbkind_attackm"), 0);
+    }
+}
+
+
 #[acmd_script( agent = "captain", script = "game_attackairlw" , category = ACMD_GAME , low_priority)]
 unsafe fn captain_attack_air_lw_game(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
@@ -278,6 +293,7 @@ pub fn install() {
         captain_attack_air_f_sound,
         captain_attack_air_b_game,
         captain_attack_air_hi_game,
+        captain_attack_air_hi_expression,
         captain_attack_air_lw_game,
         captain_attack_air_n_effect,
         captain_attack_air_hi_effect,
