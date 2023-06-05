cf2d5979a29d222606746054f87447c68fc09be3
oasisharp <ohussain11@hotmail.com>
2023-06-05 23:36:49 +0100

    sped up slightly to match speed of original HDR down throw

--- fighters/lucas/src/acmd/throws.rs
+++ fighters/lucas/src/acmd/throws.rs
@@ -234,14 +234,14 @@ unsafe fn game_throwlw(fighter: &mut L2CAgentBase) {
     let boma = fighter.boma();
     frame(lua_state, 1.0);
     if is_excute(fighter) {
-        FT_MOTION_RATE(fighter, 0.600);
+        FT_MOTION_RATE(fighter, 0.550);
         ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, 0, 7.0, 105, 50, 0, 85, 0.5, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_MAGIC, *ATTACK_REGION_THROW);
         ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH, 0, 7.0, 361, 100, 0, 60, 0.5, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_MAGIC, *ATTACK_REGION_THROW);
     }
 
     frame(lua_state, 34.0);
     if is_excute(fighter){
-        FT_MOTION_RATE(fighter, 0.800);
+        FT_MOTION_RATE(fighter, 0.750);
         CHECK_FINISH_CAMERA(fighter, 9.0, 0.0);
     }
 
