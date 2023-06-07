d2a4bea4d450775779ca2569733301104a048b23
danc <dcidoni01@gmail.com>
2023-06-06 20:50:38 -0400

    backroom requested changes
    Reduce the size of second hit of nair from 11.5 to 10.5
    Increase the active frame of second hit nair by 1 frame

--- fighters/pitb/src/acmd/aerials.rs
+++ fighters/pitb/src/acmd/aerials.rs
@@ -75,9 +75,9 @@ unsafe fn pitb_attack_air_n_game(fighter: &mut L2CAgentBase) {
     frame(lua_state, 24.0);
     FT_MOTION_RATE(fighter, 1.000);
     if is_excute(fighter) {
-        ATTACK(fighter, 1, 1, Hash40::new("top"), 7.0, 361, 85, 0, 50, 11.5, 1.0, 10.0, 3.0, None, None, None, 1.3, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_PALUTENA);
+        ATTACK(fighter, 1, 1, Hash40::new("top"), 7.0, 361, 85, 0, 50, 10.5, 1.0, 10.0, 3.0, None, None, None, 1.3, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_PALUTENA);
     }
-    wait(lua_state, 1.0);
+    wait(lua_state, 2.0);
     if is_excute(fighter) {
         AttackModule::clear_all(boma);
     }
