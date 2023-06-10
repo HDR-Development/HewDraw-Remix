3dfa7c178f23c6429460480e5fd6fecbd0125eec
jaccob101 <jacobking101007@gmail.com>
2023-06-10 12:56:33 -0500

    Fixes

--- fighters/robin/src/status.rs
+++ fighters/robin/src/status.rs
@@ -3,13 +3,10 @@ use globals::*;
 
 #[status_script(agent = "reflet", status = FIGHTER_STATUS_KIND_ATTACK_AIR, condition = LUA_SCRIPT_STATUS_FUNC_INIT_STATUS)]
 pub unsafe fn init_attack_air(fighter: &mut L2CFighterCommon) -> L2CValue {
-    if fighter.is_button_off(Buttons::Smash) {
-        VisibilityModule::set_int64(fighter.module_accessor, Hash40::new("sword").hash as i64, Hash40::new("sword_normal").hash as i64);
-        WorkModule::off_flag(fighter.module_accessor, *FIGHTER_REFLET_INSTANCE_WORK_ID_FLAG_THUNDER_SWORD_ON);
-        0.into()
-    } else {
-        0.into()
-    }
+    VisibilityModule::set_int64(fighter.module_accessor, Hash40::new("sword").hash as i64, Hash40::new("sword_normal").hash as i64);
+    WorkModule::off_flag(fighter.module_accessor, *FIGHTER_REFLET_INSTANCE_WORK_ID_FLAG_THUNDER_SWORD_ON);
+    fighter.sub_attack_air_uniq_process_init();
+    0.into()
 }
 
 pub fn install() {
