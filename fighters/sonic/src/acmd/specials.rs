a7d67f23b97896fde28f9671a2701a574b19f8b9
WuBoytH <alexwuief@gmail.com>
2023-06-23 13:24:41 -0400

    Hitbox Duration Adjustments

--- fighters/sonic/src/acmd/specials.rs
+++ fighters/sonic/src/acmd/specials.rs
@@ -24,10 +24,11 @@ unsafe fn sonic_specialsbooststart_exp(fighter: &mut L2CAgentBase) {
 
 #[acmd_script( agent = "sonic", script = "game_specialsboost", category = ACMD_GAME, low_priority )]
 unsafe fn sonic_specialsboost(fighter: &mut L2CAgentBase) {
+    frame(fighter.lua_state_agent, 3.0);
     if is_excute(fighter) {
         ATTACK(fighter, 0, 0, Hash40::new("top"), 4.0, 361, 100, 0, 45, 4.0, 0.0, 5.5, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_BODY);
     }
-    frame(fighter.lua_state_agent, 7.0);
+    frame(fighter.lua_state_agent, 10.0);
     if is_excute(fighter) {
         ATTACK(fighter, 0, 0, Hash40::new("top"), 1.0, 25, 120, 0, 50, 5.5, 0.0, 7.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_BODY);
         VarModule::on_flag(fighter.battle_object, vars::sonic::status::SPECIAL_S_ENABLE_JUMP);
@@ -71,11 +72,12 @@ unsafe fn sonic_specialsboost_exp(fighter: &mut L2CAgentBase) {
 #[acmd_script( agent = "sonic", script = "game_specialsboostend", category = ACMD_GAME, low_priority )]
 unsafe fn sonic_specialsboostend(fighter: &mut L2CAgentBase) {
     FT_MOTION_RATE(fighter, 0.5);
-    frame(fighter.lua_state_agent, 8.0);
-    FT_MOTION_RATE(fighter, 0.8);
+    frame(fighter.lua_state_agent, 4.0);
     if is_excute(fighter) {
         AttackModule::clear_all(fighter.module_accessor);
     }
+    frame(fighter.lua_state_agent, 8.0);
+    FT_MOTION_RATE(fighter, 0.8);
 }
 
 #[acmd_script( agent = "sonic", script = "effect_specialsboostend", category = ACMD_EFFECT, low_priority )]
@@ -114,7 +116,7 @@ unsafe fn sonic_specialsboostend_exp(fighter: &mut L2CAgentBase) {
 #[acmd_script( agent = "sonic", script = "game_specialairsboostend" , category = ACMD_GAME, low_priority )]
 unsafe fn sonic_specialairsboostend(fighter: &mut L2CAgentBase) {
     FT_MOTION_RATE(fighter, 0.8);
-    frame(fighter.lua_state_agent, 4.0);
+    frame(fighter.lua_state_agent, 2.0);
     if macros::is_excute(fighter) {
         AttackModule::clear_all(fighter.module_accessor);
     }
