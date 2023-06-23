b57b97af8b9d1ec645c4422111bd1c97b01c7ed3
WuBoytH <alexwuief@gmail.com>
2023-06-23 14:01:36 -0400

    Disable both Down or Side Special if either are used

--- fighters/sonic/src/status.rs
+++ fighters/sonic/src/status.rs
@@ -2,10 +2,11 @@ use super::*;
 
 mod dash;
 mod special_s;
+mod special_s_dash;
 mod special_lw_hold;
 
 /// Prevents side b from being used again in air
-unsafe extern "C" fn should_use_special_s_callback(fighter: &mut L2CFighterCommon) -> L2CValue {
+unsafe extern "C" fn should_use_special_callback(fighter: &mut L2CFighterCommon) -> L2CValue {
     if fighter.is_situation(*SITUATION_KIND_AIR) && VarModule::is_flag(fighter.battle_object, vars::sonic::instance::USED_AIR_ACTION) {
         false.into()
     } else {
@@ -27,7 +28,8 @@ fn sonic_init(fighter: &mut L2CFighterCommon) {
     unsafe {
         // set the callbacks on fighter init
         if fighter.kind() == *FIGHTER_KIND_SONIC {
-            fighter.global_table[globals::USE_SPECIAL_S_CALLBACK].assign(&L2CValue::Ptr(should_use_special_s_callback as *const () as _));
+            fighter.global_table[globals::USE_SPECIAL_S_CALLBACK].assign(&L2CValue::Ptr(should_use_special_callback as *const () as _));
+            fighter.global_table[globals::USE_SPECIAL_LW_CALLBACK].assign(&L2CValue::Ptr(should_use_special_callback as *const () as _));
             fighter.global_table[globals::STATUS_CHANGE_CALLBACK].assign(&L2CValue::Ptr(change_status_callback as *const () as _));   
         }
     }
@@ -37,5 +39,6 @@ pub fn install() {
     install_agent_init_callbacks!(sonic_init);
     dash::install();
     special_s::install();
+    special_s_dash::install();
     special_lw_hold::install();
 }
