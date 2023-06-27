f8541f595df69898be475012e80cb3d3ad3672c5
jdipalermo27 <jdipalermo27@gmail.com>
2023-06-27 13:08:01 -0400

    im dumb

--- fighters/robin/src/acmd/other.rs
+++ fighters/robin/src/acmd/other.rs
@@ -280,7 +280,7 @@ unsafe fn robin_thoron_game(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
     let boma = fighter.boma();
     if is_excute(fighter) {
-    ATTACK(fighter, 0, 0, Hash40::new("top"), 6, 45, 145, 0, 70, 3.0, 0.0, 0.0, 0.0, Some(0.0), Some(0.0), Some(0.0), 1.0, 0.5, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 4, true, true, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_MAGIC);
+    ATTACK(fighter, 0, 0, Hash40::new("top"), 5.0, 45, 145, 0, 70, 3.0, 0.0, 0.0, 0.0, Some(0.0), Some(0.0), Some(0.0), 1.0, 0.5, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 4, true, true, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_ELEC, *ATTACK_REGION_MAGIC);
     AttackModule::set_no_finish_camera(boma, 0, true, false);
     }
 }
