795ac6492297a1988bddae05f4bdc98c7715e939
jdipalermo27 <jdipalermo27@gmail.com>
2023-06-27 13:18:19 -0400

    thing that didn't save for some reason

--- fighters/robin/src/status.rs
+++ fighters/robin/src/status.rs
@@ -9,15 +9,8 @@ pub unsafe fn init_attack_air(fighter: &mut L2CFighterCommon) -> L2CValue {
     0.into()
 }
 
-#[status_script(agent = "reflet", status = FIGHTER_STATUS_KIND_SPECIAL_N, condition = LUA_SCRIPT_STATUS_FUNC_INIT_STATUS)]
-pub unsafe fn init_special_n(fighter: &mut L2CFighterCommon) -> L2CValue {
-    VarModule::set_int(fighter.battle_object, vars::robin::instance::THUNDER_BAR, fighter.get_int(*FIGHTER_REFLET_INSTANCE_WORK_ID_INT_SPECIAL_N_CURRENT_POINT));
-    original!(fighter)
-}
-
 pub fn install() {
     install_status_scripts!(
         init_attack_air,
-        init_special_n
     );
 }
