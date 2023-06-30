a90442683a007cc14c93d0e1a2a0cf586a7741a5
Mad Villain <101300494+MadVillainLordQ@users.noreply.github.com>
2023-06-30 17:09:36 -0400

    Dair/Spin Dash tweaks

--- fighters/sonic/src/acmd/aerials.rs
+++ fighters/sonic/src/acmd/aerials.rs
@@ -89,7 +89,7 @@ unsafe fn sonic_attack_air_f_effect(fighter: &mut L2CAgentBase) {
         LAST_EFFECT_SET_COLOR(fighter, 3.0, 0.15, 0.15);
         LAST_EFFECT_SET_RATE(fighter, 2.0);
     }
-    frame(lua_state, 13.0);
+    frame(lua_state, 12.0);
     if is_excute(fighter) {
         EFFECT_FOLLOW_ALPHA(fighter, Hash40::new_raw(0x1156ac182a), Hash40::new("footr"), 0, 0, 0, 0, 0, 0, 1.3, true, 0.9);
         LAST_EFFECT_SET_RATE(fighter, 2);
@@ -240,9 +240,8 @@ unsafe fn sonic_attack_air_hi_effect(fighter: &mut L2CAgentBase) {
 unsafe fn sonic_attack_air_lw_game(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
     let boma = fighter.boma();
-    frame(lua_state, 1.0);
-    FT_MOTION_RATE(fighter, 0.5);
-    frame(lua_state, 4.0);
+    FT_MOTION_RATE(fighter, 10.0/(16.0-0.0));
+    frame(lua_state, 6.0);
     if is_excute(fighter) {
         WorkModule::on_flag(boma, *FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING);
     }
@@ -270,7 +269,7 @@ unsafe fn sonic_attack_air_lw_game(fighter: &mut L2CAgentBase) {
         ATTACK(fighter, 0, 0, Hash40::new("top"), 6.0, 361, 75, 0, 45, 4.3, -4.0, 6.0, 1.8, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
     }
     frame(lua_state, 33.0);
-    FT_MOTION_RATE(fighter, 13.0/(49.0-33.0));
+    FT_MOTION_RATE(fighter, 14.0/(49.0-33.0));
     if is_excute(fighter) {
         AttackModule::clear_all(boma);
     }
