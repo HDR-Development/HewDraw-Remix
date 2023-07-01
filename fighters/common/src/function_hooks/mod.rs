e6056627aecd885f8b58ebff1b2d822713caf0e6
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2023-06-30 23:14:56 -0400

    fix elec hitlag impl

--- fighters/common/src/function_hooks/mod.rs
+++ fighters/common/src/function_hooks/mod.rs
@@ -662,7 +662,7 @@ unsafe fn status_module__change_status(status_module: *const u64, status_kind_ne
 // Only extra elec hitlag for hit character
 #[skyline::hook(offset = 0x406804, inline)]
 unsafe fn change_elec_hitlag_for_attacker(ctx: &mut skyline::hooks::InlineCtx) {
-  let is_attacker = *ctx.registers[2].w.as_ref() & 1 != 0;
+  let is_attacker = *ctx.registers[4].w.as_ref() & 1 == 0;
   if *ctx.registers[8].x.as_ref() == smash::hash40("collision_attr_elec") && is_attacker {
     *ctx.registers[8].x.as_mut() = smash::hash40("collision_attr_normal");
   }
