043af0efda76333e55d98524852f05b3db5b43d3
C# <afr0man005@gmail.com>
2023-07-05 20:04:56 -0700

    2head implementation

--- fighters/sonic/src/status/special_hi.rs
+++ fighters/sonic/src/status/special_hi.rs
@@ -7,7 +7,7 @@ pub fn install() {
       pre_special_hi,
 
       pre_special_hi_jump,
-      exec_special_hi_jump
+      exec_special_hi_jump,
   );
 }
 
@@ -48,9 +48,9 @@ pub unsafe fn pre_special_hi(fighter: &mut L2CFighterCommon) -> L2CValue {
 pub unsafe fn pre_special_hi_jump(fighter: &mut L2CFighterCommon) -> L2CValue {
 	StatusModule::init_settings(
         fighter.module_accessor,
-        SituationKind(*SITUATION_KIND_NONE),
+        SituationKind(*SITUATION_KIND_AIR),
         *FIGHTER_KINETIC_TYPE_SONIC_SPECIAL_HI_JUMP,
-        *GROUND_CORRECT_KIND_NONE as u32,
+        *GROUND_CORRECT_KIND_AIR as u32,
         GroundCliffCheckKind(*GROUND_CLIFF_CHECK_KIND_NONE),
         true,
         *FIGHTER_STATUS_WORK_KEEP_FLAG_SONIC_SPECIAL_HI_FLAG,
@@ -70,15 +70,18 @@ pub unsafe fn pre_special_hi_jump(fighter: &mut L2CFighterCommon) -> L2CValue {
         *FIGHTER_POWER_UP_ATTACK_BIT_SPECIAL_HI as u32,
         0
     );
+    GroundModule::set_attach_ground(fighter.module_accessor, false);
     0.into()
 }
 
 #[status_script(agent = "sonic", status =  FIGHTER_SONIC_STATUS_KIND_SPECIAL_HI_JUMP, condition = LUA_SCRIPT_STATUS_FUNC_EXEC_STATUS)]
 pub unsafe fn exec_special_hi_jump(fighter: &mut L2CFighterCommon) -> L2CValue {
     let boma = fighter.boma();
-    let speed_y = KineticModule::get_sum_speed_y(boma, *FIGHTER_KINETIC_ENERGY_ID_GRAVITY);
-    if speed_y <= 0.0 {
-        GroundModule::correct(boma, app::GroundCorrectKind(*GROUND_CORRECT_KIND_AIR));
+    if fighter.motion_frame() > 2.0 {
+        GroundModule::set_correct(fighter.module_accessor, GroundCorrectKind(*GROUND_CORRECT_KIND_AIR));
+    }
+    else{
+        StatusModule::set_situation_kind(fighter.module_accessor, SituationKind(*SITUATION_KIND_AIR), false);
     }
     return 0.into()
 }
