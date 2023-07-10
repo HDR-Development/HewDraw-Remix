d34d377a720a2a9655fd8e0976b2a32cf01eba5a
Elias Michaias <emskeirik@gmail.com>
2023-07-10 19:46:19 -0400

    Update ground.rs

--- fighters/ganon/src/acmd/ground.rs
+++ fighters/ganon/src/acmd/ground.rs
@@ -25,6 +25,7 @@ unsafe fn ganon_attack_11_game(fighter: &mut L2CAgentBase) {
 unsafe fn ganon_attack_dash_game(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
     let boma = fighter.boma();
+    sv_kinetic_energy!(set_speed_mul, fighter, FIGHTER_KINETIC_ENERGY_ID_MOTION, 0.89);
     frame(lua_state, 10.0);
     if is_excute(fighter) {
         ATTACK(fighter, 0, 0, Hash40::new("top"), 15.0, 70, 85, 0, 50, 7.0, 0.0, 9.0, 7.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_HEAVY, *ATTACK_REGION_BODY);
