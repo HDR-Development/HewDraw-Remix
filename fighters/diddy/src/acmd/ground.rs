75c30f9a9e78ec2cc7902b81f908a152ce1acec2
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2023-07-22 19:26:56 -0400

    dash attack adjustment

--- fighters/diddy/src/acmd/ground.rs
+++ fighters/diddy/src/acmd/ground.rs
@@ -65,28 +65,23 @@ unsafe fn diddy_attack_dash(fighter: &mut L2CAgentBase) {
         VarModule::on_flag(fighter.battle_object, vars::common::status::ATTACK_DASH_ENABLE_AIR_FALL);
         VarModule::on_flag(fighter.battle_object, vars::common::status::ATTACK_DASH_ENABLE_AIR_CONTINUE);
     }
-  
     frame(lua_state, 5.0);
     if is_excute(fighter){
         ATTACK(fighter, 0, 0, Hash40::new("top"), 9.0, 75, 80, 0, 55, 3.5, 0.0, 11.0, 8.5, None, None, None, 0.5, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
         ATTACK(fighter, 1, 0, Hash40::new("top"), 9.0, 75, 80, 0, 55, 3.5, 0.0, 11.0, 8.5, None, None, None, 0.5, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
         ATTACK(fighter, 2, 0, Hash40::new("top"), 9.0, 75, 80, 0, 55, 3.5, 0.0, 5.5, 7.5, None, None, None, 0.5, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
-}
-frame(lua_state, 10.0);
-if is_excute(fighter){
+    }
+    frame(lua_state, 10.0);
+    if is_excute(fighter){
         ATTACK(fighter, 0, 0, Hash40::new("top"), 5.0, 90, 80, 0, 35, 3.5, 0.0, 11.0, 8.5, None, None, None, 0.5, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
         ATTACK(fighter, 1, 0, Hash40::new("top"), 5.0, 90, 80, 0, 35, 3.5, 0.0, 11.0, 8.5, None, None, None, 0.5, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
         ATTACK(fighter, 2, 0, Hash40::new("top"), 5.0, 90, 80, 0, 35, 3.5, 0.0, 5.5, 7.5, None, None, None, 0.5, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 1, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
-}
-wait(lua_state, 15.0);
-if is_excute(fighter){
-	AttackModule::clear_all(boma);
-}
-frame(lua_state,  23.0);
-    if is_excute(fighter) {
+    }
+    wait(lua_state, 15.0);
+    if is_excute(fighter){
+        AttackModule::clear_all(boma);
         VarModule::on_flag(fighter.battle_object, vars::common::status::ATTACK_DASH_ENABLE_AIR_DRIFT);
     }
-
     frame(lua_state,  26.0);
     if is_excute(fighter) {
         FT_MOTION_RATE(fighter, 0.5);
@@ -95,7 +90,49 @@ frame(lua_state,  23.0);
     if is_excute(fighter) {
         FT_MOTION_RATE(fighter, 1.0);
     }
+}
 
+#[acmd_script( agent = "diddy", script = "effect_attackdash", category = ACMD_EFFECT, low_priority )]
+unsafe fn effect_attackdash(fighter: &mut L2CAgentBase) {
+    let lua_state = fighter.lua_state_agent;
+    let boma = fighter.boma();
+    frame(lua_state, 6.0);
+    if is_excute(fighter) {
+        if boma.is_situation(*SITUATION_KIND_GROUND) {
+            FOOT_EFFECT(fighter, Hash40::new("sys_atk_smoke"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
+        }
+    }
+    frame(lua_state, 8.0);
+    frame(lua_state, 9.0);
+    if is_excute(fighter) {
+        EFFECT_FLIP_ALPHA(fighter, Hash40::new("sys_spin_wind_s"), Hash40::new("sys_spin_wind_s"), Hash40::new("top"), 1, 8, 1.5, 180, -330, 110, 1.37, 0, 0, 0, 0, 0, 0, true, *EF_FLIP_YZ, 0.8);
+        LAST_EFFECT_SET_RATE(fighter, 1.5);
+    }
+    frame(lua_state, 11.0);
+    if is_excute(fighter) {
+        EFFECT_OFF_KIND(fighter, Hash40::new("sys_spin_wind_s"), true, true);
+    }
+    frame(lua_state, 17.0);
+    if is_excute(fighter) {
+        EFFECT_FLIP_ALPHA(fighter, Hash40::new("sys_spin_wind_s"), Hash40::new("sys_spin_wind_s"), Hash40::new("top"), 2, 8, 0, 171, -323, 80, 1.6, 0, 0, 0, 0, 0, 0, true, *EF_FLIP_YZ, 0.8);
+        LAST_EFFECT_SET_RATE(fighter, 1.5);
+    }
+    frame(lua_state, 19.0);
+    if is_excute(fighter) {
+        EFFECT_OFF_KIND(fighter, Hash40::new("sys_spin_wind_s"), true, true);
+    }
+    frame(lua_state, 23.0);
+    if is_excute(fighter) {
+        EFFECT_FOLLOW_FLIP_ALPHA(fighter, Hash40::new("sys_spin_wind_s"), Hash40::new("sys_spin_wind_s"), Hash40::new("top"), 0, 7, -1, 180, -315, 90, 1.6, true, *EF_FLIP_YZ, 0.8);
+        LAST_EFFECT_SET_RATE(fighter, 1.95);
+    }
+    frame(lua_state, 26.0);
+    if is_excute(fighter) {
+        EFFECT_OFF_KIND(fighter, Hash40::new("sys_spin_wind_s"), true, true);
+        if boma.is_situation(*SITUATION_KIND_GROUND) {
+            LANDING_EFFECT(fighter, Hash40::new("sys_v_smoke_a"), Hash40::new("top"), 10, 0, 0, 0, 0, 0, 0.8, 0, 0, 0, 0, 0, 0, false);
+        }
+    }
 }
 
 pub fn install() {
@@ -104,7 +141,7 @@ pub fn install() {
         diddy_attack_12,
         diddy_attack_13,
         diddy_attack_dash,
-
+        effect_attackdash,
     );
 }
 
