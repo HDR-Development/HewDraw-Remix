26c074ac13e3079347c48ec2cd2546c85aa0be68
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2023-07-25 21:16:42 -0400

    bugfixes/cleanup

--- fighters/common/src/general_statuses/catch/catchattack.rs
+++ fighters/common/src/general_statuses/catch/catchattack.rs
@@ -17,16 +17,22 @@ fn nro_hook(info: &skyline::nro::NroInfo) {
 
 #[skyline::hook(replace = smash::lua2cpp::L2CFighterCommon_status_CatchAttack_Main)]
 unsafe fn status_CatchAttack_Main(fighter: &mut L2CFighterCommon) -> L2CValue {
-    if MotionModule::is_end(fighter.module_accessor)
-    || CancelModule::is_enable_cancel(fighter.module_accessor) {
+    if StatusModule::is_changing(fighter.module_accessor) {
+        MotionModule::set_rate(fighter.module_accessor, 0.5);
+    }
+    // Your pummel FAF is equal to your animation's length, with a cap at 20f
+    // which is a range of 15-20f castwide
+    if fighter.global_table[CURRENT_FRAME].get_i32() as f32 >= MotionModule::end_frame(fighter.module_accessor).min(20.0) {
         if fighter.CatchCont().get_bool() {
             return 0.into();
         }
-        if fighter.global_table[SITUATION_KIND] != SITUATION_KIND_AIR {
+    }
+    if fighter.global_table[SITUATION_KIND] != SITUATION_KIND_AIR {
+        if MotionModule::is_end(fighter.module_accessor) { 
             fighter.change_status_req(*FIGHTER_STATUS_KIND_CATCH_WAIT, false);
         }
     }
-    if fighter.global_table[SITUATION_KIND] == SITUATION_KIND_AIR {
+    else {
         fighter.change_status_req(*FIGHTER_STATUS_KIND_CATCH_JUMP, false);
     }
     0.into()
