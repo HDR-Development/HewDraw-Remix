3ad0e6a031c114d1a8fb43934bdb6ad598e8b17f
SirTanknSpank <115139386+SirTanknSpank@users.noreply.github.com>
2023-07-31 03:27:55 -0500

    tank forgor

--- scripts/moveFiles.bat
+++
@@ -1,3 +0,0 @@
-robocopy E:\HewDraw-Remix\build\hdr-ryujinx\sdcard E:\RyuJinx\publish\portable\sdcard /E /MOVE
-rmdir /s /q E:\RyuJinx\publish\portable\sdcard\ultimate\mods\hdr
-ren E:\RyuJinx\publish\portable\sdcard\ultimate\mods\hdr-dev hdr
