af3f9bf8309b0a2da434855356514e00a0fd4d86
Paul Joseph Canavan <ssbmotobug@gmail.com>
2023-08-08 00:15:52 -0400

    Pac-Man changes
    Multiple changes to make Pac a bit more rushdown and allow for more kill opportunities than raw forward smash/bair/key
    
    + Nair sweetspot reworked to be a combo finisher and not as similar to fair (Damage 10>12, KBG 70>95)
    + Bair KBG 115>125
    + Hyrant health reduced 16>10
    + Run speed 1.6>1.7
    + Fall speed 1.655>1.72
    + Up smash KBG 102>112
    
    (new to HDR modding so feel free to point out if I messed something up lol)

--- fighters/pacman/src/acmd/smashes.rs
+++ fighters/pacman/src/acmd/smashes.rs
@@ -62,7 +62,7 @@ unsafe fn pacman_attack_hi4_game(fighter: &mut L2CAgentBase) {
     }
     frame(lua_state, 15.0);
     if is_excute(fighter) {
-        ATTACK(fighter, 0, 0, Hash40::new("ghost1"), 14.0, 83, 102, 0, 32, 7.0, 0.0, 5.8, 0.0, Some(0.0), Some(5.5), Some(0.0), 1.0, 1.0, *ATTACK_SETOFF_KIND_THRU, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_PUNCH, *ATTACK_REGION_NONE);
+        ATTACK(fighter, 0, 0, Hash40::new("ghost1"), 14.0, 83, 112, 0, 32, 7.0, 0.0, 5.8, 0.0, Some(0.0), Some(5.5), Some(0.0), 1.0, 1.0, *ATTACK_SETOFF_KIND_THRU, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_PUNCH, *ATTACK_REGION_NONE);
     }
     wait(lua_state, 3.0);
     if is_excute(fighter) {
