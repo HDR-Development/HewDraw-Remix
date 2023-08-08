1983b3dae6942ee4ffa5b716e37776a6a7e54708
Zarek Syed <zarek.syed@gmail.com>
2023-08-08 05:14:38 -0400

    Kirby with Ken's aerial Hadouken
    + extra checks for Ryu and Ken for Kirby to accidentally not gain meter
    
    Note: Kirby can perform true Hadouken with A and B but Ken can only with B.

--- fighters/kirby/src/opff.rs
+++ fighters/kirby/src/opff.rs
@@ -922,6 +922,42 @@ unsafe fn master_nspecial_cancels(boma: &mut BattleObjectModuleAccessor, status_
     }
 }
 
+// Ken
+unsafe fn ken_air_hado_distinguish(fighter: &mut L2CFighterCommon, boma: &mut BattleObjectModuleAccessor, frame: f32) {
+    if !boma.is_status_one_of(&[
+        *FIGHTER_KIRBY_STATUS_KIND_RYU_SPECIAL_N,
+        *FIGHTER_KIRBY_STATUS_KIND_RYU_SPECIAL_N_COMMAND,
+        *FIGHTER_KIRBY_STATUS_KIND_RYU_SPECIAL_N2_COMMAND,
+        *FIGHTER_KIRBY_STATUS_KIND_KEN_SPECIAL_N,
+        *FIGHTER_KIRBY_STATUS_KIND_KEN_SPECIAL_N_COMMAND,
+        *FIGHTER_KIRBY_STATUS_KIND_KEN_SPECIAL_N2_COMMAND,
+    ]) {
+        return;
+    }
+
+    // set VarModule flag on f12 - this flag changes hado properties
+    if frame == 12.0 && fighter.is_motion_one_of(&[
+        Hash40::new("ken_special_air_n"),
+    ]) {
+        VarModule::on_flag(fighter.battle_object, vars::shotos::instance::IS_CURRENT_HADOKEN_AIR);
+    }
+    // after frame 13, disallow changing from aerial to grounded hadoken
+    // instead, we enter a landing animation
+    if (frame > 13.0 || fighter.is_motion_one_of(&[
+        Hash40::new("ken_special_air_n_empty"),
+        Hash40::new("ken_special_n_empty"),
+    ]))
+    && boma.is_situation(*SITUATION_KIND_GROUND)
+    && boma.is_prev_situation(*SITUATION_KIND_AIR) {
+        if frame < 70.0 { // the autocancel frame
+            WorkModule::set_float(boma, 11.0, *FIGHTER_INSTANCE_WORK_ID_FLOAT_LANDING_FRAME);
+            boma.change_status_req(*FIGHTER_STATUS_KIND_LANDING_FALL_SPECIAL, false);
+        } else {
+            boma.change_status_req(*FIGHTER_STATUS_KIND_WAIT, false);
+        }
+    }
+}
+
 unsafe fn fastfall_specials(fighter: &mut L2CFighterCommon) {
     let copystatus = StatusModule::status_kind(fighter.module_accessor);
     if !fighter.is_in_hitlag()
@@ -1342,6 +1378,7 @@ pub unsafe fn moveset(fighter: &mut L2CFighterCommon, boma: &mut BattleObjectMod
     edge_nspecial_cancels(boma, status_kind, situation_kind, cat[2]);
     miigunner_nspecial_cancels(boma, status_kind, situation_kind, cat[1], cat[2]);
     master_nspecial_cancels(boma, status_kind, situation_kind);
+    ken_air_hado_distinguish(fighter, boma, frame);
 }
 
 #[utils::macros::opff(FIGHTER_KIND_KIRBY )]
