5f8e2e4bcf7ae929768f0aea070318af15b1f543
Nakedkoops <115908645+Nakedkoops@users.noreply.github.com>
2023-08-07 21:14:30 -0400

    updates
    if you do galaxy spin you cannot do wall jump up b and if you do wall jump up b you can’t do galaxy spin

--- fighters/mario/src/opff.rs
+++ fighters/mario/src/opff.rs
@@ -84,12 +84,14 @@ unsafe fn up_b_wall_jump(fighter: &mut L2CFighterCommon, boma: &mut BattleObject
                         if boma.is_cat_flag(Cat1::TurnDash) {
                             VarModule::on_flag(boma.object(), vars::common::instance::SPECIAL_WALL_JUMP);
                             StatusModule::change_status_request_from_script(boma, *FIGHTER_STATUS_KIND_WALL_JUMP, true);
+                            VarModule::off_flag(fighter.battle_object, vars::mario::instance::DISABLE_DSPECIAL_STALL);
                         }
                     }
                     if GroundModule::is_wall_touch_line(boma, *GROUND_TOUCH_FLAG_LEFT_SIDE as u32) {
                         if boma.is_cat_flag(Cat1::TurnDash) {
                             VarModule::on_flag(boma.object(), vars::common::instance::SPECIAL_WALL_JUMP);
                             StatusModule::change_status_request_from_script(boma, *FIGHTER_STATUS_KIND_WALL_JUMP, true);
+                            VarModule::off_flag(fighter.battle_object, vars::mario::instance::DISABLE_DSPECIAL_STALL);
                         }
                     }
                 }
@@ -163,6 +165,7 @@ unsafe fn galaxy_spin_rise(fighter: &mut L2CFighterCommon, boma: &mut BattleObje
                 }
                 if frame >= 50.0 {
                     VarModule::off_flag(fighter.battle_object, vars::mario::instance::DISABLE_DSPECIAL_STALL);
+                    VarModule::on_flag(boma.object(), vars::common::instance::SPECIAL_WALL_JUMP);
                 }
             }  
         }
