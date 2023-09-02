75ee930e8a89ec506e0e0cf8b2648baccf31657b
Odd-Rick <130805011+Odd-Rick@users.noreply.github.com>
2023-09-02 15:42:41 -0700

    Update throws.rs

--- fighters/ganon/src/acmd/throws.rs
+++ fighters/ganon/src/acmd/throws.rs
@@ -1,9 +1,30 @@
 
 use super::*;
 
+#[acmd_script( agent = "ganon", script = "game_throwlw", category = ACMD_GAME, low_priority )]
+unsafe fn game_throwlw(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    if is_excute(agent) {
+        ATTACK_ABS(agent, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, 0, 7.0, 80, 45, 0, 80, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
+        ATTACK_ABS(agent, *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH, 0, 3.0, 361, 100, 0, 60, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
+    }
+    frame(lua_state, 22.0);
+    if is_excute(agent) {
+        CHECK_FINISH_CAMERA(agent, 11, 0);
+    }
+    frame(lua_state, 23.0);
+    if is_excute(agent) {
+        let target = WorkModule::get_int64(boma, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_OBJECT);
+        let target_group = WorkModule::get_int64(boma, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_GROUP);
+        let target_no = WorkModule::get_int64(boma, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_NO);
+        ATK_HIT_ABS(agent, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, Hash40::new("throw"), target, target_group, target_no);
+    }
+}
 
 pub fn install() {
     install_acmd_scripts!(
+        game_throwlw,
     );
 }
 
