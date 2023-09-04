5d7a91e12f3e59e910db41a9d0c1260e03bfeadc
Odd-Rick <130805011+Odd-Rick@users.noreply.github.com>
2023-09-04 11:06:47 -0700

    Update throws.rs

--- fighters/captain/src/acmd/throws.rs
+++ fighters/captain/src/acmd/throws.rs
@@ -2,6 +2,27 @@
 use super::*;
 
 
+#[acmd_script( agent = "captain", script = "game_throwlw", category = ACMD_GAME, low_priority )]
+unsafe fn game_throwlw(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    if is_excute(agent) {
+        ATTACK_ABS(agent, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, 0, 6.0, 63, 34, 0, 75, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
+        ATTACK_ABS(agent, *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH, 0, 3.0, 361, 100, 0, 60, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
+    }
+    frame(lua_state, 14.0);
+    if is_excute(agent) {
+        CHECK_FINISH_CAMERA(agent, 13, 3);
+    }
+    frame(lua_state, 21.0);
+    if is_excute(agent) {
+        let target = WorkModule::get_int64(boma, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_OBJECT);
+        let target_group = WorkModule::get_int64(boma, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_GROUP);
+        let target_no = WorkModule::get_int64(boma, *FIGHTER_STATUS_THROW_WORK_INT_TARGET_HIT_NO);
+        ATK_HIT_ABS(agent, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, Hash40::new("throw"), target, target_group, target_no);
+    }
+}
+
 #[acmd_script( agent = "captain", script = "game_throwhi" , category = ACMD_GAME , low_priority)]
 unsafe fn game_throwhi(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
@@ -28,6 +49,7 @@ unsafe fn game_throwhi(fighter: &mut L2CAgentBase) {
 
 pub fn install() {
     install_acmd_scripts!(
+        game_throwlw,
         game_throwhi,
     );
 }
