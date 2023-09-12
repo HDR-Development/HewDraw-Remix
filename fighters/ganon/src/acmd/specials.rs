a6d52e3e98eb969cc64ddc3227fb2751a1506edc
Odd-Rick <130805011+Odd-Rick@users.noreply.github.com>
2023-09-11 18:45:44 -0700

    Update specials.rs

--- fighters/ganon/src/acmd/specials.rs
+++ fighters/ganon/src/acmd/specials.rs
@@ -129,9 +129,9 @@ unsafe fn game_specialairsstart(agent: &mut L2CAgentBase) {
     if is_excute(agent) {
         ATTACK_ABS(agent, *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH, 0, 4.0, 0, 10, 0, 100, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_NONE);
     }
-    frame(lua_state, 16.0);
+    frame(lua_state, 15.0);
     if is_excute(agent) {
-        ATTACK(agent, 1, 0, Hash40::new("top"), 0.0, 350, 100, 30, 0, 6.5, 0.0, 10.0, 3.0, Some(0.0), Some(7.0), Some(5.0), 0.0, 0.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, true, true, false, *COLLISION_SITUATION_MASK_GA_d, *COLLISION_CATEGORY_MASK_FEB, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_NONE);
+        ATTACK(agent, 1, 0, Hash40::new("top"), 0.0, 350, 100, 30, 0, 6.5, 0.0, 13.0, 3.0, Some(0.0), Some(7.0), Some(5.0), 0.0, 0.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, true, true, false, *COLLISION_SITUATION_MASK_GA_d, *COLLISION_CATEGORY_MASK_FEB, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_NONE);
     }
     frame(lua_state, 1.0);
     FT_MOTION_RATE(agent, 0.8);
