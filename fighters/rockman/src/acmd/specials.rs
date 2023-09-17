9736bbebe9dcdbeb069aa51035d41022d73bfbf4
WuBoytH <alexwuief@gmail.com>
2023-09-17 19:37:18 -0400

    Nerfs Charge Shot KB

--- fighters/rockman/src/acmd/specials.rs
+++ fighters/rockman/src/acmd/specials.rs
@@ -134,12 +134,12 @@ unsafe fn rockman_chargeshot_regular(agent: &mut L2CAgentBase) {
         if is_charge_max {
             damage = 15.0;
             bkb = 40;
-            kbg = 90;
+            kbg = 75;
         }
         else {
             damage = 9.0;
             bkb = 50;
-            kbg = 85;
+            kbg = 70;
         }
         macros::ATTACK(agent, 0, 0, Hash40::new("top"), damage, 361, kbg, 0, bkb, 2.6, 0.0, 0.0, 0.0, None, None, None, 0.3, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_SPEED, false, 0, 0.0, 0, true, true, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_ENERGY);
         macros::ATK_SET_SHIELD_SETOFF_MUL(agent, 0, 0.32);
