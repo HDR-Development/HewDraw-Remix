80cc965d44389591d9744c0e97d960dfaecd3f70
TeamPearson <99865721+TeamPearson@users.noreply.github.com>
2023-09-25 14:19:54 -0500

    Up smash FAF and cleanup

--- fighters/younglink/src/acmd/smashes.rs
+++ fighters/younglink/src/acmd/smashes.rs
@@ -95,9 +95,8 @@ unsafe fn younglink_attack_hi4_game (fighter: &mut L2CAgentBase) {
 	if is_excute(fighter) {
 		WorkModule::on_flag(fighter.module_accessor, /*Flag*/ *FIGHTER_STATUS_ATTACK_FLAG_START_SMASH_HOLD);
 	}
-    //frame(lua_state, 10.0);
-    //FT_MOTION_RATE(fighter, 1.0);
     frame(lua_state, 10.0);
+    //FT_MOTION_RATE(fighter, 1.0);
 	if is_excute(fighter) {
 		ATTACK(fighter, 0, 0, Hash40::new("colonells"), 4.0, 105, 100, 33, 0, 3.0, 0.0, 0.0, 0.0, None, None, None, 0.8, 0.4, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, true, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
 		ATTACK(fighter, 1, 0, Hash40::new("sword"), 4.0, 95, 100, 23, 0, 3.2, 0.0, -1.0, 1.0, None, None, None, 0.8, 0.4, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, true, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_A, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
@@ -120,6 +119,37 @@ unsafe fn younglink_attack_hi4_game (fighter: &mut L2CAgentBase) {
 	if is_excute(fighter) {
 		AttackModule::clear_all(fighter.module_accessor);
 	}
+    frame(lua_state,50.0);
+    FT_MOTION_RATE(fighter,7.0/(50.0-40.0));
+
+}
+
+#[acmd_script( agent = "younglink", script = "sound_attackhi4", category = ACMD_SOUND, low_priority )]
+unsafe fn sound_attackhi4(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    frame(lua_state, 6.0);
+    if is_excute(agent) {
+        STOP_SE(agent, Hash40::new("se_common_smash_start_02"));
+    }
+    wait(lua_state, 4.0);
+    if is_excute(agent) {
+        PLAY_SE(agent, Hash40::new("vc_younglink_attack04"));
+        PLAY_SE(agent, Hash40::new("se_common_smashswing_03"));
+        PLAY_SE(agent, Hash40::new("se_younglink_swing_ll"));
+    }
+    wait(lua_state, 15.0);
+    if is_excute(agent) {
+        PLAY_SE(agent, Hash40::new("se_younglink_swing_ll"));
+    }
+   // wait(lua_state, 15.0);
+    //if is_excute(agent) {
+    //    PLAY_SE(agent, Hash40::new("se_younglink_swing_ll"));
+    //}
+    //wait(lua_state, 16.0);
+    //if is_excute(agent) {
+    //    PLAY_SE(agent, Hash40::new("se_younglink_smash_h01"));
+   // }
 }
 
 pub fn install() {
@@ -127,7 +157,8 @@ pub fn install() {
         younglink_attack_s4_s_game,
         younglink_attack_s4_s2_game,
         younglink_attack_lw4_game,
-        younglink_attack_hi4_game
+        younglink_attack_hi4_game,
+        sound_attackhi4
     );
 }
 
