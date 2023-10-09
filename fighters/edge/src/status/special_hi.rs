42a8f41cef06ec0709aae35363818539dba2dfad
black-calculus <52509873+black-calculus@users.noreply.github.com>
2023-10-08 23:46:33 -0700

    Initial dash, Blade Dash movement multiplier, dthrow

--- fighters/edge/src/status/special_hi.rs
+++ fighters/edge/src/status/special_hi.rs
@@ -342,7 +342,7 @@ pub unsafe fn edge_special_hi_rush_main(fighter: &mut L2CFighterCommon) -> L2CVa
         let stopEnergy = KineticModule::get_energy(fighter.module_accessor, *FIGHTER_KINETIC_ENERGY_ID_STOP) as *mut app::KineticEnergyNormal;
         let mut movement_mul = 1.0;
         if VarModule::is_flag(fighter.battle_object, vars::edge::instance::SPECIAL_HI_BLADE_DASH_NO_HITBOX) {
-            movement_mul = 0.5;
+            movement_mul = 0.65;
         }
         let vec2 = Vector2f{x: rush_speed * dir_x * movement_mul, y: 0.0};
         app::lua_bind::KineticEnergyNormal::set_speed(stopEnergy, &vec2);
