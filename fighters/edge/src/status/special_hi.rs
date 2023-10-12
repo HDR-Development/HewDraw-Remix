43101d8fd67cf59b478cc5e50013db0d8d900fd6
black-calculus <52509873+black-calculus@users.noreply.github.com>
2023-10-12 14:28:43 -0700

    Fix holding down during Blade Dash fakeout traveling full distance

--- fighters/edge/src/status/special_hi.rs
+++ fighters/edge/src/status/special_hi.rs
@@ -347,7 +347,7 @@ pub unsafe fn edge_special_hi_rush_main(fighter: &mut L2CFighterCommon) -> L2CVa
     // Grounded Blade Rush shorten mechanic
     let dir_x = WorkModule::get_float(fighter.module_accessor, *FIGHTER_EDGE_STATUS_SPECIAL_HI_FLOAT_DECIDE_DIR_X);
     if fighter.global_table[SITUATION_KIND] == SITUATION_KIND_GROUND
-    && dir_x != 0.0 {
+    && (dir_x != 0.0 || VarModule::is_flag(fighter.battle_object, vars::edge::instance::SPECIAL_HI_BLADE_DASH_NO_HITBOX)){
         let rush_speed = WorkModule::get_param_float(fighter.module_accessor, hash40("param_special_hi"), hash40("rush_speed"));
         let stopEnergy = KineticModule::get_energy(fighter.module_accessor, *FIGHTER_KINETIC_ENERGY_ID_STOP) as *mut app::KineticEnergyNormal;
         let mut movement_mul = 1.0;
