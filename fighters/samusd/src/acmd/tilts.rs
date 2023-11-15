9a4bea1c1a8bc36781ba4fe6e0b837deec5bb7f1
sorenpkt <soren.miller@icloud.com>
2023-11-15 03:22:55 -0800

    Fix f7 pop up hitbox angle

--- fighters/samusd/src/acmd/tilts.rs
+++ fighters/samusd/src/acmd/tilts.rs
@@ -157,7 +157,7 @@ unsafe fn samusd_attack_hi3_game(fighter: &mut L2CAgentBase) {
     //ground only pop up hitbox, f7
     frame(lua_state, 7.0); 
     if is_excute(fighter) {
-        ATTACK(fighter, 0, 0, Hash40::new("top"), 4.0, 100, 95, 70, 0, 5.5, 0.0, 8.0, 4.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
+        ATTACK(fighter, 0, 0, Hash40::new("top"), 4.0, 95, 95, 70, 0, 5.5, 0.0, 8.0, 4.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_G, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
     }
     //the rest of the first kick, f8-10
     frame(lua_state, 8.0); 
