5e375364176bc66ac2f69375bf93449b43196a43
Zarek Syed <zarek.syed@gmail.com>
2023-11-22 15:03:16 -0500

    Even more rumble fixes
    Cloud f smash
    Marth grounded side B 1
    Link dash attack
    Lucina jab 2
    Dark Samus fair, up tilt
    Snake exploding C4 from box

--- fighters/miiswordsman/src/acmd/tilts.rs
+++ fighters/miiswordsman/src/acmd/tilts.rs
@@ -1,45 +1,4 @@
 use super::*;
-// Vanilla animation
-/*
-#[acmd_script( agent = "miiswordsman", script = "game_attacks3" , category = ACMD_GAME , low_priority)]
-unsafe fn attack_s3_game(fighter: &mut L2CAgentBase) {
-    let lua_state = fighter.lua_state_agent;
-    let boma = fighter.boma();
-	if is_excute(fighter) {
-		FighterAreaModuleImpl::enable_fix_jostle_area(boma, 0.0, 4.0);
-	}
-	frame(lua_state, 9.0);
-	if is_excute(fighter) {
-		// Body hitboxes
-		ATTACK(fighter, 0, 0, Hash40::new("shoulderr"), 12.0, 361, 96, 0, 20, 3.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
-		ATTACK(fighter, 1, 0, Hash40::new("armr"), 12.0, 361, 96, 0, 20, 3.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
-		ATTACK(fighter, 2, 0, Hash40::new("haver"), 12.0, 361, 96, 0, 20, 3.5, 0.0, 2.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
-		ATTACK(fighter, 3, 0, Hash40::new("haver"), 12.0, 361, 96, 0, 20, 3.5, 0.0, 6.5, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
-		ATTACK(fighter, 4, 0, Hash40::new("haver"), 15.0, 361, 100, 0, 20, 3.5, 0.0, 13.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
-		// 5 and 6 are copies of 0 and 4 respectively
-		ATTACK(fighter, 5, 0, Hash40::new("shoulderr"), 12.0, 361, 96, 0, 20, 3.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
-		ATTACK(fighter, 6, 0, Hash40::new("haver"), 15.0, 361, 100, 0, 20, 3.5, 0.0, 13.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
-		ATK_SET_SHIELD_SETOFF_MUL_arg3(fighter, 4, 6, 1.6);
-	}
-	frame(lua_state, 10.0);
-	if is_excute(fighter) {
-		// Capsule hitbox
-		ATTACK(fighter, 5, 0, Hash40::new("top"), 12.0, 361, 96, 0, 20, 3.0, 0.0, 5.4, 4.0, Some(0.0), Some(5.4), Some(18.0), 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
-		ATTACK(fighter, 6, 0, Hash40::new("top"), 15.0, 361, 100, 0, 20, 3.0, 0.0, 5.4, 4.0, Some(0.0), Some(5.4), Some(24.0), 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
-		ATK_SET_SHIELD_SETOFF_MUL_arg3(fighter, 4, 6, 1.6);
-	}
-	wait(lua_state, 2.0);
-	if is_excute(fighter) {
-		AttackModule::clear(boma, 5, false);
-		AttackModule::clear(boma, 6, false);
-	}
-	frame(lua_state, 14.0);
-	if is_excute(fighter) {
-		AttackModule::clear_all(boma);
-	}
-
-}
-*/
 
 // New animation
 #[acmd_script( agent = "miiswordsman", script = "game_attacks3" , category = ACMD_GAME , low_priority)]
