363c6618c24c9eabc41637161dd18119316f792d
ncsuddut <ncsuddut@ncsu.edu>
2023-11-25 18:33:04 -0500

    DSpecial reflector

--- fighters/pikmin/src/acmd/specials.rs
+++ fighters/pikmin/src/acmd/specials.rs
@@ -190,6 +190,38 @@ unsafe fn game_spsthrown_y(agent: &mut L2CAgentBase) {
     }
 }
 
+#[acmd_script( agent = "pikmin", scripts = ["game_speciallw", "game_specialairlw"] , category = ACMD_GAME , low_priority)]
+unsafe fn game_speciallw(fighter: &mut L2CAgentBase) {
+    let lua_state = fighter.lua_state_agent;
+    let boma = fighter.boma();
+    frame(lua_state, 1.0);
+    if is_excute(fighter) {
+        WorkModule::on_flag(boma, *FIGHTER_PIKMIN_STATUS_SPECIAL_LW_FLAG_SORT);
+        // damage!(fighter, *MA_MSC_DAMAGE_DAMAGE_NO_REACTION, *DAMAGE_NO_REACTION_MODE_ALWAYS, 0);
+        shield!(fighter, *MA_MSC_CMD_REFLECTOR, *COLLISION_KIND_REFLECTOR, 0, hash40("top"), 7.5, 0.0, 7.0, -8.5, 0.0, 7.0, 8.5, 1.0, 0.8, 50, false, 0.8, *FIGHTER_REFLECTOR_GROUP_HOMERUNBAT);
+    }
+    frame(lua_state, 7.0);
+    if is_excute(fighter) {
+        // damage!(fighter, *MA_MSC_DAMAGE_DAMAGE_NO_REACTION, *DAMAGE_NO_REACTION_MODE_NORMAL, 0);
+        shield!(fighter, *MA_MSC_CMD_SHIELD_OFF, *COLLISION_KIND_REFLECTOR, 0, *FIGHTER_REFLECTOR_GROUP_HOMERUNBAT);
+    }
+}
+
+#[acmd_script( agent = "pikmin", scripts = ["effect_speciallw", "effect_specialairlw"] , category = ACMD_EFFECT , low_priority)]
+unsafe fn effect_speciallw(fighter: &mut L2CAgentBase) {
+    let lua_state = fighter.lua_state_agent;
+    let boma = fighter.boma();
+    frame(lua_state, 0.0);
+    if is_excute(fighter) {
+        EFFECT_FOLLOW(fighter, Hash40::new("pikmin_order"), Hash40::new("s_antenna4"), 0, 0, 0, 0, 0, 0, 1, true);
+    }
+    frame(lua_state, 1.0);
+    if is_excute(fighter) {
+        EFFECT_FLW_POS(fighter, Hash40::new("pikmin_seiretsu"), Hash40::new("top"), 0, 3, 0, 0, 0, 0, 0.55, true);
+        LAST_EFFECT_SET_RATE(fighter, 2.0);
+    }
+}
+
 pub fn install() {
     install_acmd_scripts!(
         pikmin_whistle_damage,
@@ -202,6 +234,8 @@ pub fn install() {
         game_spsthrown_v,
         game_spsthrown_w,
         game_spsthrown_y,
+        game_speciallw,
+        effect_speciallw
     );
 }
 
