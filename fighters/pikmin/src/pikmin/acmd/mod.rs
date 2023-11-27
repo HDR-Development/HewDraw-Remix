a8a45591b899e3a5920878db2805dac1f8f79499
ncsuddut <ncsuddut@ncsu.edu>
2023-11-26 21:37:32 -0500

    readability improvements

--- fighters/pikmin/src/pikmin/acmd/mod.rs
+++ fighters/pikmin/src/pikmin/acmd/mod.rs
@@ -9,74 +9,74 @@ mod throws;
 
 #[repr(C)]
 pub struct PikminInfo {
-    damage: f32,
+    dmg: f32,
     kbg: i32,
     bkb: i32,
-    shield_damage: f32,
+    shield_dmg: f32,
     hitlag: f32,
     attr: &'static str,
     sound: i32,
-    delta_angle: u64,
-    effect_color: Vector3f
+    angle: u64,
+    color: Vector3f
 }
 
 impl From<i32> for PikminInfo {
     fn from(other: i32) -> Self {
         match other {
             0 => PikminInfo { // Red
-                damage: 1.0,
+                dmg: 1.0,
                 kbg: 8,
                 bkb: 0,
-                shield_damage: 0.1,
-                delta_angle: 0,
+                shield_dmg: 0.1,
+                angle: 0,
                 hitlag: 1.1,
                 attr: "collision_attr_fire",
                 sound: *COLLISION_SOUND_ATTR_FIRE,
-                effect_color: Vector3f{x: 1.0, y: 0.05, z: 0.0}
+                color: Vector3f{x: 1.0, y: 0.05, z: 0.0}
             },
             1 => PikminInfo { // yellow
-                damage: 0.75,
+                dmg: 0.75,
                 kbg: 0,
                 bkb: 0,
-                shield_damage: 0.0,
-                delta_angle: 8,
+                shield_dmg: 0.0,
+                angle: 8,
                 hitlag: 1.0,
                 attr: "collision_attr_elec",
                 sound: *COLLISION_SOUND_ATTR_ELEC,
-                effect_color: Vector3f{x: 1.0, y: 1.0, z: 0.14}
+                color: Vector3f{x: 1.0, y: 1.0, z: 0.14}
             },
             2 => PikminInfo { // Blue
-                damage: 0.8,
+                dmg: 0.8,
                 kbg: 0,
                 bkb: 0,
-                shield_damage: 0.0,
-                delta_angle: 5,
+                shield_dmg: 0.0,
+                angle: 5,
                 hitlag: 1.0,
                 attr: "collision_attr_water",
                 sound: *COLLISION_SOUND_ATTR_WATER,
-                effect_color: Vector3f{x: 0.1, y: 0.4, z: 1.0}
+                color: Vector3f{x: 0.1, y: 0.4, z: 1.0}
             },
             3 => PikminInfo { // White
-                damage: 0.6,
+                dmg: 0.6,
                 kbg: 0,
                 bkb: 0,
-                shield_damage: 0.75,
-                delta_angle: 8,
+                shield_dmg: 0.75,
+                angle: 8,
                 hitlag: 1.0,
                 attr: "collision_attr_purple",
                 sound: *COLLISION_SOUND_ATTR_FIRE,
-                effect_color: Vector3f{x: 1.0, y: 1.0, z: 1.0}
+                color: Vector3f{x: 1.0, y: 1.0, z: 1.0}
             },
             _ => PikminInfo { // Violet (Rock), also default
-                damage: 0.8,
+                dmg: 0.8,
                 kbg: 0,
                 bkb: 0,
-                shield_damage: 0.5,
-                delta_angle: 0,
+                shield_dmg: 0.5,
+                angle: 0,
                 hitlag: 1.2,
                 attr: "collision_attr_normal",
                 sound: *COLLISION_SOUND_ATTR_KICK,
-                effect_color: Vector3f{x: 0.36, y: 0.0, z: 1.0}
+                color: Vector3f{x: 0.36, y: 0.0, z: 1.0}
             },
         }
     }
