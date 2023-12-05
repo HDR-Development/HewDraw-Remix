c50262875ff5825b691273d57bfb47270bf36f6c
h3ra-ai2020 <66037428+h3ra-ai2020@users.noreply.github.com>
2023-12-04 21:34:38 -0500

    god

--- fighters/packun/src/status/attack_s4.rs
+++ fighters/packun/src/status/attack_s4.rs
@@ -12,7 +12,7 @@ pub fn install() {
 #[status_script(agent = "packun", status = FIGHTER_STATUS_KIND_ATTACK_S4_START, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_MAIN)]
 unsafe fn attack_s4_start_main(fighter: &mut L2CFighterCommon) -> L2CValue {
     if VarModule::get_int(fighter.object(), vars::packun::instance::CURRENT_STANCE) == 2 {
-        MotionModule::change_motion(boma, Hash40::new("attack_s4_2"), 0.0, 1.0, false, 0.0, false, false);
+        MotionModule::change_motion(fighter.module_accessor, Hash40::new("attack_s4_2"), 0.0, 1.0, false, 0.0, false, false);
     }
     return fighter.status_AttackS4Start();
 }
@@ -20,7 +20,7 @@ unsafe fn attack_s4_start_main(fighter: &mut L2CFighterCommon) -> L2CValue {
 #[status_script(agent = "packun", status = FIGHTER_STATUS_KIND_ATTACK_S4_HOLD, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_MAIN)]
 unsafe fn attack_s4_hold_main(fighter: &mut L2CFighterCommon) -> L2CValue {
     if VarModule::get_int(fighter.object(), vars::packun::instance::CURRENT_STANCE) == 2 {
-        MotionModule::change_motion(boma, Hash40::new("attack_s4_hold_2"), 0.0, 1.0, false, 0.0, false, false);
+        MotionModule::change_motion(fighter.module_accessor, Hash40::new("attack_s4_hold_2"), 0.0, 1.0, false, 0.0, false, false);
     }
     fighter.sub_shift_status_main(L2CValue::Ptr(L2CFighterCommon_bind_address_call_status_AttackS4Hold as *const () as _))
 }
@@ -28,7 +28,7 @@ unsafe fn attack_s4_hold_main(fighter: &mut L2CFighterCommon) -> L2CValue {
 #[status_script(agent = "packun", status = FIGHTER_STATUS_KIND_ATTACK_S4, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_MAIN)]
 unsafe fn attack_s4_main(fighter: &mut L2CFighterCommon) -> L2CValue {
     if VarModule::get_int(fighter.object(), vars::packun::instance::CURRENT_STANCE) == 2 {
-        MotionModule::change_motion(boma, Hash40::new("attack_s4_2"), 24.0, 1.0, false, 0.0, false, false);
+        MotionModule::change_motion(fighter.module_accessor, Hash40::new("attack_s4_2"), 24.0, 1.0, false, 0.0, false, false);
     }
     return fighter.status_AttackS4();
 }
