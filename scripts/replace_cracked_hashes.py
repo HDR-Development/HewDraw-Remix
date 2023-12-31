2a7e2bd43b98e286dbbb43987e6ea6dab27a0747
Zarek Syed <zarek.syed@gmail.com>
2023-12-31 17:52:47 -0500

    XML/YAML functionality added to replace_cracked_hashes.py

--- scripts/replace_cracked_hashes.py
+++ scripts/replace_cracked_hashes.py
@@ -16,34 +16,58 @@ uncrackedHashes = set()
 
 
 for root, dirs, files in os.walk(os.pardir):
-  for file in files:
-    if file.endswith(".rs"):
-        with open(os.path.join(root, file), "r+") as f:
-            changed = False
-            text = f.read()
-            
-            for hashString in re.findall(regex, text):
-                if len(hashString) == 11:
-                    hashString = hashString.replace("0x", "0x0")
+    for file in files:
+        if file.endswith(".rs"):
+            with open(os.path.join(root, file), "r+") as f:
+                changed = False
+                text = f.read()
                 
-                if hashString in paramDict.keys():
-                    changed = True
+                for hashString in re.findall(regex, text):
+                    if len(hashString) == 11:
+                        hashString = hashString.replace("0x", "0x0")
                     
-                    crackedHashes.add(paramDict[hashString])
-                    text = text.replace(hashString + "u64", hashString)
-                    text = text.replace("Hash40::new_raw(" + hashString, "Hash40::new(\"" + paramDict[hashString] + "\"")
+                    if hashString in paramDict.keys():
+                        changed = True
+                        
+                        crackedHashes.add(paramDict[hashString])
+                        text = text.replace(hashString + "u64", hashString)
+                        text = text.replace("Hash40::new_raw(" + hashString, "Hash40::new(\"" + paramDict[hashString] + "\"")
 
-                    withoutLeadingZero = hashString.replace("0x0", "0x")
-                    text = text.replace(withoutLeadingZero + "u64", withoutLeadingZero)
-                    text = text.replace("Hash40::new_raw(" + withoutLeadingZero, "Hash40::new(\"" + paramDict[hashString] + "\"")
+                        withoutLeadingZero = hashString.replace("0x0", "0x")
+                        text = text.replace(withoutLeadingZero + "u64", withoutLeadingZero)
+                        text = text.replace("Hash40::new_raw(" + withoutLeadingZero, "Hash40::new(\"" + paramDict[hashString] + "\"")
 
-                    #UNCOMMENTING THIS LINE WILL LIKELY CAUSE HDR TO NOT COMPILE AND WILL REQUIRE MANUAL EDITING AFTERWARDS
-                    #text = text.replace(lookingFor, "hash40(\"" + paramDict[hashString] + "\")")
-                else:
-                    uncrackedHashes.add(hashString)
+                        #UNCOMMENTING THESE LINES LIKELY CAUSE HDR TO NOT COMPILE AND WILL REQUIRE MANUAL EDITING AFTERWARDS
+                        #text = text.replace(hashString, "hash40(\"" + paramDict[hashString] + "\")")
+                        #text = text.replace(withoutLeadingZero, "hash40(\"" + paramDict[hashString] + "\")")
+                    else:
+                        uncrackedHashes.add(hashString)
 
-            if changed:
-                f.write(text)
+                if changed:
+                    f.write(text)
+        elif file.endswith((".prcxml", ".xml", ".yaml")):
+            with open(os.path.join(root, file), "r+") as f:
+                changed = False
+                text = f.read()
+                
+                for hashString in re.findall(regex, text):
+                    if len(hashString) == 11:
+                        hashString = hashString.replace("0x", "0x0")
+                    
+                    if hashString in paramDict.keys():
+                        changed = True
+                        
+                        crackedHashes.add(paramDict[hashString])
+                        text = text.replace(hashString, paramDict[hashString])
+
+                        withoutLeadingZero = hashString.replace("0x0", "0x")
+                        text = text.replace(withoutLeadingZero, paramDict[hashString])
+                    else:
+                        uncrackedHashes.add(hashString)
+
+                if changed:
+                    f.write(text)
+            
                 
 print(f"Cracked hashes: {crackedHashes}")
 print(f"Uncracked hashes: {uncrackedHashes}")
