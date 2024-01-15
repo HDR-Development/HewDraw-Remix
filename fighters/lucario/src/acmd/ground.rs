012244b2c3888703dd6a1fcaa1fe652ccef6744b
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2024-01-15 13:46:21 -0500

    Update ground.rs

--- fighters/lucario/src/acmd/ground.rs
+++ fighters/lucario/src/acmd/ground.rs
@@ -79,6 +79,9 @@ unsafe fn lucario_attack_12_game(fighter: &mut L2CAgentBase) {
     if is_excute(fighter) {
         MeterModule::watch_damage(fighter.battle_object, false);
         AttackModule::clear_all(boma);
+    }
+    frame(lua_state, 11.0);
+    if is_excute(fighter) {
         WorkModule::on_flag(boma, *FIGHTER_STATUS_ATTACK_FLAG_ENABLE_COMBO);
     }
 }
