babd9cca203ab91315f308c28f9bec9922f44afb
4Lukaska <79385530+4Lukaska@users.noreply.github.com>
2024-01-16 01:19:18 -0500

    Pill slipping and chill pill buffs
    lol

--- fighters/mariod/src/opff.rs
+++ fighters/mariod/src/opff.rs
@@ -21,6 +21,21 @@ unsafe fn super_sheet_stall(boma: &mut BattleObjectModuleAccessor) {
         }
     }
 }
+//Pill Slipping
+unsafe fn pill_slip(fighter: &mut L2CFighterCommon, boma: &mut BattleObjectModuleAccessor, id: usize, status_kind: i32, situation_kind: i32, cat1: i32, frame: f32) {
+    if StatusModule::is_changing(boma) {
+        return;
+    }
+    if status_kind == *FIGHTER_STATUS_KIND_SPECIAL_N {
+        if situation_kind == *SITUATION_KIND_GROUND {
+            if frame <= 6.0 {
+                if boma.is_cat_flag(Cat1::Dash) {
+                    KineticModule::add_speed(boma, &Vector3f{ x: 0.28, y: 0.0, z: 0.0 });
+                }
+            }
+        }
+    }
+}
 
 unsafe fn fastfall_specials(fighter: &mut L2CFighterCommon) {
     if !fighter.is_in_hitlag()
@@ -55,6 +70,7 @@ unsafe fn fastfall_specials(fighter: &mut L2CFighterCommon) {
 pub unsafe fn moveset(fighter: &mut L2CFighterCommon, boma: &mut BattleObjectModuleAccessor, id: usize, cat: [i32 ; 4], status_kind: i32, situation_kind: i32, motion_kind: u64, stick_x: f32, stick_y: f32, facing: f32, frame: f32) {
     //super_sheet_stall(boma, status_kind, situation_kind, frame);
     fastfall_specials(fighter);
+    pill_slip(fighter, boma, id, status_kind, situation_kind, cat[0], frame);
 }
 
 #[utils::macros::opff(FIGHTER_KIND_MARIOD )]
@@ -64,7 +80,6 @@ pub fn mariod_frame_wrapper(fighter: &mut smash::lua2cpp::L2CFighterCommon) {
 		mariod_frame(fighter)
     }
 }
-
 pub unsafe fn mariod_frame(fighter: &mut smash::lua2cpp::L2CFighterCommon) {
     if let Some(info) = FrameInfo::update_and_get(fighter) {
         moveset(fighter, &mut *info.boma, info.id, info.cat, info.status_kind, info.situation_kind, info.motion_kind.hash, info.stick_x, info.stick_y, info.facing, info.frame);
