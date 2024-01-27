1b7b948456be97509c052487176e2615c3f639da
WuBoytH <alexwuief@gmail.com>
2024-01-26 19:50:06 -0500

    Up Air Total Duration Set the Same

--- fighters/ganon/src/acmd/aerials.rs
+++ fighters/ganon/src/acmd/aerials.rs
@@ -192,7 +192,7 @@ unsafe fn ganon_attack_air_hi_game(fighter: &mut L2CAgentBase) {
         ATTACK(fighter, 1, 0, Hash40::new("kneel"), 7.0, 0, 80, 0, 20, 5.8, 6.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
     }
     frame(lua_state, 19.0);
-    FT_MOTION_RATE(fighter, 15.0/(31.0 - 19.0));
+    FT_MOTION_RATE(fighter, 14.0/(31.0 - 19.0));
     if is_excute(fighter) {
         AttackModule::clear_all(boma);
     }
