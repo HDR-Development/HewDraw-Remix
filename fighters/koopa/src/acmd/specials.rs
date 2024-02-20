04cf66762b3091b4cc9ad22efda26bd0757797ca
C# <afr0man005@gmail.com>
2024-02-20 03:47:31 -0800

    Update specials.rs

--- fighters/koopa/src/acmd/specials.rs
+++ fighters/koopa/src/acmd/specials.rs
@@ -152,7 +152,7 @@ unsafe fn koopa_special_s_squat_effect(fighter: &mut L2CAgentBase) {
 #[acmd_script( agent = "koopa", scripts = ["game_specialsthrowlw"], category = ACMD_GAME)]
 unsafe fn koopa_special_s_throwlw_game(fighter: &mut L2CAgentBase) {
     if is_excute(fighter) {
-        ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, 0, 12.0, 80, 81, 0, 60, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
+        ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_THROW, 0, 12.0, 82, 80, 0, 60, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
         ATTACK_ABS(fighter, *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH, 0, 3.0, 361, 100, 0, 40, 0.0, 1.0, *ATTACK_LR_CHECK_F, 0.0, true, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
     }
     frame(fighter.lua_state_agent, 18.0);
