38886d6c5e39785001b9854f66ee60674402612f
LunchablesTX <ZacharySain@yahoo.com>
2024-02-21 13:41:39 -0600

    fixing is_execute line for counter ft_motion_rate

--- fighters/roy/src/acmd/specials.rs
+++ fighters/roy/src/acmd/specials.rs
@@ -1015,9 +1015,7 @@ unsafe fn game_speciallwhit(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
     let boma = fighter.boma();
     frame(lua_state, 1.0);
-    if is_excute(fighter) {
     FT_MOTION_RATE_RANGE(fighter, 1.0, 5.0, 4.0);
-    }
     frame(lua_state, 5.0);
     if is_excute(fighter) {
         ATTACK(fighter, 0, 0, Hash40::new("top"), 8.0, 35, 100, 0, 40, 3.0, 0.0, 19.0, 16.5, Some(0.0), Some(21.0), Some(10.0), 1.2, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_fire"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_FIRE, *ATTACK_REGION_SWORD);
