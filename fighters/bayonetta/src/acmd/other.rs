a5c4a3e59a44091cad231902a9ec266ed863017d
Askew <sqk9gw@gmail.com>
2024-03-15 19:59:31 -0400

    Update other.rs

--- fighters/bayonetta/src/acmd/other.rs
+++ fighters/bayonetta/src/acmd/other.rs
@@ -115,7 +115,7 @@ unsafe extern "C" fn damageflytop_sound(fighter: &mut L2CAgentBase) {
     }
 }
 
-unsafe fn dash_game(fighter: &mut L2CAgentBase) {
+unsafe extern "C" fn dash_game(fighter: &mut L2CAgentBase) {
     let lua_state = fighter.lua_state_agent;
     let boma = fighter.boma();
     frame(lua_state, 14.0);
