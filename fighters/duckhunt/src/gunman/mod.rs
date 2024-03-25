a1352a54ecfc719c80d7a463f687d61bd2295f36
mokl <tr1pledee64@gmail.com>
2024-03-25 14:50:01 -0500

    woof

--- fighters/duckhunt/src/gunman/mod.rs
+++ fighters/duckhunt/src/gunman/mod.rs
@@ -6,6 +6,6 @@ mod opff;
 pub fn install() {
     let agent = &mut Agent::new("duckhunt_gunman");
     acmd::install(agent);
-    opff:install(agent);
+    opff::install(agent);
     agent.install();
 }
