9e8e0923694d5fba5915fe15a3e02e7491073fdb
mokl <tr1pledee64@gmail.com>
2024-03-25 20:28:16 -0500

    hehe

--- fighters/richter/src/axe/mod.rs
+++ fighters/richter/src/axe/mod.rs
@@ -3,7 +3,7 @@ use super::*;
 mod acmd;
 
 pub fn install() {
-    let agent = &mut Agent::new("richter_whip");
+    let agent = &mut Agent::new("richter_axe");
     acmd::install(agent);
     agent.install();
 }
