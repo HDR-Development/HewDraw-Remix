c7d87d1dbc221c9f550131168f21bc120f9825eb
C# <afr0man005@gmail.com>
2024-04-06 14:46:04 -0700

    fixes

--- fighters/inkling/src/status/special_lw.rs
+++ fighters/inkling/src/status/special_lw.rs
@@ -1,6 +1,41 @@
 use super::*;
 use globals::*;
 
+unsafe extern "C" fn special_lw_empty_main(agent: &mut L2CFighterCommon) -> L2CValue {
+    let use_cancel_anim = WorkModule::is_flag(agent.module_accessor,*FIGHTER_INKLING_INSTANCE_WORK_ID_FLAG_CAN_SPECIAL_LW_BOMB);
+    let motion_g = if use_cancel_anim {hash40("special_lw_cancel")} else {hash40("special_lw_empty")};
+    let motion_a = if use_cancel_anim {hash40("special_air_lw_cancel")} else {hash40("special_air_lw_empty")};
+    agent.sub_change_motion_by_situation(motion_g.into(), motion_a.into(), false.into());
+    agent.sub_set_ground_correct_by_situation(true.into());
+    agent.main_shift(special_lw_empty_main_loop)
+}
+
+unsafe extern "C" fn special_lw_empty_main_loop(agent: &mut L2CFighterCommon) -> L2CValue {
+    if CancelModule::is_enable_cancel(agent.module_accessor) {
+        if agent.sub_wait_ground_check_common(false.into()).get_bool()
+        || agent.sub_air_check_fall_common().get_bool() {
+            return 1.into();
+        }
+    }
+    if MotionModule::is_end(agent.module_accessor) {
+        let new_status = if agent.global_table[SITUATION_KIND] == SITUATION_KIND_AIR {FIGHTER_STATUS_KIND_FALL} else {FIGHTER_STATUS_KIND_WAIT};
+        agent.change_status(new_status.into(), false.into());
+        return 1.into();
+    }
+    if !StatusModule::is_changing(agent.module_accessor)
+    && StatusModule::is_situation_changed(agent.module_accessor) {
+        agent.sub_set_ground_correct_by_situation(false.into());
+        agent.sub_change_kinetic_type_by_situation(FIGHTER_KINETIC_TYPE_GROUND_STOP.into(),FIGHTER_KINETIC_TYPE_AIR_STOP.into());
+        let use_cancel_anim =WorkModule::is_flag(agent.module_accessor,*FIGHTER_INKLING_INSTANCE_WORK_ID_FLAG_CAN_SPECIAL_LW_BOMB);
+        let motion_g = if use_cancel_anim {hash40("special_lw_cancel")} else {hash40("special_lw_empty")};
+        let motion_a = if use_cancel_anim {hash40("special_air_lw_cancel")} else {hash40("special_air_lw_empty")};
+        agent.sub_change_motion_by_situation(motion_g.into(), motion_a.into(), true.into());
+    }
+
+    0.into()
+} 
+
+
 pub unsafe extern "C" fn special_lw_main(fighter: &mut L2CFighterCommon) -> L2CValue {
     let toreturn = smashline::original_status(Main, fighter, *FIGHTER_STATUS_KIND_SPECIAL_LW)(fighter);
 
@@ -8,6 +43,11 @@ pub unsafe extern "C" fn special_lw_main(fighter: &mut L2CFighterCommon) -> L2CV
     if stored_charge > 0.0 {
         MotionModule::set_frame_sync_anim_cmd(fighter.module_accessor, stored_charge, true, true, false);
         WorkModule::set_float(fighter.module_accessor,stored_charge,*FIGHTER_INKLING_STATUS_SPECIAL_LW_WORK_FLOAT_CHARGE_FRAME);
+
+        let fighter_ptr = fighter.global_table[0x4].get_ptr() as *mut Fighter;
+        let ink_cost = FighterSpecializer_Inkling::get_sub_ink_special_lw(fighter_ptr);
+        let can_max_shot = super::spend_ink(fighter,-ink_cost);
+
         if stored_charge >= MotionModule::end_frame(fighter.module_accessor) {
             EffectModule::remove_common(fighter.module_accessor, Hash40::new("charge_max"));
             fighter.change_status(FIGHTER_INKLING_STATUS_KIND_SPECIAL_LW_THROW.into(),false.into());
@@ -41,20 +81,9 @@ pub unsafe extern "C" fn special_lw_throw_init(fighter: &mut L2CFighterCommon) -
 
 pub fn install() {
     smashline::Agent::new("inkling")
-        .status(
-            Main,
-            *FIGHTER_STATUS_KIND_SPECIAL_LW,
-            special_lw_main,
-        )
-        .status(
-            Exec,
-            *FIGHTER_STATUS_KIND_SPECIAL_LW,
-            special_lw_exec,
-        )
-        .status(
-            Init,
-            *FIGHTER_INKLING_STATUS_KIND_SPECIAL_LW_THROW,
-            special_lw_throw_init,
-        )
-        .install();
+        .status(Main, *FIGHTER_STATUS_KIND_SPECIAL_LW, special_lw_main)
+        .status(Exec, *FIGHTER_STATUS_KIND_SPECIAL_LW, special_lw_exec)
+        .status(Init, *FIGHTER_INKLING_STATUS_KIND_SPECIAL_LW_THROW, special_lw_throw_init)
+        .status(Main, *FIGHTER_INKLING_STATUS_KIND_SPECIAL_LW_EMPTY, special_lw_empty_main)
+    .install();
 }
