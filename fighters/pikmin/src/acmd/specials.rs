6bd9055fd75cc3637540a30ba5cc42301252ffb7
ncsuddut <ncsuddut@ncsu.edu>
2024-04-10 20:05:08 -0400

    olimar add pluck hitbox and buff reflect

--- fighters/pikmin/src/acmd/specials.rs
+++ fighters/pikmin/src/acmd/specials.rs
@@ -36,7 +36,7 @@ unsafe extern "C" fn game_speciallw(agent: &mut L2CAgentBase) {
     if is_excute(agent) {
         WorkModule::on_flag(boma, *FIGHTER_PIKMIN_STATUS_SPECIAL_LW_FLAG_SORT);
         // damage!(fighter, *MA_MSC_DAMAGE_DAMAGE_NO_REACTION, *DAMAGE_NO_REACTION_MODE_ALWAYS, 0);
-        shield!(agent, *MA_MSC_CMD_REFLECTOR, *COLLISION_KIND_REFLECTOR, 0, hash40("top"), 7.5, 0.0, 7.0, -8.5, 0.0, 7.0, 8.5, 1.0, 0.8, 50, false, 0.8, *FIGHTER_REFLECTOR_GROUP_HOMERUNBAT);
+        shield!(agent, *MA_MSC_CMD_REFLECTOR, *COLLISION_KIND_REFLECTOR, 0, hash40("top"), 7.5, 0.0, 7.0, -8.5, 0.0, 7.0, 8.5, 1.5, 1.0, 50, false, 0.8, *FIGHTER_REFLECTOR_GROUP_HOMERUNBAT);
     }
     frame(lua_state, 7.0);
     if is_excute(agent) {
@@ -59,14 +59,21 @@ unsafe extern "C" fn effect_speciallw(agent: &mut L2CAgentBase) {
     }
 }
 
-unsafe extern "C" fn stub(agent: &mut L2CAgentBase) {}
+unsafe extern "C" fn game_specialnfailure(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    frame(lua_state, 3.0);
+    if is_excute(agent) {
+        ATTACK(agent, 0, 0, Hash40::new("top"), 8.5, 80, 85, 0, 45, 4.0, 0.0, 2.0, 3.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.5, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_PUNCH, *ATTACK_REGION_PUNCH);
+    }
+}
 
 pub fn install(agent: &mut Agent) {
     agent.acmd("game_specials", game_specials);
     agent.acmd("game_specialairs", game_specials);
     agent.acmd("game_specialnstart", game_specialnstart);
-    agent.acmd("game_specialnfailure", stub);
-    agent.acmd("game_specialairnfailure", stub);
+    agent.acmd("game_specialnfailure", game_specialnfailure);
+    agent.acmd("game_specialairnfailure", game_specialnfailure);
     agent.acmd("game_speciallw", game_speciallw);
     agent.acmd("game_specialairlw", game_speciallw);
     agent.acmd("effect_speciallw", effect_speciallw);
