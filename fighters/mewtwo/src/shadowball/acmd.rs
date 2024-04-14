2e53fc5965226389b207e3710dbd109120dd1e32
ncsuddut <ncsuddut@ncsu.edu>
2024-04-13 20:55:59 -0400

    add mewtwo shadowball charge hitbox - same stats as lucario aura ball

---
+++ fighters/mewtwo/src/shadowball/acmd.rs
@@ -0,0 +1,13 @@
+use super::*;
+
+unsafe extern "C" fn game_charge(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    if is_excute(agent) {
+        ATTACK(agent, 0, 0, Hash40::new("top"), 1.0, 83, 93, 0, 10, 2.5, 0.0, 0.0, 0.0, None, None, None, 0.78, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 5, false, false, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_purple"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_FIRE, *ATTACK_REGION_NONE);
+    }
+}
+
+pub fn install(agent: &mut Agent) {
+    agent.acmd("game_charge", game_charge);
+}
