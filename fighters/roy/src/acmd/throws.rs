6ff28f213970e8205f39c07b5054440491e4d6f2
4Lukaska <79385530+4Lukaska@users.noreply.github.com>
2024-04-25 08:21:07 -0400

    resolve conflicts

--- fighters/roy/src/acmd/throws.rs
+++ fighters/roy/src/acmd/throws.rs
@@ -174,14 +174,14 @@ unsafe extern "C" fn game_throwlw(agent: &mut L2CAgentBase) {
 }
 
 pub fn install(agent: &mut Agent) {
-    agent.acmd("game_catch", game_catch);
-    agent.acmd("game_catchdash", game_catchdash);
-    agent.acmd("game_catchturn", game_catchturn);
+    agent.acmd("game_catch", game_catch, Priority::Low);
+    agent.acmd("game_catchdash", game_catchdash, Priority::Low);
+    agent.acmd("game_catchturn", game_catchturn, Priority::Low);
 
-    agent.acmd("game_throwf", game_throwf);
-    agent.acmd("effect_throwf", effect_throwf);
-    agent.acmd("sound_throwf", sound_throwf);
-    agent.acmd("game_throwb", game_throwb);
-    agent.acmd("game_throwhi", game_throwhi);
-    agent.acmd("game_throwlw", game_throwlw);
+    agent.acmd("game_throwf", game_throwf, Priority::Low);
+    agent.acmd("effect_throwf", effect_throwf, Priority::Low);
+    agent.acmd("sound_throwf", sound_throwf, Priority::Low);
+    agent.acmd("game_throwb", game_throwb, Priority::Low);
+    agent.acmd("game_throwhi", game_throwhi, Priority::Low);
+    agent.acmd("game_throwlw", game_throwlw, Priority::Low);
 }
