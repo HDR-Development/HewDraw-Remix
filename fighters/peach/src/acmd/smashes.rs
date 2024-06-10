1ac50eadd349f4988dae663eee217dc17ea2fff2
stingers135 <115596132+stingers135@users.noreply.github.com>
2024-06-09 21:56:42 -0400

    one less loop + less shaking

--- fighters/peach/src/acmd/smashes.rs
+++ fighters/peach/src/acmd/smashes.rs
@@ -8,12 +8,14 @@ unsafe extern "C" fn game_attacklw4(agent: &mut L2CAgentBase) {
         WorkModule::on_flag(boma, *FIGHTER_STATUS_ATTACK_FLAG_START_SMASH_HOLD);
     }
     frame(lua_state, 5.0);
-    for _ in 0..6 {
+    for _ in 0..5 {
         if is_excute(agent) {
             ATTACK(agent, 0, 0, Hash40::new("top"), 14.0, 140, 80, 0, 40, 3.5, -2.5, 2.0, 0.0, Some(2.5), Some(2.0), Some(0.0), 0.5, 0.5, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_HEAVY, *ATTACK_REGION_BODY);
             ATTACK(agent, 1, 0, Hash40::new("top"), 12.0, 140, 80, 0, 35, 4.5, -2.5, 2.0, 7.5, Some(2.5), Some(2.0), Some(7.5), 0.5, 0.5, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_HEAVY, *ATTACK_REGION_BODY);
             ATTACK(agent, 2, 0, Hash40::new("top"), 12.0, 140, 80, 0, 35, 4.5, -2.5, 2.0, -7.5, Some(2.5), Some(2.0), Some(-7.5), 0.5, 0.5, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_HEAVY, *ATTACK_REGION_BODY);
             AttackModule::set_attack_height_all(boma, app::AttackHeight(*ATTACK_HEIGHT_LOW), false);
+            AttackModule::set_damage_shake_scale(boma, 0.5);
+            AttackModule::set_no_damage_fly_smoke_all(boma, true, false);
         }
         wait(lua_state, 2.0);
         if is_excute(agent) {
