77f5db5b96e7d882a5595045358bc843c58d389f
WuBoytH <alexwuief@gmail.com>
2024-06-25 20:59:55 -0400

    fix jostle on jump

--- fighters/gekkouga/src/acmd/specials.rs
+++ fighters/gekkouga/src/acmd/specials.rs
@@ -12,6 +12,18 @@ unsafe extern "C" fn game_speciallw(agent: &mut L2CAgentBase) {
     }
 }
 
+unsafe extern "C" fn game_speciallwjump(agent: &mut L2CAgentBase) {
+    let lua_state = agent.lua_state_agent;
+    let boma = agent.boma();
+    if is_excute(agent) {
+        JostleModule::set_status(boma, false);
+    }
+    frame(lua_state, 5.0);
+    if is_excute(agent) {
+        JostleModule::set_status(boma, true);
+    }
+}
+
 unsafe extern "C" fn effect_speciallwjump(agent: &mut L2CAgentBase) {
     if is_excute(agent) {
         EFFECT(agent, Hash40::new("gekkouga_migawari_smoke"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
@@ -28,6 +40,7 @@ unsafe extern "C" fn sound_speciallwjump(agent: &mut L2CAgentBase) {
 pub fn install(agent: &mut Agent) {
     agent.acmd("game_speciallw", game_speciallw, Priority::Low);
 
+    agent.acmd("game_speciallwjump", game_speciallwjump, Priority::Low);
     agent.acmd("effect_speciallwjump", effect_speciallwjump, Priority::Low);
     agent.acmd("sound_speciallwjump", sound_speciallwjump, Priority::Low);
 }
