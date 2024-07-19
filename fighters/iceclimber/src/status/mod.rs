99ecc89ba9d8056b795b22540c121932dc62fbf0
ncsuddut <ncsuddut@ncsu.edu>
2024-07-19 01:44:43 -0400

    update throws and couple distance

--- fighters/iceclimber/src/status/mod.rs
+++ fighters/iceclimber/src/status/mod.rs
@@ -42,7 +42,7 @@ pub unsafe extern "C" fn throw_nana(fighter: &mut L2CFighterCommon) -> L2CValue 
     let is_under_platform = GroundModule::ray_check(
         fighter.module_accessor, 
         &Vector2f{ x: pos.x, y: pos.y + 38.0}, 
-        &Vector2f{ x: 0.0, y: 37.0},
+        &Vector2f{ x: 0.0, y: -37.0},
         true
     ) == 1;
     let motion = if is_near_cliff {
@@ -58,16 +58,12 @@ pub unsafe extern "C" fn throw_nana(fighter: &mut L2CFighterCommon) -> L2CValue 
     } else if is_under_platform {
         Hash40::new("throw_hi")
     } else {
-        Hash40::new("throw_lw")
+        let selected = app::sv_math::rand(hash40("fighter"), 100);
+        match selected {
+            0..=49 => Hash40::new("throw_f"),
+            _ => Hash40::new("throw_lw")
+        }
     };
-
-    let pos = PostureModule::pos_2d(fighter.module_accessor);
-    let is_platform_above = GroundModule::ray_check(
-        fighter.module_accessor, 
-        &Vector2f{ x: pos.x, y: pos.y + 38.0}, 
-        &Vector2f{ x: 0.0, y: -37.0},
-        true
-    ) == 1;
 
     // change into the selected motion
     MotionModule::change_motion(fighter.boma(), motion, 0.0, 1.0, false, 0.0, false, false);
@@ -76,20 +72,19 @@ pub unsafe extern "C" fn throw_nana(fighter: &mut L2CFighterCommon) -> L2CValue 
     fighter.sub_shift_status_main(L2CValue::Ptr(L2CFighterCommon_status_Throw_Main as *const () as _))
 }
 
-unsafe extern "C" fn catchwait_nana_main(fighter: &mut L2CFighterCommon) -> L2CValue {
-    fighter.sub_shift_status_main(L2CValue::Ptr(catchwait_nana_main_loop as *const () as _))
+unsafe extern "C" fn nana_catch_wait_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
+    StatusModule::set_status_kind_interrupt(fighter.module_accessor, *FIGHTER_STATUS_KIND_THROW);
+    return true.into();
 }
 
-unsafe extern "C" fn catchwait_nana_main_loop(fighter: &mut L2CFighterCommon) -> L2CValue {
-    // let boma = fighter.boma();
-    // let opponent_boma = boma.get_grabbed_opponent_boma();
-    // let damage = DamageModule::damage(opponent_boma, 0);
-    // if damage > 50.0 {
-    //     fighter.change_status(FIGHTER_STATUS_KIND_CATCH_ATTACK.into(), false.into());
-    //     return 0.into();
-    // }
+unsafe extern "C" fn nana_catch_wait_main(fighter: &mut L2CFighterCommon) -> L2CValue {
+    fighter.sub_shift_status_main(L2CValue::Ptr(nana_catch_wait_main_loop as *const () as _))
+}
+
+unsafe extern "C" fn nana_catch_wait_main_loop(fighter: &mut L2CFighterCommon) -> L2CValue {
+    MotionModule::set_frame(fighter.module_accessor, MotionModule::end_frame(fighter.module_accessor), true);
     fighter.change_status(FIGHTER_STATUS_KIND_THROW.into(), false.into());
-    0.into()
+    return true.into();
 }
 
 unsafe extern "C" fn popo_status_kind_throw_nana_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
@@ -129,7 +124,8 @@ pub fn install_nana(agent: &mut Agent) {
     agent.status(Main, *FIGHTER_STATUS_KIND_TURN_RUN, status_TurnRun_main);
     agent.status(Main, *FIGHTER_STATUS_KIND_TURN_RUN_BRAKE, status_TurnRunBrake_main);
     agent.status(Main, *FIGHTER_STATUS_KIND_THROW, throw_nana);
-    agent.status(Main, *FIGHTER_STATUS_KIND_CATCH_WAIT, catchwait_nana_main);
+    agent.status(Pre, *FIGHTER_STATUS_KIND_CATCH_WAIT, nana_catch_wait_pre);
+    agent.status(Main, *FIGHTER_STATUS_KIND_CATCH_WAIT, nana_catch_wait_main);
     agent.status(Pre, *FIGHTER_POPO_STATUS_KIND_THROW_NANA, popo_status_kind_throw_nana_pre);
     agent.status(Main, *FIGHTER_POPO_STATUS_KIND_THROW_NANA, popo_status_kind_throw_nana_main);
 }
