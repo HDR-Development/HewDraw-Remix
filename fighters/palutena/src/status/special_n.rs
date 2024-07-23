7e1d59ac1096ad88a7d069db2af5c3e4485c4fed
WuBoytH <alexwuief@gmail.com>
2024-07-23 14:48:17 -0400

    palu neutral b fix (real)

--- fighters/palutena/src/status/special_n.rs
+++ fighters/palutena/src/status/special_n.rs
@@ -95,14 +95,142 @@ unsafe extern "C" fn special_n_color_pre(fighter: &mut L2CFighterCommon) -> L2CV
     0.into()
 }
 
+#[no_mangle]
+unsafe extern "C" fn palutena_special_n_init_common(fighter: &mut L2CFighterCommon) -> L2CValue {
+    palutena_special_n_momentum_helper(fighter, true.into());
+
+    let mut speed_y = KineticModule::get_sum_speed_y(fighter.module_accessor, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN);
+    if !WorkModule::is_flag(fighter.module_accessor, *FIGHTER_PALUTENA_INSTANCE_WORK_ID_FLAG_SPECIAL_N_LANDING) {
+        let special_n_speed_y_mul = WorkModule::get_param_float(fighter.module_accessor, hash40("param_special_n"), hash40("special_n_speed_y_mul"));
+        speed_y *= special_n_speed_y_mul;
+        if fighter.global_table[SITUATION_KIND].get_i32() == *SITUATION_KIND_AIR {
+            let special_n_speed_y_add = WorkModule::get_param_float(fighter.module_accessor, hash40("param_special_n"), hash40("special_n_speed_y_add"));
+            speed_y += special_n_speed_y_add;
+        }
+    }
+    sv_kinetic_energy!(
+        reset_energy,
+        fighter,
+        FIGHTER_KINETIC_ENERGY_ID_GRAVITY,
+        ENERGY_GRAVITY_RESET_TYPE_GRAVITY,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0
+    );
+    sv_kinetic_energy!(
+        set_speed,
+        fighter,
+        FIGHTER_KINETIC_ENERGY_ID_GRAVITY,
+        speed_y
+    );
+    KineticModule::enable_energy(fighter.module_accessor, *FIGHTER_KINETIC_ENERGY_ID_GRAVITY);
+    if fighter.global_table[SITUATION_KIND].get_i32() == *SITUATION_KIND_GROUND {
+        sv_kinetic_energy!(
+            set_accel,
+            fighter,
+            FIGHTER_KINETIC_ENERGY_ID_GRAVITY,
+            0.0
+        );
+    }
+    sv_kinetic_energy!(
+        set_speed,
+        fighter,
+        FIGHTER_KINETIC_ENERGY_ID_CONTROL,
+        0.0,
+        0.0
+    );
+    sv_kinetic_energy!(
+        controller_set_accel_x_add,
+        fighter,
+        0.0
+    );
+    KineticModule::unable_energy(fighter.module_accessor, *FIGHTER_KINETIC_ENERGY_ID_CONTROL);
+    sv_kinetic_energy!(
+        set_speed,
+        fighter,
+        FIGHTER_KINETIC_ENERGY_ID_MOTION,
+        0.0,
+        0.0
+    );
+    KineticModule::unable_energy(fighter.module_accessor, *FIGHTER_KINETIC_ENERGY_ID_MOTION);
+
+    0.into()
+}
+
+#[no_mangle]
+unsafe extern "C" fn palutena_special_n_momentum_helper(fighter: &mut L2CFighterCommon, start: L2CValue) {
+    let mut speed_x = KineticModule::get_sum_speed_x(fighter.module_accessor, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN);
+    if start.get_bool() {
+        let special_n_speed_x_mul = WorkModule::get_param_float(fighter.module_accessor, hash40("param_special_n"), hash40("special_n_speed_x_mul"));
+        speed_x *= special_n_speed_x_mul;
+    }
+
+    let reset_type = if fighter.global_table[SITUATION_KIND].get_i32() == *SITUATION_KIND_GROUND {
+        ENERGY_STOP_RESET_TYPE_GROUND
+    }
+    else {
+        ENERGY_STOP_RESET_TYPE_AIR
+    };
+    sv_kinetic_energy!(
+        reset_energy,
+        fighter,
+        FIGHTER_KINETIC_ENERGY_ID_STOP,
+        reset_type,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0
+    );
+    sv_kinetic_energy!(
+        set_speed,
+        fighter,
+        FIGHTER_KINETIC_ENERGY_ID_STOP,
+        speed_x,
+        0.0
+    );
+    KineticModule::enable_energy(fighter.module_accessor, *FIGHTER_KINETIC_ENERGY_ID_STOP);
+
+    if !start.get_bool() {
+        sv_kinetic_energy!(
+            reset_energy,
+            fighter,
+            FIGHTER_KINETIC_ENERGY_ID_GRAVITY,
+            ENERGY_GRAVITY_RESET_TYPE_GRAVITY,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0
+        );
+        sv_kinetic_energy!(
+            set_speed,
+            fighter,
+            FIGHTER_KINETIC_ENERGY_ID_GRAVITY,
+            0.0
+        );
+        KineticModule::enable_energy(fighter.module_accessor, *FIGHTER_KINETIC_ENERGY_ID_GRAVITY);
+        if fighter.global_table[SITUATION_KIND].get_i32() == *SITUATION_KIND_GROUND {
+            sv_kinetic_energy!(
+                set_accel,
+                fighter,
+                FIGHTER_KINETIC_ENERGY_ID_GRAVITY,
+                0.0
+            );
+        }
+    }
+}
+
 unsafe extern "C" fn special_n_main_common(fighter: &mut L2CFighterCommon, g_mot: u64, a_mot: u64) {
     if !fighter.is_situation(*SITUATION_KIND_GROUND) {
         GroundModule::correct(fighter.module_accessor, GroundCorrectKind(*GROUND_CORRECT_KIND_AIR));
-        MotionModule::change_motion(fighter.module_accessor, Hash40::new_raw(g_mot), 0.0, 1.0, false, 0.0, false, false);
+        MotionModule::change_motion(fighter.module_accessor, Hash40::new_raw(a_mot), 0.0, 1.0, false, 0.0, false, false);
     }
     else {
         GroundModule::correct(fighter.module_accessor, GroundCorrectKind(*GROUND_CORRECT_KIND_GROUND_CLIFF_STOP));
-        MotionModule::change_motion(fighter.module_accessor, Hash40::new_raw(a_mot), 0.0, 1.0, false, 0.0, false, false);
+        MotionModule::change_motion(fighter.module_accessor, Hash40::new_raw(g_mot), 0.0, 1.0, false, 0.0, false, false);
     }
 }
 
@@ -114,18 +242,18 @@ unsafe extern "C" fn special_n_main_loop_common(fighter: &mut L2CFighterCommon, 
         }
     }
 
-    if !StatusModule::is_changing(fighter.module_accessor) {
-        if StatusModule::is_situation_changed(fighter.module_accessor) {
-            if fighter.global_table[SITUATION_KIND] == SITUATION_KIND_GROUND {
-                GroundModule::correct(fighter.module_accessor, app::GroundCorrectKind(*GROUND_CORRECT_KIND_GROUND));
-                KineticModule::change_kinetic(fighter.module_accessor, *FIGHTER_KINETIC_TYPE_GROUND_STOP);
-                MotionModule::change_motion_inherit_frame(fighter.module_accessor, Hash40::new_raw(g_mot), -1.0, 1.0, 0.0, false, false);
-            }
-            else {
-                GroundModule::correct(fighter.module_accessor, app::GroundCorrectKind(*GROUND_CORRECT_KIND_AIR));
-                KineticModule::change_kinetic(fighter.module_accessor, *FIGHTER_KINETIC_TYPE_AIR_STOP);
-                MotionModule::change_motion_inherit_frame(fighter.module_accessor, Hash40::new_raw(a_mot), -1.0, 1.0, 0.0, false, false);
-            }
+    if !StatusModule::is_changing(fighter.module_accessor)
+    && StatusModule::is_situation_changed(fighter.module_accessor) {
+        palutena_special_n_momentum_helper(fighter, false.into());
+        if fighter.global_table[SITUATION_KIND] == SITUATION_KIND_GROUND {
+            GroundModule::correct(fighter.module_accessor, app::GroundCorrectKind(*GROUND_CORRECT_KIND_GROUND));
+            KineticModule::change_kinetic(fighter.module_accessor, *FIGHTER_KINETIC_TYPE_GROUND_STOP);
+            MotionModule::change_motion_inherit_frame(fighter.module_accessor, Hash40::new_raw(g_mot), -1.0, 1.0, 0.0, false, false);
+        }
+        else {
+            GroundModule::correct(fighter.module_accessor, app::GroundCorrectKind(*GROUND_CORRECT_KIND_AIR));
+            KineticModule::change_kinetic(fighter.module_accessor, *FIGHTER_KINETIC_TYPE_AIR_STOP);
+            MotionModule::change_motion_inherit_frame(fighter.module_accessor, Hash40::new_raw(a_mot), -1.0, 1.0, 0.0, false, false);
         }
     }
 
@@ -140,10 +268,21 @@ unsafe extern "C" fn special_n_main_loop_common(fighter: &mut L2CFighterCommon, 
     return 0.into()
 }
 
+// colorless
+unsafe extern "C" fn special_n_main(fighter: &mut L2CFighterCommon) -> L2CValue {
+    special_n_main_common(fighter, hash40("special_n"), hash40("special_air_n"));
+
+    fighter.main_shift(special_n_main_loop)
+}
+
+unsafe extern "C" fn special_n_main_loop(fighter: &mut L2CFighterCommon) -> L2CValue {
+    special_n_main_loop_common(fighter, hash40("special_n"), hash40("special_air_n"))
+}
+
 // red: burn attack
 unsafe extern "C" fn special_n_r_main(fighter: &mut L2CFighterCommon) -> L2CValue {
     special_n_main_common(fighter, hash40("special_n_r"), hash40("special_air_n_r"));
-    
+
     if VarModule::get_int(fighter.object(), vars::palutena::instance::POWER_BOARD_SLOT_2) == 1 {
         VarModule::on_flag(fighter.object(), vars::palutena::instance::POWERED);
     }
@@ -245,27 +384,35 @@ unsafe extern "C" fn special_n_g_end(fighter: &mut L2CFighterCommon) -> L2CValue
 
 pub fn install(agent: &mut Agent) {
     agent.status(Pre, *FIGHTER_STATUS_KIND_SPECIAL_N, special_n_pre);
+    agent.status(Init, *FIGHTER_STATUS_KIND_SPECIAL_N, palutena_special_n_init_common);
+    agent.status(Main, *FIGHTER_STATUS_KIND_SPECIAL_N, special_n_main);
 
     agent.status(Pre, statuses::palutena::SPECIAL_N_R, special_n_color_pre);
+    agent.status(Init, statuses::palutena::SPECIAL_N_R, palutena_special_n_init_common);
     agent.status(Main, statuses::palutena::SPECIAL_N_R, special_n_r_main);
     agent.status(End, statuses::palutena::SPECIAL_N_R, special_n_r_end);
 
     agent.status(Pre, statuses::palutena::SPECIAL_N_B, special_n_color_pre);
+    agent.status(Init, statuses::palutena::SPECIAL_N_B, palutena_special_n_init_common);
     agent.status(Main, statuses::palutena::SPECIAL_N_B, special_n_b_main);
     agent.status(End, statuses::palutena::SPECIAL_N_B, special_n_b_end);
 
     agent.status(Pre, statuses::palutena::SPECIAL_N_Y, special_n_color_pre);
+    agent.status(Init, statuses::palutena::SPECIAL_N_Y, palutena_special_n_init_common);
     agent.status(Main, statuses::palutena::SPECIAL_N_Y, special_n_y_main);
     agent.status(End, statuses::palutena::SPECIAL_N_Y, special_n_y_end);
 
     agent.status(Pre, statuses::palutena::SPECIAL_N_P, special_n_color_pre);
+    agent.status(Init, statuses::palutena::SPECIAL_N_P, palutena_special_n_init_common);
     agent.status(Main, statuses::palutena::SPECIAL_N_P, special_n_p_main);
     agent.status(End, statuses::palutena::SPECIAL_N_P, special_n_p_end);
 
     agent.status(Pre, statuses::palutena::SPECIAL_N_O, special_n_color_pre);
+    agent.status(Init, statuses::palutena::SPECIAL_N_O, palutena_special_n_init_common);
     agent.status(Main, statuses::palutena::SPECIAL_N_O, special_n_o_main);
 
     agent.status(Pre, statuses::palutena::SPECIAL_N_G, special_n_color_pre);
+    agent.status(Init, statuses::palutena::SPECIAL_N_G, palutena_special_n_init_common);
     agent.status(Main, statuses::palutena::SPECIAL_N_G, special_n_g_main);
     agent.status(End, statuses::palutena::SPECIAL_N_G, special_n_g_end);
 }
