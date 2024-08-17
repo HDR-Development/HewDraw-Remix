9e0243d7bd8bc0a0381d8c2070c89ba2926898f8
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2024-08-16 22:00:22 -0400

    sub_calc_landing_motion_rate impl

--- fighters/common/src/general_statuses/mod.rs
+++ fighters/common/src/general_statuses/mod.rs
@@ -132,6 +132,7 @@ fn nro_hook(info: &skyline::nro::NroInfo) {
             FighterStatusDamage__correctDamageVectorEffect,
             sub_fighter_pre_end_status,
             sub_is_dive,
+            sub_calc_landing_motion_rate,
         );
     }
 }
@@ -748,6 +749,20 @@ pub unsafe fn sub_is_dive(fighter: &mut L2CFighterCommon) -> L2CValue {
     }
     
     true.into()
+}
+
+#[skyline::hook(replace = L2CFighterCommon_sub_calc_landing_motion_rate)]
+unsafe fn sub_calc_landing_motion_rate(_fighter: &mut L2CFighterCommon, end_frame: L2CValue, landing_frame: L2CValue) -> L2CValue {
+    // Coupled with "landing_heavy" change in change_motion hook
+    // Because we start heavy landing anims on f3 rather than f1, we need to reduce the total length of the anim by 3 frames
+    // when calculating motion rate for airdodge landing or hitstun landing
+    let end_frame_adjusted = end_frame.get_f32() - 3.0;
+    let end_over_landing = end_frame_adjusted / landing_frame.get_f32();
+    let end_frame_for_some_reason = end_over_landing * landing_frame.get_f32();
+    let huh = end_frame_adjusted - end_frame_for_some_reason;
+    let end_frame_plus = huh + 0.01;
+    let idk_anymore = end_frame_plus / landing_frame.get_f32();
+    (end_over_landing + idk_anymore).into()
 }
 
 pub fn install() {
