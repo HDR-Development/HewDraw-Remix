7a35cc6c820f7255fcf59777e1c0a824aa3ee469
mokl <tr1pledee64@gmail.com>
2024-08-18 13:12:16 -0500

    revert dair startup

--- fighters/inkling/src/acmd/aerials.rs
+++ fighters/inkling/src/acmd/aerials.rs
@@ -162,7 +162,7 @@ unsafe extern "C" fn game_attackairlw(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
     frame(lua_state, 6.0);
-    FT_MOTION_RATE_RANGE(agent, 6.0, 16.0, 6.0);
+    FT_MOTION_RATE_RANGE(agent, 6.0, 16.0, 8.0);
     if is_excute(agent) {
         WorkModule::on_flag(boma, *FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING);
     }
@@ -182,7 +182,7 @@ unsafe extern "C" fn game_attackairlw(agent: &mut L2CAgentBase) {
         }
     }
     wait(lua_state, 2.0);
-    FT_MOTION_RATE(agent, 1.4);  
+    FT_MOTION_RATE(agent, 1.2);  
     if is_excute(agent){
         ATTACK(agent, 0, 0, Hash40::new("shoulderr"), 9.0, 65, 100, 0, 10, 3.5, 1.5, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_OBJECT);
         ATTACK(agent, 1, 0, Hash40::new("haver"), 9.0, 65, 100, 0, 10, 4.0, 0.0, 1.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_OBJECT);
