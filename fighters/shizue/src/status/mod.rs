74b1d6d1ad623fbbc3dba1ec874dbf7f3a4bdf94
mokl <tr1pledee64@gmail.com>
2024-08-20 15:26:00 -0500

    initial fishing rod reimplementation

--- fighters/shizue/src/status/mod.rs
+++ fighters/shizue/src/status/mod.rs
@@ -3,6 +3,7 @@ use globals::*;
 // status script import
 
 mod special_hi;
+mod special_s;
 
 unsafe extern "C" fn on_start(fighter: &mut L2CFighterCommon) {
     VarModule::off_flag(fighter.object(), vars::shizue::instance::LLOID_ASYNC);
@@ -13,4 +14,5 @@ pub fn install(agent: &mut Agent) {
     agent.on_start(on_start);
 
     special_hi::install(agent);
+    special_s::install(agent);
 }
