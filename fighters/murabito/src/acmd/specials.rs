2c799f5d2b38ce4949abd671dde89d3ba33001cb
SuddyN <suddy121800@hotmail.com>
2024-09-30 19:40:54 -0400

    villager shovel size nerf

--- fighters/murabito/src/acmd/specials.rs
+++ fighters/murabito/src/acmd/specials.rs
@@ -45,7 +45,7 @@ unsafe extern "C" fn game_specialairlw1failure(agent: &mut L2CAgentBase) {
     if is_excute(agent) {
         MotionModule::set_rate(boma, 0.4);
         WorkModule::on_flag(boma, *FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING);
-        ATTACK(agent, 0, 0, Hash40::new("top"), 6.0, 78, 62, 0, 70, 5.0, 0.0, -6.0, 1.0, Some(0.0), Some(-1.5), Some(1.0), 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 30, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_OBJECT);
+        ATTACK(agent, 0, 0, Hash40::new("top"), 6.0, 78, 62, 0, 70, 4.0, 0.0, -1.0, 1.0, Some(0.0), Some(-5.0), Some(1.0), 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 30, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_OBJECT);
     }
     frame(lua_state, 63.0);
     if is_excute(agent) {
