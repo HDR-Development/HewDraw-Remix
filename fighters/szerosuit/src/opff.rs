9252857c398f29d6eef5602ba71099c46f5e0b36
Askew <sqk9gw@gmail.com>
2024-10-02 09:28:21 -0400

    ZSS

--- fighters/szerosuit/src/opff.rs
+++ fighters/szerosuit/src/opff.rs
@@ -2,46 +2,39 @@
 utils::import_noreturn!(common::opff::fighter_common_opff);
 use super::*;
 use globals::*;
- 
-unsafe fn paralyzer_dash_cancel(boma: &mut BattleObjectModuleAccessor, status_kind: i32, situation_kind: i32, cat1: i32, frame: f32) {
-    if status_kind == *FIGHTER_SZEROSUIT_STATUS_KIND_SPECIAL_N_SHOOT {
-        if frame > 8.0 {
-            boma.check_dash_cancel();
-        }
-    }
-}
 
-// ZSS Flip Jump - Jump Cancel and Flipstool Handilng
-unsafe fn flip_jump_jc_flipstool(boma: &mut BattleObjectModuleAccessor, status_kind: i32, motion_kind: u64, cat1: i32, frame: f32) {
+// ZSS Flip Jump - Jump Cancel and Flipstool Handling
+unsafe fn flip_jump_jc_flipstool(boma: &mut BattleObjectModuleAccessor) {
     if StatusModule::is_changing(boma) {
         return;
     }
-    if status_kind == *FIGHTER_STATUS_KIND_SPECIAL_LW
-        || motion_kind == hash40("special_lw_start")
-        || motion_kind == hash40("special_air_lw_start") {
-        if frame > 21.0 {
-            if !boma.is_in_hitlag() {
-                boma.check_jump_cancel(false, false);
-            }
+    if boma.is_status(*FIGHTER_STATUS_KIND_SPECIAL_LW)
+    || boma.is_motion_one_of(&[Hash40::new("special_lw_start"), Hash40::new("special_air_lw_start")]) {
+        if boma.status_frame() > 21 && !boma.is_in_hitlag() {
+            boma.check_jump_cancel(false, false);
         }
         // Turn on the vanilla flip jump footstool-enable flag if you're holding the special button and you're in the window to be able to flipstool manually
-        if VarModule::is_flag(boma.object(), vars::szerosuit::status::SPECIAL_LW_ENABLE_MANUAL_FOOTSTOOL){
-            if ControlModule::check_button_on(boma, *CONTROL_PAD_BUTTON_SPECIAL) || ControlModule::check_button_on(boma, *CONTROL_PAD_BUTTON_SPECIAL_RAW){
+        if VarModule::is_flag(boma.object(), vars::szerosuit::status::SPECIAL_LW_ENABLE_MANUAL_FOOTSTOOL) {
+            if ControlModule::check_button_on(boma, *CONTROL_PAD_BUTTON_SPECIAL)
+            || ControlModule::check_button_on(boma, *CONTROL_PAD_BUTTON_SPECIAL_RAW) {
                 WorkModule::on_flag(boma, *FIGHTER_SZEROSUIT_STATUS_SPECIAL_LW_FLAG_TREAD_ENABLE);
             }
-            else{
+            else {
                 WorkModule::off_flag(boma, *FIGHTER_SZEROSUIT_STATUS_SPECIAL_LW_FLAG_TREAD_ENABLE);
             }
         }
     }
 }
 
-// Transitions ZSS into Flip Jump's footstool rebund animation upon connecting with dair
-unsafe fn dair_rebound(fighter: &mut smash::lua2cpp::L2CFighterCommon) {
-    if fighter.is_status(*FIGHTER_STATUS_KIND_ATTACK_AIR) && fighter.is_motion(Hash40::new("attack_air_lw")) {
-        if AttackModule::is_infliction_status(fighter.module_accessor, *COLLISION_KIND_MASK_HIT | *COLLISION_KIND_MASK_SHIELD) && !fighter.is_in_hitlag(){
-            VarModule::on_flag(fighter.battle_object, vars::szerosuit::status::ATTACK_AIR_LW_REBOUND);
-            fighter.change_status_req(*FIGHTER_SZEROSUIT_STATUS_KIND_SPECIAL_LW_FLIP, true);
+// Transitions ZSS into Flip Jump's footstool rebound animation upon connecting with dair
+unsafe fn dair_rebound(fighter: &mut L2CFighterCommon) {
+    if fighter.is_motion(Hash40::new("attack_air_lw"))
+    && AttackModule::is_infliction_status(fighter.module_accessor, *COLLISION_KIND_MASK_HIT | *COLLISION_KIND_MASK_SHIELD)
+    && !fighter.is_in_hitlag() {
+        VarModule::on_flag(fighter.battle_object, vars::szerosuit::status::ATTACK_AIR_LW_REBOUND);
+        fighter.change_status_req(*FIGHTER_SZEROSUIT_STATUS_KIND_SPECIAL_LW_FLIP, true);
+        if AttackModule::is_infliction_status(fighter.module_accessor, *COLLISION_KIND_MASK_SHIELD) {
+            VarModule::on_flag(fighter.battle_object, vars::szerosuit::status::SPECIAL_LW_ENABLE_MANUAL_FOOTSTOOL);
         }
     }
 }
@@ -78,10 +71,8 @@ unsafe fn fastfall_specials(fighter: &mut L2CFighterCommon) {
     }
 }
 
-pub unsafe fn moveset(fighter: &mut smash::lua2cpp::L2CFighterCommon, boma: &mut BattleObjectModuleAccessor, id: usize, cat: [i32 ; 4], status_kind: i32, situation_kind: i32, motion_kind: u64, stick_x: f32, stick_y: f32, facing: f32, frame: f32) {
-
-    //paralyzer_dash_cancel(boma, status_kind, situation_kind, cat[0], frame);
-    flip_jump_jc_flipstool(boma, status_kind, motion_kind, cat[0], frame);
+pub unsafe fn moveset(fighter: &mut smash::lua2cpp::L2CFighterCommon, boma: &mut BattleObjectModuleAccessor) {
+    flip_jump_jc_flipstool(boma);
     dair_rebound(fighter);
     fastfall_specials(fighter);
 }
@@ -95,7 +86,7 @@ pub extern "C" fn szerosuit_frame_wrapper(fighter: &mut smash::lua2cpp::L2CFight
 
 pub unsafe fn szerosuit_frame(fighter: &mut smash::lua2cpp::L2CFighterCommon) {
     if let Some(info) = FrameInfo::update_and_get(fighter) {
-        moveset(fighter, &mut *info.boma, info.id, info.cat, info.status_kind, info.situation_kind, info.motion_kind.hash, info.stick_x, info.stick_y, info.facing, info.frame);
+        moveset(fighter, &mut *info.boma);
     }
 }
 
