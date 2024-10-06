c6eb8d8b285c3642d3abd71ed914d7fad07c6021
Askew <sqk9gw@gmail.com>
2024-10-06 08:16:47 -0400

    pocket.rs crash fix

--- fighters/common/src/opff/pocket.rs
+++ fighters/common/src/opff/pocket.rs
@@ -11,6 +11,8 @@ unsafe fn ac_update(fighter: &mut L2CFighterCommon) {
         let object_id = WorkModule::get_int(fighter.module_accessor,*FIGHTER_MURABITO_INSTANCE_WORK_ID_INT_TARGET_OBJECT_ID) as u32;
         if object_id == 0 || object_id == 0x50000000 {return;}
         let object_boma = sv_battle_object::module_accessor(object_id);
+        let object_object = utils::util::get_battle_object_from_id(object_id);
+        if !VarModule::has_var_module(object_object) { return; }
         if VarModule::is_flag(&mut *(*object_boma).object(), vars::common::status::NO_POCKET) {
             //Change Villager status
             StatusModule::change_status_request_from_script(fighter.module_accessor, *FIGHTER_MURABITO_STATUS_KIND_SPECIAL_N_FAILURE,false);
@@ -42,7 +44,6 @@ unsafe fn ac_update(fighter: &mut L2CFighterCommon) {
                 false,
                 0
             );
-            
         }
     }
 }
