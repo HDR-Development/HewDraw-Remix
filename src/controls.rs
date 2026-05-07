7641ad1b89831adbd481e5d8f11f57eb6b46b022
BlankMauser <thomasluvan@gmail.com>
2026-05-07 16:30:21 -0700

    Finish ssbusync integration

--- src/controls.rs
+++ src/controls.rs
@@ -1,6 +1,5 @@
 use skyline::hooks::InlineCtx;
 use std::sync::atomic::{AtomicBool, Ordering};
-// use ssbusync::*;
 
 mod css;
 mod submenu;
--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,7 @@ mod online;
 #[cfg(feature = "main_nro")]
 mod matchup;
 
-pub mod vsync;
+//pub mod vsync;
 
 use skyline::libc::c_char;
 #[cfg(feature = "main_nro")]
