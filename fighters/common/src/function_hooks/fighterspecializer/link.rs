280a7c046c460bca7c7435bee5c5a5697387e9b5
xTheft <bensonalex324@gmail.com>
2026-06-22 22:09:44 -0500

    Bomb roll and boomerang tweak

---
+++ fighters/common/src/function_hooks/fighterspecializer/link.rs
@@ -0,0 +1,6 @@
+    extern "C" {
+        #[link_name = "\u{1}_ZN3app23FighterSpecializer_Link29set_item_hold_anim_for_scriptERNS_7FighterE"]
+        pub fn set_item_hold_anim_for_script(
+            fighter: *mut smash::app::Fighter,
+        ) -> u64;
+    }
--- fighters/link/src/boomerang/acmd.rs
+++ fighters/link/src/boomerang/acmd.rs
@@ -4,8 +4,12 @@ unsafe extern "C" fn game_fly(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
     if is_excute(agent) {
-        ATTACK(agent, 0, 0, Hash40::new("top"), 11.0, 78, 43, 0, 60, 4.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_SPEED, false, 0, 0.0, 0, true, false, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_OBJECT);
+        ATTACK(agent, 0, 0, Hash40::new("top"), 11.0, 78, 40, 0, 64, 4.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_SPEED, false, 0, 0.0, 0, true, false, false, false, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_OBJECT);
         AttackModule::enable_safe_pos(boma);
+    }
+    frame(lua_state, 5.0);
+    if is_excute(agent) {
+        VarModule::on_flag(agent.battle_object, vars::link::status::ENABLE_SPECIAL_LW_CANCEL);
     }
     frame(lua_state, 8.0);
     if is_excute(agent) {
