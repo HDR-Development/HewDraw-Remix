f3ebf0e3ba3b1fba18f105c51ae95fbaa534070a
ryn <ryanhunteralbert@gmail.com>
2026-08-09 16:44:15 -0500

    ling

--- fighters/inkling/src/inkbullet/acmd.rs
+++ fighters/inkling/src/inkbullet/acmd.rs
@@ -9,7 +9,14 @@ unsafe extern "C" fn game_fly(agent: &mut L2CAgentBase) {
         AttackModule::enable_safe_pos(boma);
         AttackModule::set_ink_value(boma, 0, 0.0);
     }
-    wait(lua_state, 2.0);
+    frame(lua_state, 2.0);
+    if is_excute(agent) {
+        ATTACK(agent, 0, 0, Hash40::new("top"), 0.0, 45, 100, 4, 0, 3.0, 0.0, 0.0, 5.0, None, None, None, 0.3, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_SPEED, true, 0, 0.0, 0, true, true, false, true, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_ink_hit"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_INKLING_HIT, *ATTACK_REGION_WATER);
+        ATK_SET_SHIELD_SETOFF_MUL(agent, 0, 0.5);
+        AttackModule::enable_safe_pos(boma);
+        AttackModule::set_ink_value(boma, 0, 0.0);
+    }
+    frame(lua_state, 3.0);
     if is_excute(agent) {
         ATTACK(agent, 0, 0, Hash40::new("top"), 0.0, 361, 0, 0, 0, 3.0, 0.0, 0.0, 5.0, None, None, None, 0.3, 1.0, *ATTACK_SETOFF_KIND_OFF, *ATTACK_LR_CHECK_SPEED, false, 0, 0.0, 0, true, true, true, true, false, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_ink_hit"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_INKLING_HIT, *ATTACK_REGION_WATER);
         ATK_SET_SHIELD_SETOFF_MUL(agent, 0, 0.5);
